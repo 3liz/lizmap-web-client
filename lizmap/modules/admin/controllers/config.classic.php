38ad534a8d9267ccfbdf39d4df3654cb0ae04582
Michael Douchin <mdouchin@3liz.com>
2012-09-28 17:23:49 +0200

    Admin - remove use of sqlite3 group_concat and replace by php code

--- lizmap/modules/admin/controllers/config.classic.php
+++ lizmap/modules/admin/controllers/config.classic.php
@@ -52,13 +52,26 @@ class configCtrl extends jController {
     $cnx = jDb::getConnection('jacl2_profile');
     $data = array();
     foreach($lizmapConfig->repositoryList as $repo){
-      $sql = " SELECT r.id_aclsbj, group_concat(g.name, ' - ') AS group_names";
+      //$sql = " SELECT r.id_aclsbj, group_concat(g.name, ' - ') AS group_names";
+      $sql = " SELECT r.id_aclsbj, g.name AS group_name";
       $sql.= " FROM jacl2_rights r";
       $sql.= " INNER JOIN jacl2_group g ON r.id_aclgrp = g.id_aclgrp";
       $sql.= " WHERE g.grouptype = 0 AND r.id_aclgrp NOT IN ('".implode("','", $this->groupBlacklist)."')";
       $sql.= " AND id_aclres=".$cnx->quote($repo);
-      $sql.= " GROUP BY r.id_aclsbj;";
+      //$sql.= " GROUP BY r.id_aclsbj;";
       $rights = $cnx->query($sql);
+
+      $group_names = array();
+      foreach ($rights as $r) {
+	if (!array_key_exists($r->id_aclsbj,$group_names))
+          $group_names[$r->id_aclsbj] = array();
+	$group_names[$r->id_aclsbj][] = $r->group_name;
+      }
+      foreach ($group_names as $k => $v) {
+	$group_names[$k] = implode(' - ', $v);
+      }
+      $rights = (object) $group_names;
+
       $data[$repo] = $rights;
     }
 
