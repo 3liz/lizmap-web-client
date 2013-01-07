3a54c7c7b3106146adfc02d305bec77e5ec60386
René-Luc D'Hont <rldhont@gmail.com>
2013-01-07 11:54:07 +0100

    authentification with jauth on index entry point

--- lizmap/var/config/index/config.ini.php
+++ lizmap/var/config/index/config.ini.php
@@ -24,7 +24,8 @@ jacl2db.installparam=defaultuser
 jacl2db_admin.access=2
 admin.access=2
 [simple_urlengine_entrypoints]
-admin="jacl2db~*@classic, jauth~*@classic, jacl2db_admin~*@classic, jauthdb_admin~*@classic, master_admin~*@classic, admin~*@classic"
+index="jauth~*@classic"
+admin="jacl2db~*@classic, jacl2db_admin~*@classic, jauthdb_admin~*@classic, master_admin~*@classic, admin~*@classic"
 [acl2]
 driver=db
 
