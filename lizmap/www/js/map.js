cb1d91cb52ab61b6f25acc5daf68f6820f832eaa
Michael Douchin <mdouchin@3liz.com>
2014-02-24 17:58:16 +0100

    Openlayers - re-define image reload attempt for 2.13

--- lizmap/www/js/map.js
+++ lizmap/www/js/map.js
@@ -722,7 +722,7 @@ var lizMap = function() {
     });
 
     // creating the map
-    OpenLayers.IMAGE_RELOAD_ATTEMPTS = 3; // Avoid some issues with tiles not displayed
+    OpenLayers.Util.IMAGE_RELOAD_ATTEMPTS = 3; // Avoid some issues with tiles not displayed
     OpenLayers.Util.DEFAULT_PRECISION=20; // default is 14 : change needed to avoid rounding problem with cache
     map = new OpenLayers.Map('map'
       ,{
