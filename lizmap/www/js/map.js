3liz/lizmap-web-client

commit 4e6d64278c32ad9729936de64b7edc71c7aade56

[DEBUG] Removing duplicate scales
Verifying that the scales list does not contain duplicate values.

lizmap/www/js/map.js
@@ -720,6 +720,15 @@ var lizMap = function() {
     scales.sort(function(a, b) {
       return Number(b) - Number(a);
     });
+    // remove duplicate scales
+    nScales = [];
+    while (scales.length != 0){
+      var scale = scales.pop(0);
+      if (nScales.indexOf( scale ) == -1 )
+        nScales.push( scale );
+    }
+    scales = nScales;
+
 
     // creating the map
     OpenLayers.Util.IMAGE_RELOAD_ATTEMPTS = 3; // Avoid some issues with tiles not displayed

commit 4e06d9c75b3e8211e43144349bfce84ae9e8440b

[DEBUG] Array.indexOf not supported by IE
Array.indexOf is not supported by Internet Explorer, replaced by
$.inArray
BUG introduced by commit 4e6d64278c32ad9729936de64b7edc71c7aade56

lizmap/www/js/map.js
@@ -724,7 +724,7 @@ var lizMap = function() {
     nScales = [];
     while (scales.length != 0){
       var scale = scales.pop(0);
-      if (nScales.indexOf( scale ) == -1 )
+      if ($.inArray( scale, nScales ) == -1 )
         nScales.push( scale );
     }
     scales = nScales;