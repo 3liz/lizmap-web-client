1b015d136298c2f2de90b37d027bedc877926134
rldhont <rldhont@gmail.com>
2014-11-17 16:39:03 +0100

    [BUGFIX] #104 Add print overview compatibility with QGIS 2.6
    If no overview map doesn't add ComposerMapOverview->attributes()->frameMap

--- lizmap/modules/lizmap/classes/lizmapProject.class.php
+++ lizmap/modules/lizmap/classes/lizmapProject.class.php
@@ -426,7 +426,7 @@ class lizmapProject{
             // >= 2.6
             $cMapOverviews = $cMap->xpath('ComposerMapOverview');
             foreach($cMapOverviews as $cMapOverview){
-              if ( $cMapOverview ){
+              if ( $cMapOverview and (string)$cMapOverview->attributes()->frameMap != '-1' ){
                 $ptMap['overviewMap'] = 'map' . (string)$cMapOverview->attributes()->frameMap;
               }
             }
