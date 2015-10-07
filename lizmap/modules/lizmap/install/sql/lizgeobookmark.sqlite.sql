44e34d6229f6a1a424f19576a7e8636d6260b864
Michael Douchin <mdouchin@3liz.com>
2015-10-07 20:13:07 +0200

    Debug migration from 3.0pre to 3.0beta1

--- lizmap/modules/lizmap/install/sql/lizgeobookmark.sqlite.sql
+++ lizmap/modules/lizmap/install/sql/lizgeobookmark.sqlite.sql
@@ -4,6 +4,7 @@ CREATE TABLE geobookmark(
   bname text NOT NULL,
   bmap text NOT NULL,
   bparams text NOT NULL,
-  FOREIGN KEY (usr_login) REFERENCES jlx_user (id_aclgrp)
+  FOREIGN KEY (usr_login) REFERENCES jlx_user (usr_login)
 );
 CREATE INDEX idx_geobookmark_usr_login ON geobookmark( usr_login );
+
--- lizmap/modules/lizmap/install/upgrade_geobookmark.php
+++ lizmap/modules/lizmap/install/upgrade_geobookmark.php
@@ -8,15 +8,14 @@ class lizmapModuleUpgrader_geobookmark extends jInstallerModule {
         '2.10.3',
         '2.11.0',
         '2.11.1',
-        '3.0pre'
+        '3.0pre',
+        '3.0beta1'
     );
     public $date = '2015-09-16';
 
     function install() {
-
         // Ajout de la table geobookmark
         if( $this->firstDbExec() ) {
-
             // Add geobookmark table
             $this->useDbProfile('jauth');
             $this->execSQLScript('sql/lizgeobookmark');
