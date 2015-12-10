f0d68a6106d96f08294aedce9db20267d45c8713
Michael Douchin <mdouchin@3liz.com>
2015-12-10 19:08:01 +0100

    [BUGFIX] Correct database new config created with upgrade-to-3.php

--- lizmap/install/upgrade-to-3.php
+++ lizmap/install/upgrade-to-3.php
@@ -34,10 +34,14 @@ if (null === $installerIni->getValue('view.contexts', '__modules_data')) {
 $installerIni->save();
 
 if ($profiles->getValue('dsn', 'jdb:jauth') == 'sqlite:var:jauth.db') {
-    $profiles->setValue('dsn', 'sqlite:var:db/jauth.db', 'jdb:jauth');
+    $profiles->setValue('dsn', Null, 'jdb:jauth');
+    $profiles->setValue('driver', 'sqlite3', 'jdb:jauth');
+    $profiles->setValue('database', 'var:db/jauth.db', 'jdb:jauth');
 }
 if ($profiles->getValue('dsn', 'jdb:lizlog') == 'sqlite:var:logs.db') {
-    $profiles->setValue('dsn', 'sqlite:var:db/logs.db', 'jdb:lizlog');
+    $profiles->setValue('dsn', Null, 'jdb:lizlog');
+    $profiles->setValue('driver', 'sqlite3', 'jdb:lizlog');
+    $profiles->setValue('database', 'var:db/logs.db', 'jdb:lizlog');
 }
 $profiles->save();
 
