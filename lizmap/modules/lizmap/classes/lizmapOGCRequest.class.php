4445a738e963a138d769c184a3c9bd2cd43e0b4b
Michael Douchin <mdouchin@3liz.com>
2016-02-05 10:22:08 +0100

    OGC Service - Cache GetCapabilities responses only for WMTS

--- lizmap/modules/lizmap/classes/lizmapOGCRequest.class.php
+++ lizmap/modules/lizmap/classes/lizmapOGCRequest.class.php
@@ -11,15 +11,15 @@
 
 jClasses::inc('lizmap~lizmapProxy');
 class lizmapOGCRequest {
-    
+
     protected $project = null;
-    
+
     protected $repository = null;
-    
+
     protected $params = null;
-    
+
     protected $services = null;
-    
+
     protected $tplExceptions = null;
 
     /**
@@ -30,15 +30,15 @@ class lizmapOGCRequest {
     public function __construct ( $project, $params ) {
         //print_r( $project != null );
         $this->project = $project;
-        
+
         $this->repository = $project->getRepository();
-        
+
         $this->services = lizmap::getServices();
-        
+
         $params['map'] = realpath($this->repository->getPath()) . '/' . $project->getKey() . ".qgs";
         $this->params = lizmapProxy::normalizeParams( $params );
     }
-    
+
     /**
     * Gets the value of a request parameter. If not defined, gets its default value.
     * @param string  $name           the name of the request parameter
@@ -58,23 +58,23 @@ class lizmapOGCRequest {
             return $defaultValue;
         }
     }
-    
+
     public function process ( ) {
         return $this->{$this->param('request')}();
     }
-    
+
     protected function serviceException ( ) {
         $messages = jMessage::getAll();
         $mime = 'text/plain';
         $data = implode('\n', $messages);
-        
+
         if ( $this->tplExceptions !== null ) {
             $mime = 'text/xml';
             $tpl = new jTpl();
             $tpl->assign('messages', $messages);
             $data = $tpl->fetch( $this->tplExceptions );
         }
-        
+
         return (object) array(
             'code' => 200,
             'mime' => $mime,
@@ -82,34 +82,9 @@ class lizmapOGCRequest {
             'cached' => False
         );
     }
-    
+
     protected function getcapabilities ( ) {
-        $cacheId = $this->repository->getKey().'_'.$this->project->getKey().'_'.$this->param('service');
-        $hash = jCache::get($cacheId . '_hash');
 
-        $cachedMime = jCache::get($cacheId . '_mime');
-        $cachedData = jCache::get($cacheId . '_data');
-        $newhash = md5_file( realpath($this->repository->getPath()) . '/' . $this->project->getKey() . ".qgs" );
-
-        // Verifying cache content
-        if ( $cachedData && preg_match( '#ServiceExceptionReport#i', $cachedData ) ) {
-            $hash = null;
-            $cachedMime = null;
-            $cachedData = null;
-            jCache::delete($cacheId . '_hash');
-        }
-
-        // Cache exists for the unchanged QGIS project file
-        if( $hash and $cachedMime and $cachedData and $hash == $newhash ) {
-            return (object) array(
-                'code' => 200, // 304 ?
-                'mime' => $cachedMime,
-                'data' => $cachedData,
-                'cached' => True
-            );
-        }
-        
-        // No cache or the file content has changed
         $url = $this->services->wmsServerURL.'?';
 
         $bparams = http_build_query($this->params);
@@ -124,7 +99,7 @@ class lizmapOGCRequest {
         $data = $getRemoteData[0];
         $mime = $getRemoteData[1];
         $code = $getRemoteData[2];
-        
+
         return (object) array(
             'code' => $code,
             'mime' => $mime,
@@ -133,5 +108,5 @@ class lizmapOGCRequest {
         );
 
     }
-  
+
 }
