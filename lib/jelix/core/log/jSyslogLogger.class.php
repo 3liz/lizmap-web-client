7172ce49b0a3314a676b55fef2aae80d4df47921
Michael Douchin <mdouchin@3liz.com>
2016-04-07 18:06:52 +0200

    [jelix] Improve syslog logger

--- lib/jelix/core/log/jSyslogLogger.class.php
+++ lib/jelix/core/log/jSyslogLogger.class.php
@@ -12,20 +12,47 @@
  * logger storing message into syslog
  */
 class jSyslogLogger implements jILogger {
+
+    protected $catSyslog = array();
+
+    public function __construct() {
+        $this->catSyslog = array(
+            'error'=> LOG_ERR,
+            'warning'=> LOG_WARNING,
+            'notice'=> LOG_NOTICE,
+            'deprecated'=> LOG_NOTICE,
+            'strict'=> LOG_NOTICE,
+            'debug'=> LOG_DEBUG,
+        );
+        openlog('jelix - '.jApp::config()->domainName,
+                LOG_ODELAY | LOG_PERROR,
+                (jApp::config()->isWindows?LOG_USER:LOG_LOCAL0));
+    }
+
     /**
      * @param jILogMessage $message the message to log
      */
     function logMessage($message) {
         $type = $message->getCategory();
-
-        if (jApp::coord()->request)
+        if (jApp::coord()->request) {
             $ip = jApp::coord()->request->getIP();
-        else
+        }
+        else {
             $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '127.0.0.1';
+        }
 
-        error_log(date ("Y-m-d H:i:s")."\t".$ip."\t$type\t".$message->getFormatedMessage(), 0);
+        if (isset($this->catSyslog[$type])) {
+            $priority = $this->catSyslog[$type];
+        }
+        else {
+            $priority = LOG_INFO;
+        }
+        syslog($priority, $ip."\t$type\t".$message->getFormatedMessage());
     }
 
-    function output($response) {}
+    function output($response) {
+    }
 
 }
+
+
