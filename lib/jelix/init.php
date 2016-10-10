4b140ccfb72afcc4693e32460e45e33ac307ab60
Laurent Jouanneau <dev@ljouanneau.com>
2016-10-10 15:56:14 +0200

    Upgrade jelix to 1.6.11pre

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -16,7 +16,7 @@
  * Version number of Jelix
  * @name  JELIX_VERSION
  */
-define ('JELIX_VERSION', '1.6.11pre.3312');
+define ('JELIX_VERSION', '1.6.11pre.3313');
 
 /**
  * base of namespace path used in xml files of jelix
