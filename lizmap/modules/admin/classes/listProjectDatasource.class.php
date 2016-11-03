308909f9d9962ea57e9bb4f45a1a5180d8204160
Laurent Jouanneau <dev@ljouanneau.com>
2016-11-03 15:10:28 +0100

    Fix warning during dynamic load of projects list
    The listProjectDatasource did not return an array on getCriteriaControls().
    This datasource has been modernized.

--- lizmap/modules/admin/classes/listProjectDatasource.class.php
+++ lizmap/modules/admin/classes/listProjectDatasource.class.php
@@ -2,25 +2,24 @@
 
 require_once (JELIX_LIB_PATH.'forms/jFormsDatasource.class.php');
 
-class listProjectDatasource extends jFormsDaoDatasource
+class listProjectDatasource extends jFormsDynamicDatasource
 {
   protected $formId = 0;
-  protected $criteriaFrom = 'defaultRepository';
+  protected $criteriaFrom = array('defaultRepository');
   protected $data = array();
 
   function __construct($id)
   {
     $this->formId = $id;
-    $mydata = array();
-    foreach(lizmap::getRepositoryList() as $repo)
-      $mydata[$repo] = $repo;
-    $this->data = $mydata;
+    foreach(lizmap::getRepositoryList() as $repo) {
+        $this->data[$repo] = $repo;
+    }
   }
 
   public function getData($form)
   {
     $pdata = array();
-    $criteria = $form->getData($this->criteriaFrom);
+    $criteria = $form->getData($this->criteriaFrom[0]);
     if ( $criteria && array_key_exists($criteria, $this->data ) ) {
         $rep = lizmap::getRepository( $criteria );
         $projects = $rep->getProjects();
@@ -34,13 +33,9 @@ class listProjectDatasource extends jFormsDaoDatasource
     return $pdata;
   }
 
-  public function getLabel($key) {
-      throw new Exception("should not be called");
-  }
-
   public function getLabel2($key,$form)
   {
-    $criteria = $form->getData($this->criteriaFrom);
+    $criteria = $form->getData($this->criteriaFrom[0]);
     if ( $criteria && array_key_exists($criteria, $this->data ) ) {
         try {
             $p = lizmap::getProject( $criteria.'~'.$key );
@@ -54,17 +49,4 @@ class listProjectDatasource extends jFormsDaoDatasource
     }
     return null;
   }
-
-  public function getDependentControls() {
-      return array($this->criteriaFrom);
-  }
-
-  public function hasGroupedData() {
-      return $this->groupeBy;
-  }
-
-  public function setGroupBy($group) {
-      $this->groupeBy = $group;
-  }
-
 }
