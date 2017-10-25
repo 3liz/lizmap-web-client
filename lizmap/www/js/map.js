314d860dd0a443184f61bf1a5028038614f7619f
rldhont <rldhont@gmail.com>
2017-10-25 17:42:45 +0200

    [BUGFIX] GetPrint for inverted axis projection

--- lizmap/www/js/map.js
+++ lizmap/www/js/map.js
@@ -3641,7 +3641,14 @@ var lizMap = function() {
               if( t.composerMap == -1 || ('map'+t.composerMap) == dragCtrl.layout.mapId )
                 return t.vectorLayer;
           });
+      // Print Extent
       var extent = dragCtrl.layer.features[0].geometry.getBounds();
+
+      // Projection code and reverseAxisOrder
+      var projCode = map.projection.getCode();
+      var reverseAxisOrder = (OpenLayers.Projection.defaults[projCode] && OpenLayers.Projection.defaults[projCode].yx);
+
+      // Build URL
       var url = OpenLayers.Util.urlAppend(lizUrls.wms
           ,OpenLayers.Util.getParameterString(lizUrls.params)
           );
@@ -3649,10 +3656,10 @@ var lizMap = function() {
       //url += '&VERSION='+capabilities.version+'&REQUEST=GetPrint';
       url += '&VERSION=1.3.0&REQUEST=GetPrint';
       url += '&FORMAT=pdf&EXCEPTIONS=application/vnd.ogc.se_inimage&TRANSPARENT=true';
-      url += '&SRS='+map.projection;
+      url += '&SRS='+projCode;
       url += '&DPI='+$('#print-dpi').val();
       url += '&TEMPLATE='+pTemplate.title;
-      url += '&'+dragCtrl.layout.mapId+':extent='+extent;
+      url += '&'+dragCtrl.layout.mapId+':extent='+extent.toBBOX(null, reverseAxisOrder);
       //url += '&'+dragCtrl.layout.mapId+':rotation=0';
       var scale = $('#print-scale').val();
       url += '&'+dragCtrl.layout.mapId+':scale='+scale;
