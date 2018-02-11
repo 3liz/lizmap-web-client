dd7eb20accae004c415a981910f810cedceeb0d2
Laurent Jouanneau <dev@ljouanneau.com>
2018-02-11 16:35:22 +0100

    Restore email sending to the admin after a registration

--- lizmap/modules/admin/classes/lzmAuth.listener.php
+++ lizmap/modules/admin/classes/lzmAuth.listener.php
@@ -5,8 +5,22 @@ class lzmAuthListener extends jEventListener
     function onjcommunity_registration_prepare_save($event)
     {
         $event->user->comment = $event->form->getData('comment');
+        $event->user->firstname = $event->form->getData('firstname');
+        $event->user->lastname = $event->form->getData('lastname');
+        $event->user->organization = $event->form->getData('organization');
     }
 
+    function onjcommunity_registration_after_save($event) {
 
+        $services = lizmap::getServices();
+        if( $email = filter_var($services->adminContactEmail, FILTER_VALIDATE_EMAIL) ){
+            $mail = new jMailer();
+            $mail->Subject = jLocale::get("admin~user.email.admin.subject");
+            $mail->Body = jLocale::get("admin~user.email.admin.body",
+                array($event->user->login, $event->user->email));
+            $mail->AddAddress( $email, 'Lizmap Notifications');
+            $mail->Send();
+        }
+    }
 
 }
