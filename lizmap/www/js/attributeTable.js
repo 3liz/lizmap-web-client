021c51b97468692c9f58ae9c571e7c55463d09ca
rldhont <rldhont@gmail.com>
2018-03-23 17:08:24 +0100

    [BUGFIX] Attribute table: refresh after editing

--- lizmap/www/js/attributeTable.js
+++ lizmap/www/js/attributeTable.js
@@ -2390,7 +2390,12 @@ var lizAttributeTable = function() {
             function refreshTablesAfterEdition( featureType ){
                 // Loop through each datatable, and refresh if it corresponds to the layer edited
                 $('.attribute-table-table').each(function(){
+                    // get table id
                     var tableId = $(this).attr('id');
+                    // verifying the id
+                    if ( !tableId )
+                        return true;
+
                     var tableLayerName = $(this).parents('div.dataTables_wrapper:first').prev('input.attribute-table-hidden-layer').val()
 
                     if ( tableLayerName
