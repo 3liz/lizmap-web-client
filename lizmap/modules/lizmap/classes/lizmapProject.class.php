4d9c8f9f197080d94061be6bcf827b4f08c245f3
rldhont <rldhont@gmail.com>
2018-09-06 17:24:49 +0200

    [BUGFIX] Remove exclude attributes from WFS if it is in wfs field list

--- lizmap/modules/lizmap/classes/lizmapProject.class.php
+++ lizmap/modules/lizmap/classes/lizmapProject.class.php
@@ -430,6 +430,9 @@ class lizmapProject{
                 if ( $excludeFields && count($excludeFields) > 0 ) {
                     foreach( $excludeFields as $eField ) {
                         $eField = (string) $eField;
+                        if ( !in_array($eField, $wfsFields) ) {
+                            continue; // QGIS sometimes stores them twice
+                        }
                         array_splice( $wfsFields, array_search( $eField, $wfsFields ), 1 );
                     }
                     $layer['wfsFields'] = $wfsFields;
