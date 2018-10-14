1993370a96269522f66149249032b20a455acd67
Laurent Jouanneau <dev@ljouanneau.com>
2018-10-14 15:16:53 +0200

    Fix regression in html forms: bad jforms js lib loaded
    Refs #958

--- lizmap/plugins/formbuilder/htmlbootstrap/htmlbootstrap.formbuilder.php
+++ lizmap/plugins/formbuilder/htmlbootstrap/htmlbootstrap.formbuilder.php
@@ -24,6 +24,31 @@ class htmlbootstrapFormBuilder extends \jelix\forms\Builder\HtmlBuilder {
         'choice' => array( 'class' => 'form-inline', 'itemLabelClass'=>'radio')
     );
 
+    public function outputMetaContent($t) {
+        $resp= jApp::coord()->response;
+        if($resp === null || $resp->getType() !='html'){
+            return;
+        }
+
+        $confUrlEngine = &jApp::config()->urlengine;
+        $www = $confUrlEngine['jelixWWWPath'];
+        $jq = $confUrlEngine['jqueryPath'];
+        $resp->addJSLink($jq.'jquery.js');
+        $resp->addJSLink($jq.'include/jquery.include.js');
+        $resp->addJSLink($www.'js/jforms_jquery.js');
+        $resp->addCSSLink($www.'design/jform.css');
+
+        //we loop on root control has they fill call the outputMetaContent recursively
+        foreach( $this->_form->getRootControls() as $ctrlref=>$ctrl) {
+            if($ctrl->type == 'hidden') continue;
+            if(!$this->_form->isActivated($ctrlref)) continue;
+
+            $widget = $this->getWidget($ctrl, $this->rootWidget);
+            $widget->outputMetaContent($resp);
+        }
+    }
+
+
     public function outputAllControls() {
         $modal = $this->getOption('local');
 
