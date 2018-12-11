d58364a5d2f70e5c4513fe263f21920e3274e9ad
Michael Douchin <mdouchin@3liz.com>
2018-12-11 13:54:49 +0100

    [BUGFIX] Edition - Add compatibility for PostgreSQL 11

--- lib/jelix/plugins/db/pgsql/pgsql.dbtools.php
+++ lib/jelix/plugins/db/pgsql/pgsql.dbtools.php
@@ -226,11 +226,12 @@ class pgsqlDbTools extends jDbTools {
         $tableName = $this->_conn->prefixTable($tableName);
 
         // get table informations
-        $sql ='SELECT pg_class.oid, pg_class.relhaspkey, pg_class.relhasindex';
+        $sql ='SELECT pg_class.oid, coalesce(i.indisprimary, false) as relhaspkey, pg_class.relhasindex';
         $sql.=' FROM pg_class';
         if(!empty($schemaName)){
             $sql.= ' JOIN pg_catalog.pg_namespace n ON n.oid = pg_class.relnamespace';
         }
+        $sql.=' LEFT JOIN pg_index i ON (indrelid = pg_class.oid AND indisprimary)';
         $sql.=' WHERE relname = \''.$tableName.'\'';
         if(!empty($schemaName)){
             $sql.= ' AND n.nspname = \''.$schemaName.'\'';
