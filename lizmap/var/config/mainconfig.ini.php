91a56500023fdad1d6a54a752a1568ca741d919b
Laurent Jouanneau <dev@ljouanneau.com>
2019-01-16 15:04:49 +0100

    Use the new way into Jelix to configure jquery files

--- lizmap/var/config/mainconfig.ini.php
+++ lizmap/var/config/mainconfig.ini.php
@@ -225,9 +225,31 @@ controls.datetime.input=menulists
 controls.datetime.months.labels=numbers
 ; define the default config for datepickers in jforms
 datepicker=default
+datetimepicker=default
+
+[jquery]
+jquery = js/jquery-1.12.4.min.js
+jqueryui.js[] = js/jquery-ui-1.11.2.custom.min.js
+jqueryui.css[] = css/jquery-ui-1.8.23.custom.css
 
 [datepickers]
-default="jelix/js/jforms/datepickers/default/init.js"
+default = $jelix/js/jforms/datepickers/default/init.js
+default.js[]=js/jquery-ui-1.11.2.custom.min.js
+default.js[]=$jelix/js/jforms/datepickers/default/ui.$lang.js
+default.js[]=$jqueryPath/ui/i18n/jquery.ui.datepicker-$lang.js
+default.css[]=css/jquery-ui-1.8.23.custom.css
+
+[datetimepickers]
+default = $jelix/js/jforms/datepickers/default/init.js
+default.js[]=js/jquery-ui-1.11.2.custom.min.js
+default.js[]=$jelix/js/jforms/datepickers/default/ui.$lang.js
+default.js[]=$jqueryPath/ui/i18n/jquery.ui.datepicker-$lang.js
+default.css[]=css/jquery-ui-1.8.23.custom.css
+
+[htmleditors]
+default.engine.file[] = $jelix/wymeditor/jquery.wymeditor.js
+
+wymbasic.engine.file[] = $jelix/wymeditor/jquery.wymeditor.js
 
 [modules]
 jelix.access=1
