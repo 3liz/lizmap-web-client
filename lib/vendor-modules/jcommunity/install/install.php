3c9a1c5bf71f9ce19696f2062a0ca874be365d70
Laurent Jouanneau <dev@ljouanneau.com>
2019-01-30 23:30:05 +0100

    Update jCommunity to 1.2.2 for Jelix 1.6.21
    It uses the new method jAuth::canChangePassword()

--- lib/vendor-modules/jcommunity/install/install.php
+++ lib/vendor-modules/jcommunity/install/install.php
@@ -9,188 +9,102 @@
  */
 
 
-class jcommunityModuleInstaller extends jInstallerModule {
+class jcommunityModuleInstaller extends \Jelix\Installer\Module\Installer {
 
-    protected static $key = null;
-
-
-    function setupFramework() {
-        $authconfig = $this->config->getValue('auth','coordplugins');
-        $authconfigMaster = $this->config->getValue('auth','coordplugins', null, true);
-        $forWS = (in_array($this->entryPoint->type, array('json', 'jsonrpc', 'soap', 'xmlrpc')));
-        if (!$authconfig || ($forWS && $authconfig == $authconfigMaster)) {
-            //if ($this->entryPoint->type == 'cmdline') {
-            //    return;
-            //}
-
-            if ($forWS) {
-                $pluginIni = 'authsw.coord.ini.php';
-            }
-            else {
-                $pluginIni = 'auth.coord.ini.php';
-            }
-
-            $authconfig = dirname($this->entryPoint->configFile).'/'.$pluginIni;
-            if ($this->firstExec($authconfig)) {
-                // no configuration, let's install the plugin for the entry point
-                $this->config->setValue('auth', $authconfig, 'coordplugins');
-                $this->copyFile('var/config/'.$pluginIni, 'epconfig:'.$pluginIni);
-            }
-        }
-        else if ($this->firstExec($authconfig)) {
-            $conf = $this->getAuthConf();
-
-            if (!$this->getParameter('manualconfig')) {
-                $conf->setValue('driver', 'Db');
-                $conf->setValue('dao','jcommunity~user', 'Db');
-                $conf->setValue('form','jcommunity~account_admin', 'Db');
-                $conf->setValue('error_message', 'jcommunity~login.error.notlogged');
-                $conf->setValue('on_error_action', 'jcommunity~login:out');
-                $conf->setValue('bad_ip_action', 'jcommunity~login:out');
-                $conf->setValue('after_logout', 'jcommunity~login:index');
-                $conf->setValue('enable_after_login_override', 'on');
-                $conf->setValue('enable_after_logout_override', 'on');
-                $conf->setValue('after_login', 'jcommunity~account:show');
-                $conf->save();
-            }
-            else {
-                $daoSelector = $conf->getValue('dao', 'Db');
-                if (!$daoSelector) {
-                    $daoSelector = 'jcommunity~user';
-                    $conf->setValue('dao', $daoSelector, 'Db');
-                }
-
-                if ($daoSelector == 'jcommunity~user') {
-                    $conf->setValue('form','jcommunity~account_admin', 'Db');
-                }
-                $conf->save();
-            }
-        }
-
-        if ($this->getParameter('masteradmin')) {
-            $conf = $this->getAuthConf();
-            $conf->setValue('after_login', 'master_admin~default:index');
-            $conf->save();
-            $this->config->setValue('loginResponse', 'htmlauth', 'jcommunity');
-        }
-    }
-
-    protected function isJelix17() {
-        return method_exists('jApp', 'appConfigPath');
-    }
-
-    protected function getAuthConf() {
-        $authconfig = $this->config->getValue('auth','coordplugins');
-        if ($this->isJelix17()) {
-            $confPath = jApp::appConfigPath($authconfig);
-            $conf = new \Jelix\IniFile\IniModifier($confPath);
-        }
-        else {
-            $confPath = jApp::configPath($authconfig);
-            $conf = new jIniFileModifier($confPath);
-        }
+    protected function getAuthConf($configIni) {
+        $authconfig = $configIni->getValue('auth','coordplugins');
+        $confPath = jApp::appConfigPath($authconfig);
+        $conf = new \Jelix\IniFile\IniModifier($confPath);
         return $conf;
     }
 
-    function install() {
+    protected $daoProcessed = array();
 
-        if (self::$key === null) {
-            self::$key = jAuth::getRandomPassword(30, true);
+    function install()
+    {
+        // create random key for persistant authentication
+        $configIni = $this->getLiveConfigIni();
+        $currentKey = $configIni->getValue('persistant_crypt_key', 'coordplugin_auth');
+        if ($currentKey === 'exampleOfCryptKey' || $currentKey == '') {
+            $cryptokey = \Defuse\Crypto\Key::createNewRandomKey();
+            $key = $cryptokey->saveToAsciiSafeString();
+
+            $configIni->setValue('persistant_crypt_key', $key, 'coordplugin_auth');
         }
 
-        $this->setupFramework();
-
-        $configIni = isset($this->entryPoint->liveConfigIni)?$this->entryPoint->liveConfigIni : $this->entryPoint->localConfigIni;
-        $key = $configIni->getValue('persistant_crypt_key', 'coordplugin_auth');
-        if ($key === 'exampleOfCryptKey' || $key == '') {
-            if (isset($this->entryPoint->liveConfigIni)) {
-                $configIni->setValue('persistant_crypt_key', self::$key, 'coordplugin_auth');
-            }
-            else {
-                $configIni->getMaster()->setValue('persistant_crypt_key', self::$key, 'coordplugin_auth');
-            }
-        }
-
-        $conf = $this->getAuthConf();
-
-        $dbProfile = $conf->getValue('profile', 'Db');
-        $this->useDbProfile($dbProfile);
-
-        if ($this->firstDbExec()) {
+        foreach ($this->getParameter('eps') as $epId) {
+            $entryPoint = $this->getEntryPointsById($epId);
+            $configIni = $entryPoint->getConfigIni();
+            $conf = $this->getAuthConf($configIni);
             $daoSelector = $conf->getValue('dao', 'Db');
-
-            $mapper = new jDaoDbMapper($dbProfile);
-            $table = $mapper->createTableFromDao($daoSelector);
-
-            if ($this->getParameter('migratejauthdbusers')) {
-                $this->migrateUsers($daoSelector);
-            }
-            else {
-                $this->fillDefaultValues($daoSelector);
-
-                $sourceUserDataModule = null;
-                $sourceUserDataFile = '';
-                $defaultUsers = $this->getParameter('defaultusers');
-
-                if ($defaultUsers &&
-                    is_string($defaultUsers) &&
-                    preg_match("/^([a-zA-Z0-9_\.]+)~([a-zA-Z0-9_\.]+)$/", $defaultUsers, $m)
-                ) {
-                    list(,$sourceUserDataModule,$sourceUserDataFile) = $m;
-                }
-                else if ($this->getParameter('defaultuser')) {
-                    $sourceUserDataFile = 'defaultuser.json';
-                }
-
-                if ($sourceUserDataFile) {
-                    require_once(JELIX_LIB_PATH.'auth/jAuth.class.php');
-                    $confIni = parse_ini_file($conf->getFileName(), true);
-                    $authConfig = jAuth::loadConfig($confIni);
-                    $driverConfig = $authConfig[$authConfig['driver']];
-                    if ($authConfig['driver'] == 'Db' ||
-                        (isset($driverConfig['compatiblewithdb']) &&
-                            $driverConfig['compatiblewithdb'])
-                    ) {
-                        require_once(JELIX_LIB_PATH.'plugins/auth/db/db.auth.php');
-                        $driver = new dbAuthDriver($driverConfig);
-                        $this->insertUsers($driver, $daoSelector, $dbProfile, $sourceUserDataModule, $sourceUserDataFile);
-                    }
-                }
+            if (!isset($this->daoProcessed[$daoSelector])) {
+                $this->daoProcessed[$daoSelector] = true;
+                $this->_installForEntrypoint($conf);
             }
         }
 
-
-        if ($this->firstExec('preferences') && $this->getParameter('usejpref')) {
-            if ($this->firstExec('acl2') && class_exists('jAcl2DbManager')) {
+        if ($this->getParameter('usejpref')) {
+            if (class_exists('jAcl2DbManager')) {
                 jAcl2DbManager::addSubjectGroup('jcommunity.admin', 'jcommunity~prefs.admin.jcommunity');
                 jAcl2DbManager::addSubject('jcommunity.prefs.change', 'jcommunity~prefs.admin.prefs.change', 'jprefs.prefs.management');
                 jAcl2DbManager::addRight('admins', 'jcommunity.prefs.change'); // for admin group
             }
-            if (!$this->config->getValue('disableJPref', 'jcommunity')) {
-                if ($this->isJelix17()) {
-                    $prefIni = new \Jelix\IniFile\IniModifier(__DIR__.'/prefs.ini');
-                    $prefFile = jApp::appConfigPath('preferences.ini.php');
-                    if (file_exists($prefFile)) {
-                        $mainPref = new \Jelix\IniFile\IniModifier($prefFile);
-                        //import this way to not erase changed value.
-                        $prefIni->import($mainPref);
-                    }
-                }
-                else {
-                    $prefIni = new jIniFileModifier(__DIR__.'/prefs.ini');
-                    $prefFile = jApp::configPath('preferences.ini.php');
-                    if (file_exists($prefFile)) {
-                        $mainPref = new jIniFileModifier($prefFile);
-                        //import this way to not erase changed value.
-                        $prefIni->import($mainPref);
-                    }
-                }
-                $prefIni->saveAs($prefFile);
-            }
         }
     }
 
+    protected function _installForEntrypoint(jInstallerEntryPoint2 $entryPoint, $authConf) {
 
+        $dbProfile = $authConf->getValue('profile', 'Db');
+        $this->useDbProfile($dbProfile);
+
+        $daoSelector = $authConf->getValue('dao', 'Db');
+
+        $mapper = new jDaoDbMapper($dbProfile);
+        $table = $mapper->createTableFromDao($daoSelector);
+
+        if ($this->getParameter('migratejauthdbusers')) {
+            $this->migrateUsers($daoSelector);
+        }
+        else {
+            $this->fillDefaultValues($daoSelector);
+
+            $sourceUserDataModule = null;
+            $sourceUserDataFile = '';
+            $defaultUsers = $this->getParameter('defaultusers');
+
+            if ($defaultUsers &&
+                is_string($defaultUsers) &&
+                preg_match("/^([a-zA-Z0-9_\.]+)~([a-zA-Z0-9_\.]+)$/", $defaultUsers, $m)
+            ) {
+                list(,$sourceUserDataModule,$sourceUserDataFile) = $m;
+            }
+            else if ($this->getParameter('defaultuser')) {
+                $sourceUserDataFile = 'defaultuser.json';
+            }
+
+            if ($sourceUserDataFile) {
+                require_once(JELIX_LIB_PATH.'auth/jAuth.class.php');
+                $confIni = parse_ini_file($authConf->getFileName(), true);
+                $authConfig = jAuth::loadConfig($confIni);
+                $driverConfig = $authConfig[$authConfig['driver']];
+                if ($authConfig['driver'] == 'Db' ||
+                    (isset($driverConfig['compatiblewithdb']) &&
+                        $driverConfig['compatiblewithdb'])
+                ) {
+                    require_once(JELIX_LIB_PATH.'plugins/auth/db/db.auth.php');
+                    $driver = new dbAuthDriver($driverConfig);
+                    $this->insertUsers($entryPoint, $driver, $daoSelector, $dbProfile, $sourceUserDataModule, $sourceUserDataFile);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Migrate users from the jlx_user table to the jcommunity user table
+     * @param string $daoSelectorStr dao selector of the jcommunity table
+     * @throws jException
+     */
     protected function migrateUsers($daoSelectorStr) {
         $dao = jDao::get($daoSelectorStr);
         $tableProp = $dao->getTables()[$dao->getPrimaryTable()];
@@ -264,10 +178,10 @@ class jcommunityModuleInstaller extends jInstallerModule {
         }
     }
 
-    protected function insertUsers($driver, $daoSelector, $dbProfile, $module, $relativeSourcePath) {
+    protected function insertUsers(jInstallerEntryPoint2 $entryPoint, $driver, $daoSelector, $dbProfile, $module, $relativeSourcePath) {
 
         if ($module) {
-            $conf = $this->entryPoint->config->_modulesPathList;
+            $conf = $entryPoint->getConfigObj()->_modulesPathList;
             if (!isset($conf[$module])) {
                 throw new Exception('insertUsers : invalid module name');
             }
