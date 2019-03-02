ef8e0838b38f799a82730d4205a53984d95cb433
Laurent Jouanneau <dev@ljouanneau.com>
2019-03-02 09:45:20 +0100

    Infrastructure for tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(__DIR__.'/../lizmap/application.init.php');
+require_once(LIB_PATH.'jelix-tests/classes/junittestcase.class.php');
+require_once(LIB_PATH.'jelix-tests/classes/junittestcasedb.class.php');
+
+jApp::setEnv('lizmaptests');
+if (file_exists(jApp::tempPath())) {
+    jAppManager::clearTemp(jApp::tempPath());
+}
