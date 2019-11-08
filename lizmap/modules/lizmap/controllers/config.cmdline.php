ef5033a5e2c6b97165d1f9ebaf63e514e4d1312d
Laurent Jouanneau <dev@ljouanneau.com>
2019-11-08 18:59:54 +0100

    New scripts to migrate lizmap data from sqlite databases to pgsql/mysql

---
+++ lizmap/modules/lizmap/controllers/config.cmdline.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @copyright 2019 3liz
+ *
+ * @see      http://3liz.com
+ *
+ * @license   MPL-2.0
+ */
+
+
+class configCtrl extends jControllerCmdLine
+{
+    /**
+     * Options to the command line
+     *  'method_name' => array('-option_name' => true/false)
+     * true means that a value should be provided for the option on the command line.
+     */
+    protected $allowed_options = array(
+    );
+
+    /**
+     * Parameters for the command line
+     * 'method_name' => array('parameter_name' => true/false)
+     * false means that the parameter is optional. All parameters which follow an optional parameter
+     * is optional.
+     */
+    protected $allowed_parameters = array(
+        'migratelog' => array(
+        ),
+        'migrateusers' => array(
+        ),
+    );
+
+    /**
+     * Help.
+     */
+    public $help = array(
+
+        'migratelog' => 'Migrate log data from a sqlite database to the current database
+
+        Use :
+        php lizmap/scripts/script.php lizmap~config:migratelog
+
+        ',
+        'migrateusers' => 'Migrate users data from a sqlite database to the current database (experimental)
+
+        Use :
+        php lizmap/scripts/script.php lizmap~config:migrateusers
+
+        ',
+    );
+
+
+    /**
+     * Migrate log data from a sqlite database to the current database
+     */
+    public function migratelog()
+    {
+        /** @var jResponseCmdline $rep */
+        $rep = $this->getResponse();
+        $logMigrator = new \Lizmap\Logger\MigratorFromSqlite();
+        try {
+            $res = $logMigrator->migrateLog();
+        }
+        catch(\UnexpectedValueException $e) {
+            $rep->addContent("Error during the migration: ".$e->getMessage()."\n");
+            $rep->setExitCode(1);
+            return $rep;
+        }
+
+        switch ($res) {
+            case $logMigrator::MIGRATE_RES_ALREADY_MIGRATED:
+                $rep->addContent("It seems already migrated, there are some data into logCounter or logDetail table\n");
+                break;
+            case $logMigrator::MIGRATE_RES_OK:
+                $rep->addContent("Migration done\n");
+                break;
+            case 0:
+                $rep->addContent("Unknown error\n");
+                break;
+            default:
+                $rep->addContent("Unknown result\n");
+        }
+        return $rep;
+    }
+
+
+    /**
+     * Migrate users data from a sqlite database to the current database
+     */
+    public function migrateusers()
+    {
+        /** @var jResponseCmdline $rep */
+        $rep = $this->getResponse();
+        $logMigrator = new \Lizmap\Users\MigratorFromSqlite();
+        try {
+            $res = $logMigrator->migrateUsersAndRights();
+        }
+        catch(\UnexpectedValueException $e) {
+            $rep->addContent("Error during the migration: ".$e->getMessage()."\n");
+            $rep->setExitCode(1);
+            return $rep;
+        }
+
+        switch ($res) {
+            case $logMigrator::MIGRATE_RES_ALREADY_MIGRATED:
+                $rep->addContent("It seems already migrated, there are some data into existing users tables\n");
+                break;
+            case $logMigrator::MIGRATE_RES_OK:
+                $rep->addContent("Migration done\n");
+                break;
+            case 0:
+                $rep->addContent("Unknown error\n");
+                break;
+            default:
+                $rep->addContent("Unknown result\n");
+        }
+        return $rep;
+    }
+}
