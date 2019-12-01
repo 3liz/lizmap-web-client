be05e6ec032e1dc19e56f1e5cbdacf2abe003c47
rldhont <rldhont@gmail.com>
2019-12-01 16:07:36 +0100

    [Bugfix] Permalink: Decode url parameters

--- lizmap/www/js/map.js
+++ lizmap/www/js/map.js
@@ -3071,7 +3071,7 @@ var lizMap = function() {
     if (window.location.search.length > 1) {
       for (var aItKey, nKeyId = 0, aCouples = window.location.search.substr(1).split("&"); nKeyId < aCouples.length; nKeyId++) {
         aItKey = aCouples[nKeyId].split("=");
-        oParametre[unescape(aItKey[0])] = aItKey.length > 1 ? unescape(aItKey[1]) : "";
+        oParametre[decodeURIComponent(aItKey[0])] = aItKey.length > 1 ? decodeURIComponent(aItKey[1]) : "";
       }
     }
     return oParametre;
