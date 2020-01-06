102354a5a21000a7e975064e4d2c6efedfc288a8
Nicolas Boisteault <nboisteault@3liz.com>
2020-01-06 17:10:32 +0100

    Add layers edition part

--- assets/src/components/Geolocation.js
+++ assets/src/components/Geolocation.js
@@ -15,26 +15,29 @@ export default class Geolocation extends HTMLElement {
         // Display
         const myTemplate = () => html`
         <div class="menu-content">
-    <div id=geolocation-coords><small>${mainLizmap.geolocation.position ? mainLizmap.geolocation.position[0].toString() + ', ' + mainLizmap.geolocation.position[1].toString() : ''}</small></div>
+            <div id=geolocation-coords><small>${mainLizmap.geolocation.position ? mainLizmap.geolocation.position[0].toString() + ', ' + mainLizmap.geolocation.position[1].toString() : ''}</small></div>
             <div class="button-bar">
                 <button id="geolocation-stop" class="btn btn-small btn-primary" @click=${ () => mainLizmap.geolocation.toggleTracking()}><span class="icon"></span>${mainLizmap.geolocation.isTracking ? 'Stop' : 'Start'}</button>
                 <button id="geolocation-center" class="btn btn-small btn-primary" @click=${ () => mainLizmap.geolocation.center()} ?disabled=${!mainLizmap.geolocation.isTracking | mainLizmap.geolocation.isBind}><span class="icon"></span>Center</button>
                 <button id="geolocation-bind" class="btn btn-small btn-primary ${mainLizmap.geolocation.isBind ? 'active' : ''}" @click=${ () => mainLizmap.geolocation.toggleBind()} ?disabled=${!mainLizmap.geolocation.isTracking}><span class="icon"></span>Stay centered</button>
             </div>
-            <div id="geolocation-edition-group" style="display:none; margin-top:5px;">
-                <table>
-                    <tr>
-                        <td style="vertical-align: top;">
-                            <span id="geolocation-edition-title" style="font-weight:bold"></span>
-                        </td>
-                        <td>
-                            <label id="geolocation-edition-linked-label" class="checkbox"><input id="geolocation-edition-linked" type="checkbox" value="1" disabled="disabled"></label>
-                            <button id="geolocation-edition-add" class="btn btn-small btn-primary" disabled="disabled"><span class="icon"></span></button>
-                            <button id="geolocation-edition-submit" class="btn btn-small btn-primary" disabled="disabled"><span class="icon"></span></button>
-                        </td>
-                    </tr>
-                </table>
-            </div>
+            ${mainLizmap.hasEditionLayers
+                ? html`<div id="geolocation-edition-group" style="display:none; margin-top:5px;">
+                    <table>
+                        <tr>
+                            <td style="vertical-align: top;">
+                                <span id="geolocation-edition-title" style="font-weight:bold">Edition</span>
+                            </td>
+                            <td>
+                                <label id="geolocation-edition-linked-label" class="checkbox"><input id="geolocation-edition-linked" type="checkbox" value="1" disabled="disabled">linked to geolocation</label>
+                                <button id="geolocation-edition-add" class="btn btn-small btn-primary" disabled="disabled"><span class="icon"></span>Add</button>
+                                <button id="geolocation-edition-submit" class="btn btn-small btn-primary" disabled="disabled"><span class="icon"></span>Finalize</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>`
+                : ''
+            }
         </div>`;
 
         mainEventDispatcher.addListener(
