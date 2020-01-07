9bd3d5704a7ca809e99b36da5027b2f70994d64b
Nicolas Boisteault <nboisteault@3liz.com>
2020-01-07 15:27:10 +0100

    Refactoring

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,3 +1,7 @@
 import Geolocation from './components/Geolocation.js';
 
-window.customElements.define('lizmap-geolocation', Geolocation);
+lizMap.events.on({
+    uicreated: () => {
+        window.customElements.define('lizmap-geolocation', Geolocation);
+    }
+});
