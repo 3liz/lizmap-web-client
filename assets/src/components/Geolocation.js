3d9ef7c24616d643cda7747a5a1579ad07d8e51f
Nicolas Boisteault <nboisteault@3liz.com>
2020-01-14 15:27:00 +0100

    Remove edition part in geolocation tool

--- assets/src/components/Geolocation.js
+++ assets/src/components/Geolocation.js
@@ -19,22 +19,6 @@ export default class Geolocation extends HTMLElement {
                 <button id="geolocation-center" class="btn btn-small btn-primary" @click=${ () => mainLizmap.geolocation.center()} ?disabled=${!mainLizmap.geolocation.isTracking | mainLizmap.geolocation.isBind}><span class="icon"></span>Center</button>
                 <button id="geolocation-bind" class="btn btn-small btn-primary ${mainLizmap.geolocation.isBind ? 'active' : ''}" @click=${ () => mainLizmap.geolocation.toggleBind()} ?disabled=${!mainLizmap.geolocation.isTracking}><span class="icon"></span>Stay centered</button>
             </div>
-            ${mainLizmap.hasEditionLayers
-            ? html`
-            <div id="geolocation-edition-group" class="${mainLizmap.lizmapEditionDrawFeatureActivated ? '' : 'hide'}" style="margin-top:5px;">
-                <div>
-                    <strong style="vertical-align: top;">Edition&nbsp;:</strong>
-                    <div style="display:inline-block">
-                        <button class="btn btn-small btn-primary ${mainLizmap.geolocation.hasEditionLinked ? 'active' : ''}" @click=${() => mainLizmap.geolocation.toggleEditionLinked()} ?disabled=${!mainLizmap.geolocation.isTracking}>Linked</button>
-                        <div style="padding-top: 5px;">
-                            <button id="geolocation-edition-add" class="btn btn-small btn-primary ${mainLizmap.lizmapEditionLayerGeometry === "point" ? 'hide' : ''}" ?disabled=${!mainLizmap.geolocation.hasEditionLinked}><span class="icon"></span>Add</button>
-                            <button id="geolocation-edition-submit" class="btn btn-small btn-primary" ?disabled=${!mainLizmap.geolocation.hasEditionLinked}><span class="icon"></span>Finalize</button>
-                        </div>
-                    </div>
-                </div>
-            </div>`
-            : ''
-            }
         </div>`;
 
         render(mainTemplate(), this);
@@ -58,20 +42,6 @@ export default class Geolocation extends HTMLElement {
                 render(positionTemplate(), document.getElementById('geolocation-coords'));
             },
             'geolocation.position'
-        );
-
-        mainEventDispatcher.addListener(
-            () => {
-                render(mainTemplate(), this);
-            },
-            'lizmapEditionDrawFeatureChanged'
-        );
-
-        mainEventDispatcher.addListener(
-            () => {
-                render(mainTemplate(), this);
-            },
-            'geolocation.hasEditionLinked'
         );
     }
 
