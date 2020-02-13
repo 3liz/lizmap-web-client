f591465b5f4a04a9fdead83787919f959ecf19da
Nicolas Boisteault <nboisteault@3liz.com>
2020-02-13 15:46:19 +0100

    Add accuracy mode in geolocation survey

--- assets/src/components/GeolocationSurvey.js
+++ assets/src/components/GeolocationSurvey.js
@@ -8,11 +8,20 @@ export default class GeolocationSurvey extends HTMLElement {
 
     connectedCallback() {
         const mainTemplate = () => html`
-        <div class="control-group ${mainLizmap.geolocation.isLinkedToEdition && ['line', 'polygon'].includes(mainLizmap.edition.layerGeometry)  ? '' : 'hide'}">
-            <label class="jforms-label control-label"><button class="btn btn-primary ${mainLizmap.geolocationSurvey.distanceMode ? 'active' : ''}" @click=${() => mainLizmap.geolocationSurvey.toggleDistanceMode()}>Distance</button></label>
-            <div class="controls">
-                <input id="geolocation-survey-distance" class="jforms-ctrl-input input-small" type="number" min="0" @change=${ (event) => mainLizmap.geolocationSurvey.distanceLimit = parseInt(event.target.value)} ?disabled=${!mainLizmap.geolocationSurvey.distanceMode}>
-                ${mainLizmap.edition.lastSegmentLength}
+        <div class="${mainLizmap.geolocation.isLinkedToEdition && ['line', 'polygon'].includes(mainLizmap.edition.layerGeometry) ? '' : 'hide'}">
+            <div class="control-group">
+                <label class="jforms-label control-label"><button class="btn btn-primary ${mainLizmap.geolocationSurvey.distanceMode ? 'active' : ''}" @click=${() => mainLizmap.geolocationSurvey.toggleDistanceMode()}>Distance</button></label>
+                <div class="controls">
+                    <input class="jforms-ctrl-input input-small" type="number" min="0" @change=${ (event) => mainLizmap.geolocationSurvey.distanceLimit = parseInt(event.target.value)} ?disabled=${!mainLizmap.geolocationSurvey.distanceMode}>
+                    ${mainLizmap.edition.lastSegmentLength}
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="jforms-label control-label"><button class="btn btn-primary ${mainLizmap.geolocationSurvey.accuracyMode ? 'active' : ''}" @click=${() => mainLizmap.geolocationSurvey.toggleAccuracyMode()}>Accuracy</button></label>
+                <div class="controls">
+                    <input class="jforms-ctrl-input input-small" type="number" min="0" @change=${ (event) => mainLizmap.geolocationSurvey.accuracyLimit = parseInt(event.target.value)} ?disabled=${!mainLizmap.geolocationSurvey.accuracyMode}>
+                    ${mainLizmap.geolocation.accuracy}
+                </div>
             </div>
         </div>`;
 
@@ -39,6 +48,13 @@ export default class GeolocationSurvey extends HTMLElement {
                 render(mainTemplate(), this);
             },
             'geolocationSurvey.distanceMode'
+        );
+
+        mainEventDispatcher.addListener(
+            () => {
+                render(mainTemplate(), this);
+            },
+            'geolocationSurvey.accuracyMode'
         );
     }
 
