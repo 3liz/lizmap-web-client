e7467026d528d2d72a4f5263042d9d4373315388
Nicolas Boisteault <nboisteault@3liz.com>
2020-02-13 12:03:28 +0100

    Play sound when drawing points in survey mode

--- assets/src/modules/GeolocationSurvey.js
+++ assets/src/modules/GeolocationSurvey.js
@@ -7,12 +7,30 @@ export default class GeolocationSurvey {
         this.distanceLimit = 0;
         this._distanceMode = false;
 
-        // Draw automatically a point when lastSegmentLength >= distanceLimit
+        this._beep = new AudioContext();
+
+        // Draw automatically a point and beep when lastSegmentLength >= distanceLimit
         mainEventDispatcher.addListener(
             () => {
                 if (this.distanceMode && mainLizmap.edition.lastSegmentLength >= this.distanceLimit){
+                    // Draw
                     const node = mainLizmap.edition.drawControl.handler.point.geometry;
                     mainLizmap.edition.drawControl.handler.insertXY(node.x, node.y);
+
+                    // Beep
+                    const freq = 520;
+                    const duration = 0.2;
+                    const volume = 1;
+
+                    const v = this._beep.createOscillator();
+                    const u = this._beep.createGain();
+                    v.connect(u);
+                    v.frequency.value = freq;
+                    v.type = "square";
+                    u.connect(this._beep.destination);
+                    u.gain.value = volume;
+                    v.start(this._beep.currentTime);
+                    v.stop(this._beep.currentTime + duration);
                 }
             },
             'edition.lastSegmentLength'
