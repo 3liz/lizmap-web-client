52837aa16c1ae265ffdc4dd94335aaa7ec047684
Nicolas Boisteault <nboisteault@3liz.com>
2020-02-13 15:46:19 +0100

    Add accuracy mode in geolocation survey

--- assets/src/modules/GeolocationSurvey.js
+++ assets/src/modules/GeolocationSurvey.js
@@ -5,33 +5,38 @@ export default class GeolocationSurvey {
     constructor() {
 
         this.distanceLimit = 0;
+        this.accuracyLimit = 0;
         this._distanceMode = false;
+        this._accuracyMode = false;
 
         this._beep = new AudioContext();
 
         // Draw automatically a point and beep when lastSegmentLength >= distanceLimit
         mainEventDispatcher.addListener(
             () => {
-                if (this.distanceMode && mainLizmap.edition.lastSegmentLength >= this.distanceLimit){
-                    // Draw
-                    const node = mainLizmap.edition.drawControl.handler.point.geometry;
-                    mainLizmap.edition.drawControl.handler.insertXY(node.x, node.y);
+                if (!this.accuracyMode || (this.accuracyMode && mainLizmap.geolocation.accuracy <= this.accuracyLimit)){
+                    if (this.distanceMode && mainLizmap.edition.lastSegmentLength >= this.distanceLimit) {
+                        // Draw
+                        const node = mainLizmap.edition.drawControl.handler.point.geometry;
+                        mainLizmap.edition.drawControl.handler.insertXY(node.x, node.y);
 
-                    // Beep
-                    const freq = 520;
-                    const duration = 0.2;
-                    const volume = 1;
+                        // Beep
+                        const freq = 520;
+                        const duration = 0.2;
+                        const volume = 1;
 
-                    const v = this._beep.createOscillator();
-                    const u = this._beep.createGain();
-                    v.connect(u);
-                    v.frequency.value = freq;
-                    v.type = "square";
-                    u.connect(this._beep.destination);
-                    u.gain.value = volume;
-                    v.start(this._beep.currentTime);
-                    v.stop(this._beep.currentTime + duration);
+                        const v = this._beep.createOscillator();
+                        const u = this._beep.createGain();
+                        v.connect(u);
+                        v.frequency.value = freq;
+                        v.type = "square";
+                        u.connect(this._beep.destination);
+                        u.gain.value = volume;
+                        v.start(this._beep.currentTime);
+                        v.stop(this._beep.currentTime + duration);
+                    }
                 }
+
             },
             'edition.lastSegmentLength'
         );
@@ -45,5 +50,15 @@ export default class GeolocationSurvey {
         this._distanceMode = !this._distanceMode;
 
         mainEventDispatcher.dispatch('geolocationSurvey.distanceMode');
+    }
+
+    get accuracyMode() {
+        return this._accuracyMode;
+    }
+
+    toggleAccuracyMode() {
+        this._accuracyMode = !this._accuracyMode;
+
+        mainEventDispatcher.dispatch('geolocationSurvey.accuracyMode');
     }
 }
