d7ec26c274331bac8826509c28ca758cbc411202
Laurent Jouanneau <dev@ljouanneau.com>
2020-02-18 14:17:31 +0100

    [demo] remove the demo page from the wizard
    Since it needs a manual install of the module, we should remove it.
    
    We could download automatically but it could take too much memory
    and may need specific rights int lizmap/lizmap-modules..
    
    Refs #1022

--- lizmap/install/wizardpages/demo/demo.fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$locales = array(
-
-    'title'=>'Installer la demo',
-    'title.demo'=>'Installation de la demo',
-    'description.demo'=>'Voulez vous installer les données et la carte de la demo fournie avec Lizmap ?',
-    'label.installdemo'=> 'Installer la démo'
-);
