c5798d6af7195322f36c14cb26ab5f0ec5886ed0
rldhont <rldhont@gmail.com>
2020-02-24 17:31:04 +0100

    [Bugfix] Form: Use checkbox for boolean field and display labels

--- lizmap/plugins/formwidget/checkbox_htmlbootstrap/checkbox_htmlbootstrap.formwidget.php
+++ lizmap/plugins/formwidget/checkbox_htmlbootstrap/checkbox_htmlbootstrap.formwidget.php
@@ -15,26 +15,70 @@ class checkbox_htmlbootstrapFormWidget extends checkbox_htmlFormWidget
 
     public function outputControl()
     {
-        $this->labelAttributes['class'] = 'checkbox';
+
+        if($this->ctrl->valueLabelOnCheck !== '' or $this->ctrl->valueLabelOnUncheck !== '') {
+            $this->labelAttributes['class'] = 'radio jforms-radio';
+        } else {
+            $this->labelAttributes['class'] = 'checkbox';
+        }
         $attrLabel = $this->getLabelAttributes(true);
-        echo '<label class="',$attrLabel['class'],'" for="',$this->getId(),'"',$attrLabel['idLabel'],$attrLabel['hint'],'>';
 
         $attr = $this->getControlAttributes();
 
-        if ($this->ctrl->valueOnCheck == $this->getValue()) {
-            $attr['checked'] = 'checked';
+        if($this->ctrl->valueLabelOnCheck !== '' or $this->ctrl->valueLabelOnUncheck !== '') {
+
+            echo '<label class="',$attrLabel['class'],'" ',$attrLabel['idLabel'],$attrLabel['hint'],'>';
+
+            if($this->ctrl->valueOnCheck == $this->getValue()){
+                $attr['checked'] = "checked";
+            }
+            $attr['value'] = $this->ctrl->valueOnCheck;
+            $attr['type'] = 'radio';
+            // attribute readonly is not enough to make checkboxes readonly. Note that value won't be sent by submit but it is not a problem as it is readonly
+            if (array_key_exists('readonly', $attr)) {
+                $attr['disabled'] = 'disabled';
+            }
+
+            echo '<input';
+            $this->_outputAttr($attr);
+            echo '/>';
+            echo htmlspecialchars($this->ctrl->valueLabelOnCheck);
+            echo "</label>\n";
+
+
+            echo '<label class="',$attrLabel['class'],'" ',$attrLabel['idLabel'],$attrLabel['hint'],'>';
+
+            if($this->ctrl->valueOnCheck == $this->getValue()){
+                unset($attr['checked']);
+            }
+            else if($this->ctrl->valueOnUncheck == $this->getValue()){
+                $attr['checked'] = "checked";
+            }
+            $attr['value'] = $this->ctrl->valueOnUncheck;
+
+            echo '<input';
+            $this->_outputAttr($attr);
+            echo '/>';
+            echo htmlspecialchars($this->ctrl->valueLabelOnUncheck);
+            echo "</label>\n";
+        } else {
+            echo '<label class="',$attrLabel['class'],'" for="',$this->getId(),'"',$attrLabel['idLabel'],$attrLabel['hint'],'>';
+
+            if ($this->ctrl->valueOnCheck == $this->getValue()) {
+                $attr['checked'] = 'checked';
+            }
+            $attr['value'] = $this->ctrl->valueOnCheck;
+            $attr['type'] = 'checkbox';
+            // attribute readonly is not enough to make checkboxes readonly. Note that value won't be sent by submit but it is not a problem as it is readonly
+            if (array_key_exists('readonly', $attr)) {
+                $attr['disabled'] = 'disabled';
+            }
+            echo '<input';
+            $this->_outputAttr($attr);
+            echo '/>';
+            echo htmlspecialchars($this->ctrl->label), $attrLabel['reqHtml'];
+            echo "</label>\n";
         }
-        $attr['value'] = $this->ctrl->valueOnCheck;
-        $attr['type'] = 'checkbox';
-        // attribute readonly is not enough to make checkboxes readonly. Note that value won't be sent by submit but it is not a problem as it is readonly
-        if (array_key_exists('readonly', $attr)) {
-            $attr['disabled'] = 'disabled';
-        }
-        echo '<input';
-        $this->_outputAttr($attr);
-        echo '/>';
-        echo htmlspecialchars($this->ctrl->label), $attrLabel['reqHtml'];
-        echo "</label>\n";
         $this->outputJs();
     }
 }
