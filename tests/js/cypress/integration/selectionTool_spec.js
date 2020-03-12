c115a6b4ff53b4602b010be00aaa6d9728117d1d
Nicolas Boisteault <nboisteault@3liz.com>
2020-03-12 21:52:33 +0100

    [JS-tests] Add test for invert. Use stub to override LWC configuration

--- tests/js/cypress/integration/selectionTool_spec.js
+++ tests/js/cypress/integration/selectionTool_spec.js
@@ -1,13 +1,19 @@
 describe('Selection tool', function () {
     before(function () {
         // runs once before the first test in this block
-        cy.visit('/')
+        cy.visit('/index.php/view/map/?repository=montpellier&project=montpellier')
         // Todo wait for map to be fully loaded
         cy.wait(3000)
+
+        cy.get('#button-selectiontool').click()
     })
 
-    it('open selection tool', function () {
-        cy.get('#button-selectiontool').click()
+    // Refresh before
+    beforeEach(function () {
+        cy.get('.selectiontool-type-refresh').click()
+    })
+
+    it('opens selection tool', function () {
         cy.get('#selectiontool').should('be.visible')
 
         cy.get('.selectiontool-unselect').should('have.attr', 'disabled')
@@ -23,7 +29,10 @@ describe('Selection tool', function () {
     //     .trigger('mouseup')
     // })
 
-    it('selects one feature with polygon', function () {
+    it('selects one feature then two more features then unselects one with polygon', function () {
+        // Select one feature...
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('tramstop')
+
         cy.get('.selectiontool-query-polygon').click()
 
         cy.get('#map')
@@ -39,9 +48,8 @@ describe('Selection tool', function () {
 
         cy.get('.selectiontool-unselect').should('not.have.attr', 'disabled')
         cy.get('.selectiontool-filter').should('not.have.attr', 'disabled')
-    })
 
-    it('selects two more features with polygon', function () {
+        // ...then two more features...
         cy.get('.selectiontool-query-polygon').click()
         cy.get('.selectiontool-type-plus').click()
 
@@ -55,9 +63,8 @@ describe('Selection tool', function () {
 
             expect(text).to.match(/^3/)
         })
-    })
 
-    it('unselects one features with polygon', function () {
+        // ...then unselects one feature
         cy.get('.selectiontool-query-polygon').click()
         cy.get('.selectiontool-type-minus').click()
 
@@ -73,7 +80,18 @@ describe('Selection tool', function () {
         })
     })
 
-    it('unselects all feature', function () {
+    it('selects two features then unselects all feature', function () {
+        // Select two feature...
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('tramstop')
+
+        cy.get('.selectiontool-query-polygon').click()
+
+        cy.get('#map')
+            .click(750, 180)
+            .click(700, 210)
+            .dblclick(750, 220)
+
+        // ..then unselects all
         cy.get('.selectiontool-unselect').click()
 
         cy.get('.selectiontool-results').should(($div) => {
@@ -81,6 +99,46 @@ describe('Selection tool', function () {
 
             expect(text).not.to.match(/^[0-9]/)
         })
+    })
+
+    it('selects one feature then revert selection', function () {
+        // Select one feature...
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('points of interest')
+
+        cy.get('.selectiontool-query-polygon').click()
+
+        cy.get('#map')
+            .click(650, 200)
+            .click(600, 230)
+            .dblclick(650, 240)
+
+        // ...then invert selection
+        cy.get('.selectiontool-invert').click()
+
+        cy.get('.selectiontool-results').should(($div) => {
+            const text = $div.text()
+
+            expect(text).to.match(/^5/)
+        })
+
+    })
+
+    it('selects multiple selectable layers', function () {
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selectable-layers')
+
+        cy.get('.selectiontool-query-polygon').click()
+
+        cy.get('#map')
+            .click(750, 150)
+            .click(700, 200)
+            .dblclick(800, 300)
+
+        cy.get('.selectiontool-results').should(($div) => {
+            const text = $div.text()
+
+            expect(text).to.match(/^10/)
+        })
+
     })
 
     it('filters one feature from attribute table', function () {
@@ -108,6 +166,34 @@ describe('Selection tool', function () {
 
         cy.get('.selectiontool-filter').should('not.have.attr', 'disabled')
         cy.get('.selectiontool-filter').should('have.class', 'active')
+
+    })
+
+    it('selects multiple selectable and visible layers', function () {
+        // Stub getProjectConfig request to have a new Lizmap configuration with less visible layers
+        cy.server()
+        cy.route('GET', 'index.php/lizmap/service/getProjectConfig?repository=montpellier&project=montpellier', 'fixture:getProjectConfig.json')
+        // runs once before the first test in this block
+        cy.visit('/index.php/view/map/?repository=montpellier&project=montpellier')
+        // Todo wait for map to be fully loaded
+        cy.wait(3000)
+
+        cy.get('#button-selectiontool').click()
+
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selectable-visible-layers')
+
+        cy.get('.selectiontool-query-polygon').click()
+
+        cy.get('#map')
+            .click(750, 150)
+            .click(700, 200)
+            .dblclick(800, 300)
+
+        cy.get('.selectiontool-results').should(($div) => {
+            const text = $div.text()
+
+            expect(text).to.match(/^8/)
+        })
 
     })
 })
