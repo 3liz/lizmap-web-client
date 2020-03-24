575b61e7b0028ed3f93a3c5f2257eeb3a168e2a2
Laurent Jouanneau <dev@ljouanneau.com>
2020-03-24 11:32:29 +0100

    Upgrade jelix to 1.6.27

--- lib/jelix/installer/jInstallerComponentBase.class.php
+++ lib/jelix/installer/jInstallerComponentBase.class.php
@@ -11,7 +11,7 @@
 require_once (JELIX_LIB_UTILS_PATH."jVersionComparator.class.php");
 
 /**
-* a class to install a component (module or plugin) 
+* a class to install a component (module or plugin)
 * @package     jelix
 * @subpackage  installer
 * @since 1.2
@@ -28,7 +28,7 @@ abstract class jInstallerComponentBase {
      * it should be set by the constructor
      */
     protected $path = '';
-    
+
     /**
      * @var string version of the current sources of the module
      */
@@ -38,17 +38,17 @@ abstract class jInstallerComponentBase {
      * @var string the date of the current sources of the module
      */
     protected $sourceDate = '';
-    
+
     /**
      * @var string the namespace of the xml file
      */
     protected $identityNamespace = '';
-    
+
     /**
      * @var string the expected name of the root element in the xml file
      */
     protected $rootName = '';
-    
+
     /**
      * @var string the name of the xml file
      */
@@ -58,12 +58,12 @@ abstract class jInstallerComponentBase {
      * @var jInstaller the main installer controller
      */
     protected $mainInstaller = null;
-    
+
     /**
      * list of dependencies of the module
      */
     public $dependencies = array();
-    
+
     /**
      * @var string the minimum version of jelix for which the component is compatible
      */
@@ -157,11 +157,11 @@ abstract class jInstallerComponentBase {
     /**
      * return the list of objects which are responsible to upgrade the component
      * from the current installed version of the component.
-     * 
+     *
      * this method should be called after verifying and resolving
      * dependencies. Needed components (modules or plugins) should be
      * installed/upgraded before calling this method
-     * 
+     *
      * @param jInstallerEntryPoint $ep the entry point
      * @throw jInstallerException  if an error occurs during the install.
      * @return jIInstallerComponent[]
@@ -186,7 +186,7 @@ abstract class jInstallerComponentBase {
         $this->identityReaded = true;
         $this->readIdentity();
     }
-    
+
     /**
      * read the identity file
      * @throws \Exception
@@ -219,7 +219,7 @@ abstract class jInstallerComponentBase {
 
     protected function readDependencies($xml) {
 
-      /*  
+      /*
 <module xmlns="http://jelix.org/ns/module/1.0">
     <info id="jelix@modules.jelix.org" name="jelix" createdate="">
         <version stability="stable" date="">1.0</version>
@@ -249,63 +249,83 @@ abstract class jInstallerComponentBase {
                     // lets ignore tags introduced for jelix 1.7, like <choice> or <conflicts>
                     continue;
                 }
-
-                $minversion = isset($dependency['minversion'])?(string)$dependency['minversion']:'*';
-                if (trim($minversion) == '')
-                    $minversion = '*';
-                $maxversion = isset($dependency['maxversion'])?(string)$dependency['maxversion']:'*';
-                if (trim($maxversion) == '')
-                    $maxversion = '*';
-
-                $name = (string)$dependency['name'];
-                if (trim($name) == '' && $type != 'jelix')
-                    throw new Exception('Name is missing in a dependency declaration in module '.$this->name);
-                $id = (string)$dependency['id'];
-
-                if ($type == 'jelix') {
-                    $this->jelixMinVersion = $minversion;
-                    $this->jelixMaxVersion = $maxversion;
-                    if ($this->name != 'jelix') {
-                        $this->dependencies[] = array(
-                            'type'=> 'module',
-                            'id' => 'jelix@jelix.org',
-                            'name' => 'jelix',
-                            'minversion' => $this->jelixMinVersion,
-                            'maxversion' => $this->jelixMaxVersion,
-                            ''
-                        );
-                    }
-                }
-                else if ($type == 'module') {
-                    $this->dependencies[] = array(
-                            'type'=> 'module',
-                            'id' => $id,
-                            'name' => $name,
-                            'minversion' => $minversion,
-                            'maxversion' => $maxversion,
-                            ''
-                            );
-                }
-                else if ($type == 'plugin') {
-                    $this->dependencies[] = array(
-                            'type'=> 'plugin',
-                            'id' => $id,
-                            'name' => $name,
-                            'minversion' => $minversion,
-                            'maxversion' => $maxversion,
-                            ''
-                            );
+                $dependencyInfo = $this->readComponentDependencyInfo($type, $dependency);
+                if ($dependencyInfo) {
+                    $this->dependencies[] = $dependencyInfo;
                 }
             }
         }
     }
-    
-    
+
+    /**
+     * @param string     $type
+     * @param \SimpleXMLElement $xml
+     *
+     * @return array|null
+     */
+    protected function readComponentDependencyInfo($type, SimpleXMLElement $dependency)
+    {
+        $minversion = isset($dependency['minversion'])?(string)$dependency['minversion']:'*';
+        if (trim($minversion) == '')
+            $minversion = '*';
+        $maxversion = isset($dependency['maxversion'])?(string)$dependency['maxversion']:'*';
+        if (trim($maxversion) == '')
+            $maxversion = '*';
+
+        $name = (string)$dependency['name'];
+        if (trim($name) == '' && $type != 'jelix')
+            throw new Exception('Name is missing in a dependency declaration in module '.$this->name);
+        $id = (string)$dependency['id'];
+
+        if ($type == 'jelix') {
+            $this->jelixMinVersion = $minversion;
+            $this->jelixMaxVersion = $maxversion;
+            if ($this->name != 'jelix') {
+                return array(
+                    'type'=> 'module',
+                    'id' => 'jelix@jelix.org',
+                    'name' => 'jelix',
+                    'minversion' => $this->jelixMinVersion,
+                    'maxversion' => $this->jelixMaxVersion,
+                    'optional'=>false,
+                    ''
+                );
+            }
+        }
+        else if ($type == 'module') {
+            return array(
+                'type'=> 'module',
+                'id' => $id,
+                'name' => $name,
+                'minversion' => $minversion,
+                'maxversion' => $maxversion,
+                'optional' => (isset($dependency['optional'])? (((string)$dependency['optional']) === 'true'):false),
+                ''
+            );
+        }
+        else if ($type == 'plugin') {
+            return array(
+                'type'=> 'plugin',
+                'id' => $id,
+                'name' => $name,
+                'minversion' => $minversion,
+                'maxversion' => $maxversion,
+                'optional' => (isset($dependency['optional'])? (((string)$dependency['optional']) === 'true'):false),
+                ''
+            );
+        }
+        return null;
+    }
+
+
+
+
+
     public function checkJelixVersion ($jelixVersion) {
         return (jVersionComparator::compareVersion($this->jelixMinVersion, $jelixVersion) <= 0 &&
                 jVersionComparator::compareVersion($jelixVersion, $this->jelixMaxVersion) <= 0);
     }
-    
+
     public function checkVersion($min, $max) {
         return (jVersionComparator::compareVersion($min, $this->sourceVersion) <= 0 &&
                 jVersionComparator::compareVersion($this->sourceVersion, $max) <= 0);
