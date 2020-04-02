ee6e0d778ca0e563139fb977d3b89814d68d7edd
Nicolas Boisteault <nboisteault@3liz.com>
2020-04-02 12:14:55 +0200

    [Favicon] Add favicons files and declarations

--- lizmap/responses/adminLoginHtmlResponse.class.php
+++ lizmap/responses/adminLoginHtmlResponse.class.php
@@ -20,6 +20,26 @@ class adminLoginHtmlResponse extends jResponseHtml
         // Header
         $this->addHttpHeader('x-ua-compatible', 'ie=edge');
 
+        // Favicon
+        $this->addHeadContent('<link rel="shortcut icon" href="/assets/favicon/favicon.ico">');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/apple-icon-57x57.png">');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="60x60" href="/assets/favicon/apple-icon-60x60.png"> ');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/apple-icon-72x72.png">');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/apple-icon-76x76.png"> ');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/apple-icon-114x114.png">');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/apple-icon-120x120.png"> ');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/apple-icon-144x144.png">');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/apple-icon-152x152.png"> ');
+        $this->addHeadContent('<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-icon-180x180.png">');
+        $this->addHeadContent('<link rel="icon" type="image/png" sizes="192x192"  href="/assets/favicon/android-icon-192x192.png"> ');
+        $this->addHeadContent('<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">');
+        $this->addHeadContent('<link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon/favicon-96x96.png"> ');
+        $this->addHeadContent('<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">');
+        $this->addHeadContent('<link rel="manifest" href="/assets/favicon/manifest.json"> ');
+        $this->addHeadContent('<meta name="msapplication-TileColor" content="#ffffff">');
+        $this->addHeadContent('<meta name="msapplication-TileImage" content="/assets/favicon/ms-icon-144x144.png"> ');
+        $this->addHeadContent('<meta name="theme-color" content="#ffffff">');
+
         // Meta
         $this->addHeadContent('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />');
 
