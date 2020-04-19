8ba1cd34782f1c1c7be4dd0c2230a39be0699d62
Laurent Jouanneau <dev@ljouanneau.com>
2020-04-19 09:56:00 +0200

    Replace inline sources of Proj4Php by its Composer package
    Refs #1422

--- lizmap/modules/proj4php/classes/defs/EPSG4272.php
+++
@@ -1,2 +0,0 @@
-<?php
-Proj4php::$defs["EPSG:4272"] = "+title=NZGD49 +proj=longlat +ellps=intl +datum=nzgd49 +no_defs ";
--- lizmap/modules/proj4php/classes/proj4php.class.php
+++ lizmap/modules/proj4php/classes/proj4php.class.php
@@ -1,3 +1,5 @@
 <?php
-
+/**
+ * @deprecated use Proj4php classes directly
+ */
 require_once('proj4php.php');
