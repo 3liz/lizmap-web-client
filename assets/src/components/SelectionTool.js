3100def345bddb7f68e09aa6ab58c33e44c74fa7
Nicolas Boisteault <nboisteault@3liz.com>
2020-05-13 12:24:48 +0200

    [Digitizing] Move UI in a web component

--- assets/src/components/SelectionTool.js
+++ assets/src/components/SelectionTool.js
@@ -32,40 +32,7 @@ export default class SelectionTool extends HTMLElement {
                         </optgroup>
                     </select>
                 </div>
-                <div class="selectiontool-query-buttons btn-group">
-                    <button type="button" class="selectiontool-query-deactivate btn btn-small ${mainLizmap.selectionTool.toolSelected === 'deactivate' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'deactivate'} data-original-title="${lizDict['selectiontool.toolbar.query.deactivate']}">
-                        <i class="icon-none qgis_sprite mIconDeselected"></i>
-                    </button>
-                    <button type="button" class="selectiontool-query-point btn btn-small ${mainLizmap.selectionTool.toolSelected === 'point' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'point'} data-original-title="${lizDict['selectiontool.toolbar.query.point']}">
-                        <svg>
-                            <use xlink:href="#mActionSelectPoint"></use>
-                        </svg>
-                    </button>
-                    <button type="button" class="selectiontool-query-line btn btn-small ${mainLizmap.selectionTool.toolSelected === 'line' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'line'} data-original-title="${lizDict['selectiontool.toolbar.query.line']}">
-                        <svg>
-                            <use xlink:href="#mActionSelectLine"></use>
-                        </svg>
-                    </button>
-                    <button type="button" class="selectiontool-query-polygon btn btn-small ${mainLizmap.selectionTool.toolSelected === 'polygon' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'polygon'} data-original-title="${lizDict['selectiontool.toolbar.query.polygon']}">
-                        <i class="icon-none qgis_sprite mActionSelectPolygon"></i>
-                    </button>
-                    <br>
-                    <button type="button" class="selectiontool-query-box btn btn-small ${mainLizmap.selectionTool.toolSelected === 'box' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'box'} data-original-title="${lizDict['selectiontool.toolbar.query.box']}">
-                        <i class="icon-none qgis_sprite mActionSelectRectangle"></i>
-                    </button>
-                    <button type="button" class="selectiontool-query-circle btn btn-small ${mainLizmap.selectionTool.toolSelected === 'circle' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'circle'} data-original-title="${lizDict['selectiontool.toolbar.query.circle']}">
-                        <i class="icon-none qgis_sprite mActionSelectRadius"></i>
-                    </button>
-                    <button type="button" class="selectiontool-query-freehand btn btn-small ${mainLizmap.selectionTool.toolSelected === 'freehand' ? 'active' : ''}" @click=${() => mainLizmap.selectionTool.toolSelected = 'freehand'} data-original-title="${lizDict['selectiontool.toolbar.query.freehand']}">
-                        <i class="icon-none qgis_sprite mActionSelectFreehand"></i>
-                    </button>
-                </div>
-                <div>
-                    ${lizDict['selectiontool.toolbar.buffer']}&nbsp;:&nbsp;
-                    <div class="input-append">
-                        <input class="input-mini" type="number" min="0" value="0" @change=${ (event) => mainLizmap.selectionTool._bufferValue = parseInt(event.target.value)}><span class="add-on">m</span>
-                    </div>
-                </div>
+                <lizmap-digitizing></lizmap-digitizing>
                 <div>
                     <select class="selection-geom-operator" @change=${ (event) => mainLizmap.selectionTool.geomOperator = event.target.value} data-original-title="${lizDict['selectiontool.toolbar.geomOperator']}">
                         <option value="intersects">Interects</option>
