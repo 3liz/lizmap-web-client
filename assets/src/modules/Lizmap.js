a0d8f4aa804b97ea7e329b59f9a90994116e5350
Nicolas Boisteault <nboisteault@3liz.com>
2020-05-18 14:01:27 +0200

    Register unknow projections with proj4

--- assets/src/modules/Lizmap.js
+++ assets/src/modules/Lizmap.js
@@ -5,12 +5,26 @@ import GeolocationSurvey from '../modules/GeolocationSurvey.js';
 import SelectionTool from '../modules/SelectionTool.js';
 import Snapping from '../modules/Snapping.js';
 
+import { get as getProjection } from 'ol/proj';
+import { register } from 'ol/proj/proj4';
+
+import proj4 from 'proj4';
+
 export default class Lizmap {
 
     constructor() {
         lizMap.events.on({
             uicreated: () => {
                 this._lizmap3 = lizMap;
+
+                // Register projection if unknown
+                if (!getProjection(this.projection)) {
+                    const proj = this.config.options.projection;
+                    proj4.defs(proj.ref, proj.proj4);
+                    register(proj4);
+                }
+
+                // Create Lizmap modules
                 this.map = new Map();
                 this.edition = new Edition();
                 this.geolocation = new Geolocation();
