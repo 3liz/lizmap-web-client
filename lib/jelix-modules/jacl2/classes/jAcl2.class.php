b760f030f9fc9fc2b0b95fdb5529a1870ce5fd18
Laurent Jouanneau <dev@ljouanneau.com>
2020-06-18 16:20:49 +0200

    Upgrade Jelix to 1.6.29-pre
    We need jAcl2::checkByUser()

--- lib/jelix-modules/jacl2/classes/jAcl2.class.php
+++ lib/jelix-modules/jacl2/classes/jAcl2.class.php
@@ -3,13 +3,14 @@
 * @package     jelix
 * @subpackage  acl2
 * @author      Laurent Jouanneau
-* @copyright   2006-2014 Laurent Jouanneau
+* @copyright   2006-2020 Laurent Jouanneau
 * @link        http://www.jelix.org
 * @licence     http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public Licence, see LICENCE file
 * @since 1.1
 */
 
 require(__DIR__.'/jIAcl2Driver.iface.php');
+require(__DIR__.'/jIAcl2Driver2.iface.php');
 
 /**
  * Main class to query the acl system, and to know value of a right
@@ -22,6 +23,9 @@ require(__DIR__.'/jIAcl2Driver.iface.php');
  */
 class jAcl2 {
 
+    /**
+     * @var null|jIAcl2Driver|jIAcl2Driver2
+     */
     static protected $driver = null;
 
     /**
@@ -31,15 +35,17 @@ class jAcl2 {
 
     /**
      * load the acl2 driver
-     * @return jIAcl2Driver
+     * @return jIAcl2Driver|jIAcl2Driver2
      */
     protected static function _getDriver(){
         if (self::$driver == null) {
             $config = jApp::config();
             $db = strtolower($config->acl2['driver']);
-            if ($db == '')
+            if ($db == '') {
                 throw new jException('jacl2~errors.driver.notfound',$db);
+            }
 
+            /** @var jIAcl2Driver|jIAcl2Driver2 */
             self::$driver = jApp::loadPlugin($db, 'acl2', '.acl2.php', $config->acl2['driver'].'Acl2Driver', $config->acl2);
             if (is_null(self::$driver)) {
                 throw new jException('jacl2~errors.driver.notfound',$db);
@@ -57,6 +63,24 @@ class jAcl2 {
     public static function check($subject, $resource=null){
         $dr = self::_getDriver();
         return $dr->getRight($subject, $resource);
+    }
+
+
+    /**
+     * call this method to know if the given user has the right with the given value
+     *
+     * @param string $login the user login. Can be empty/null if anonymous
+     * @param string $subject the key of the subject to check
+     * @param string $resource the id of a resource
+     * @return boolean true if yes
+     * @since 1.6.29
+     */
+    public static function checkByUser($login, $subject, $resource=null){
+        $dr = self::_getDriver();
+        if (!($dr instanceof jIAcl2Driver2)) {
+            throw new Exception("the jacl2 driver does not implement the jIAcl2Driver2 interface");
+        }
+        return $dr->getRightByUser($login, $subject, $resource);
     }
 
     /**
