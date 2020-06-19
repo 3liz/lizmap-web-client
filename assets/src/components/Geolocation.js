8ce26dcc4d22382586bc5bf5aef7d0e0c6a54209
Nicolas Boisteault <nboisteault@3liz.com>
2020-06-19 15:25:16 +0200

    [Geolocation] Let user set bind interval Map can be centered at an interval defined by user and not at every geolocation position change

--- assets/src/components/Geolocation.js
+++ assets/src/components/Geolocation.js
@@ -21,7 +21,11 @@ export default class Geolocation extends HTMLElement {
             <div class="button-bar">
                 <button class="btn btn-small ${mainLizmap.geolocation.isTracking ? 'active btn-success' : ''}" @click=${ () => mainLizmap.geolocation.toggleTracking()} ?disabled=${mainLizmap.geolocation.isTracking && mainLizmap.geolocation.firstGeolocation}><span class="icon"></span>${mainLizmap.geolocation.isTracking ? (mainLizmap.geolocation.firstGeolocation ? lizDict['geolocate.toolbar.waiting'] : lizDict['geolocate.toolbar.stop']) : lizDict['geolocate.toolbar.start']}</button>
                 <button class="btn btn-small" @click=${ () => mainLizmap.geolocation.center()} ?disabled=${!mainLizmap.geolocation.isTracking | mainLizmap.geolocation.isBind | mainLizmap.geolocation.firstGeolocation}><span class="icon"></span>${lizDict['geolocate.toolbar.center']}</button>
-                <button class="btn btn-small ${mainLizmap.geolocation.isBind ? 'active btn-success' : ''}" @click=${() => mainLizmap.geolocation.toggleBind()} ?disabled=${!mainLizmap.geolocation.isTracking | mainLizmap.geolocation.firstGeolocation}><span class="icon"></span>${lizDict['geolocate.toolbar.bind']}</button>
+                <div class="input-prepend input-append">
+                    <button class="btn btn-small ${mainLizmap.geolocation.isBind ? 'active btn-success' : ''}" @click=${() => mainLizmap.geolocation.toggleBind()} ?disabled=${!mainLizmap.geolocation.isTracking | mainLizmap.geolocation.firstGeolocation}><span class="icon"></span>${lizDict['geolocate.toolbar.bind']}</button>
+                    <input class="input-mini" type="number" min="1" ?disabled=${!mainLizmap.geolocation.isBind || !mainLizmap.geolocation.isTracking} value="${mainLizmap.geolocation.bindIntervalInSecond}" @input=${(event) => mainLizmap.geolocation.bindIntervalInSecond = parseInt(event.target.value)}>
+                    <span class="add-on">s</span>
+                </div>
             </div>
             <div class="geolocation-infos">
                 <div><small class="geolocation-coords">${positionTemplate()}</small></div>
