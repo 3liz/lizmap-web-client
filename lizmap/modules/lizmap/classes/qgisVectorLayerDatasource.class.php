02b07f2e5c701e39441126cce83f1d3fcf5393bf
Michael Douchin <mdouchin@3liz.com>
2020-06-30 18:22:10 +0200

    Vector layer datasource - Add dedicated datasource parser covered by unit tests

---
+++ lizmap/modules/lizmap/classes/qgisVectorLayerDatasource.class.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Give access to qgis mapLayer configuration.
+ *
+ * @author    3liz
+ * @copyright 2013-2019 3liz
+ *
+ * @see      http://3liz.com
+ *
+ * @license Mozilla Public License : http://www.mozilla.org/MPL/
+ */
+class qgisVectorLayerDatasource
+{
+
+    /**
+     * @var array Regexes used to get datasource parameters
+     */
+    protected $datasourceRegexes = array(
+        "dbname" => "dbname='([^ ]+)' ",
+        "service" => "service='([^ ]+)' ",
+        "host" => "host=([^ ]+) port=",
+        "port" => "port=([0-9]+) ",
+        "user" => "user='([^ ]+)' ",
+        "password" => "password='([^ ]+)' ",
+        "sslmode" => "sslmode=([^ ]+) ",
+        "key" => "key='([^ ]+)' ",
+        "estimatedmetadata" => "estimatedmetadata=([^ ]+) ",
+        "selectatid" => "selectatid=([^ ]+) ",
+        "srid" => "srid=([0-9]+) ",
+        "type" => "type=([a-zA-Z]+) ",
+        "checkPrimaryKeyUnicity" => "checkPrimaryKeyUnicity='([0-1]+)' ",
+        "table" => ' table="(.+)" (\([^ ]+\) )?sql=',
+        "geocol" => '\(([^ ]+)\) sql=',
+        "sql" => " sql=(.*)$"
+    );
+
+    /**
+     * constructor.
+     *
+     * @param lizmapProject|qgisProject $project
+     * @param array                     $propLayer list of properties values
+     */
+    public function __construct($provider, $datasource)
+    {
+        $this->provider = $provider;
+        $this->datasource = $datasource;
+    }
+
+    public function getDatasourceParameter($param)
+    {
+
+        if( $this->provider == 'ogr' and preg_match('#layername=#', $this->datasource ) ){
+            return $this->getDatasourceParameterOgr($param);
+        } else {
+            return $this->getDatasourceParameterSql($param);
+        }
+
+    }
+
+    private function getDatasourceParameterSql($param)
+    {
+        $value = '';
+
+        // For tablename and schema, first get table
+        // and then get table name or schema
+        if ($param == 'tablename' or $param == 'schema') {
+            $table = $this->getDatasourceParameter('table');
+            if (substr($table, 0, 1) == '"') {
+                $exp = explode('.', str_replace('"', '', $table));
+                if ($param == 'tablename') {
+                    $value = $exp[1];
+                } elseif ($param == 'schema') {
+                    $value = $exp[0];
+                }
+            } else {
+                if ($param == 'tablename') {
+                    $value = $table;
+                } elseif ($param == 'schema') {
+                    $value = '';
+                }
+            }
+            return trim($value);
+        }
+
+        // For other parameters, use specific parameter regex
+        $regex = $this->datasourceRegexes[$param];
+        $test = preg_match(
+            '#' . $regex . '#s',
+            $this->datasource,
+            $result
+        );
+        if ((count($result) == 2 or count($result) == 3 and $param == 'table') and !empty($result[1])) {
+            $value = $result[1];
+
+            // Specific parsing for complex table parameter
+            if ($param == 'table') {
+                $table = $value;
+
+                // Complex subquery
+                if (substr($table, 0, 1) == '(' and substr($table, -1) == ')') {
+                    $table = $table.' fooliz';
+                    // remove \" which escapes table and schema names in QGIS WML within subquery
+                    $table = str_replace('\"', '"', $table);
+                }
+                // Simple "schemaname"."table_name"
+                elseif (preg_match('#"."#', $table)) {
+                    $table = '"'.$table.'"';
+                }
+                $value = $table;
+            }
+        }
+        return trim($value);
+    }
+
+    private function getDatasourceParameterOgr($param)
+    {
+        $split = explode('|', $this->datasource);
+        $dbname = $split[0];
+        $table = str_replace('layername=', '', $split[1]);
+        $sql = '';
+        if( count($split) == 3 ){
+            $sql = str_replace('subset=', '', $split[2]);
+        }
+        $ds = array (
+            'dbname' => $dbname,
+            'service' => '',
+            'host' => '',
+            'port' => '',
+            'user' => '',
+            'password' => '',
+            'sslmode' => '',
+            'key' => '',
+            'estimatedmetadata' => '',
+            'selectatid' => '',
+            'srid' => '',
+            'type' => '',
+            'checkPrimaryKeyUnicity' => '',
+            'table' => $table,
+            'geocol' => 'geom',
+            'sql' => $sql
+        );
+
+        return trim($ds[$param]);
+    }
+
+}
---
+++ tests/units/edition/qgisVectorLayerDatasourceTest.php
@@ -0,0 +1,131 @@
+<?php
+
+class qgisVectorLayerDatasourceTest extends PHPUnit_Framework_TestCase {
+
+
+    function testPostgresqlDatasource() {
+
+        $provider = 'postgres';
+        $datasource = "dbname='test_dbname' host=127.0.0.1 port=5432 user='test_user' password='test_password' sslmode=disable key='id' srid=4326 type=Polygon checkPrimaryKeyUnicity='1' table=\"test_schema\".\"test_table\" (geom) sql=";
+
+        $element = new qgisVectorLayerDatasource($provider, $datasource);
+
+        $this->assertEquals('test_dbname', $element->getDatasourceParameter('dbname'));
+        $this->assertEquals('', $element->getDatasourceParameter('service'));
+        $this->assertEquals('127.0.0.1', $element->getDatasourceParameter('host'));
+        $this->assertEquals('5432', $element->getDatasourceParameter('port'));
+        $this->assertEquals('test_user', $element->getDatasourceParameter('user'));
+        $this->assertEquals('test_password', $element->getDatasourceParameter('password'));
+        $this->assertEquals('disable', $element->getDatasourceParameter('sslmode'));
+        $this->assertEquals('id', $element->getDatasourceParameter('key'));
+        $this->assertEquals('', $element->getDatasourceParameter('estimatedmetadata'));
+        $this->assertEquals('', $element->getDatasourceParameter('selectatid'));
+        $this->assertEquals('4326', $element->getDatasourceParameter('srid'));
+        $this->assertEquals('Polygon', $element->getDatasourceParameter('type'));
+        $this->assertEquals('1', $element->getDatasourceParameter('checkPrimaryKeyUnicity'));
+        $this->assertEquals('"test_schema"."test_table"', $element->getDatasourceParameter('table'));
+        $this->assertEquals('test_table', $element->getDatasourceParameter('tablename'));
+        $this->assertEquals('test_schema', $element->getDatasourceParameter('schema'));
+        $this->assertEquals('geom', $element->getDatasourceParameter('geocol'));
+        $this->assertEquals('', $element->getDatasourceParameter('sql'));
+
+    }
+
+    function testPostgresDatasourceWithoutGeometry() {
+        $provider = 'postgres';
+        $datasource = "dbname='test_dbname' host=test_host port=5432 user='test_user' password='test_password' sslmode=disable key='id' estimatedmetadata=true checkPrimaryKeyUnicity='1' table=\"test_schema\".\"test_table\" sql=";
+
+        $element = new qgisVectorLayerDatasource($provider, $datasource);
+
+        $this->assertEquals('test_dbname', $element->getDatasourceParameter('dbname'));
+        $this->assertEquals('', $element->getDatasourceParameter('service'));
+        $this->assertEquals('test_host', $element->getDatasourceParameter('host'));
+        $this->assertEquals('5432', $element->getDatasourceParameter('port'));
+        $this->assertEquals('test_user', $element->getDatasourceParameter('user'));
+        $this->assertEquals('test_password', $element->getDatasourceParameter('password'));
+        $this->assertEquals('disable', $element->getDatasourceParameter('sslmode'));
+        $this->assertEquals('id', $element->getDatasourceParameter('key'));
+        $this->assertEquals('true', $element->getDatasourceParameter('estimatedmetadata'));
+        $this->assertEquals('', $element->getDatasourceParameter('selectatid'));
+        $this->assertEquals('', $element->getDatasourceParameter('srid'));
+        $this->assertEquals('', $element->getDatasourceParameter('type'));
+        $this->assertEquals('1', $element->getDatasourceParameter('checkPrimaryKeyUnicity'));
+        $this->assertEquals('"test_schema"."test_table"', $element->getDatasourceParameter('table'));
+        $this->assertEquals('test_table', $element->getDatasourceParameter('tablename'));
+        $this->assertEquals('test_schema', $element->getDatasourceParameter('schema'));
+        $this->assertEquals('', $element->getDatasourceParameter('geocol'));
+        $this->assertEquals('', $element->getDatasourceParameter('sql'));
+
+    }
+
+    function testComplexQueryDatasource() {
+
+        $provider = 'postgres';
+        $datasource = "service='test_service' key='id' estimatedmetadata=true checkPrimaryKeyUnicity='1' table=\"((             SELECT                 o.id,&#xD;
+                 so_unique_id AS spatial_object_code,&#xD;
+				 so.geom,&#xD;
+				 ob_timestamp AS observation_timestamp
+FROM gobs.observation AS o             &#xD;
+INNER JOIN gobs.spatial_object AS so                 ON so.id = o.fk_id_spatial_object             &#xD;
+WHERE fk_id_series = 2  )
+)\" (geom) sql=\"observation_timestamp\" &lt; '2017-01-01T00:00:00' AND \"observation_timestamp\" &gt;= '2016-01-01T00:00:00' ";
+
+        $element = new qgisVectorLayerDatasource($provider, $datasource);
+
+        $this->assertEquals('', $element->getDatasourceParameter('dbname'));
+        $this->assertEquals('test_service', $element->getDatasourceParameter('service'));
+        $this->assertEquals('', $element->getDatasourceParameter('host'));
+        $this->assertEquals('', $element->getDatasourceParameter('port'));
+        $this->assertEquals('', $element->getDatasourceParameter('user'));
+        $this->assertEquals('', $element->getDatasourceParameter('password'));
+        $this->assertEquals('', $element->getDatasourceParameter('sslmode'));
+        $this->assertEquals('id', $element->getDatasourceParameter('key'));
+        $this->assertEquals('true', $element->getDatasourceParameter('estimatedmetadata'));
+        $this->assertEquals('', $element->getDatasourceParameter('selectatid'));
+        $this->assertEquals('', $element->getDatasourceParameter('srid'));
+        $this->assertEquals('', $element->getDatasourceParameter('type'));
+        $this->assertEquals('1', $element->getDatasourceParameter('checkPrimaryKeyUnicity'));
+        $table = "((             SELECT                 o.id,&#xD;
+                 so_unique_id AS spatial_object_code,&#xD;
+				 so.geom,&#xD;
+				 ob_timestamp AS observation_timestamp
+FROM gobs.observation AS o             &#xD;
+INNER JOIN gobs.spatial_object AS so                 ON so.id = o.fk_id_spatial_object             &#xD;
+WHERE fk_id_series = 2  )
+) fooliz";
+
+        $this->assertEquals($table, $element->getDatasourceParameter('table'));
+        $this->assertEquals($table, $element->getDatasourceParameter('tablename'));
+        $this->assertEquals('', $element->getDatasourceParameter('schema'));
+        $this->assertEquals('geom', $element->getDatasourceParameter('geocol'));
+        $sql = "\"observation_timestamp\" &lt; '2017-01-01T00:00:00' AND \"observation_timestamp\" &gt;= '2016-01-01T00:00:00'";
+        $this->assertEquals($sql, $element->getDatasourceParameter('sql'));
+
+    }
+
+    function testGeopackageDatasource() {
+
+        $provider = 'ogr';
+        $datasource = './edition/events.gpkg|layername=events';
+
+        $element = new qgisVectorLayerDatasource($provider, $datasource);
+
+        $this->assertEquals('./edition/events.gpkg', $element->getDatasourceParameter('dbname'));
+        $this->assertEquals('events', $element->getDatasourceParameter('table'));
+        $this->assertEquals('', $element->getDatasourceParameter('sql'));
+    }
+
+    function testGeopackageDatasourceWithSql() {
+
+        $provider = 'ogr';
+        $datasource = './edition/events.gpkg|layername=events|subset="counter" > 3';
+
+        $element = new qgisVectorLayerDatasource($provider, $datasource);
+
+        $this->assertEquals('./edition/events.gpkg', $element->getDatasourceParameter('dbname'));
+        $this->assertEquals('events', $element->getDatasourceParameter('table'));
+        $this->assertEquals('"counter" > 3', $element->getDatasourceParameter('sql'));
+    }
+
+
+}
