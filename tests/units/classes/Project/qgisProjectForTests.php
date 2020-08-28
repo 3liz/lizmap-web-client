2a61563a6c44d494b17faaa3141f499808144e02
alagroy- <alagroy-@student.42.fr>
2020-08-28 17:32:21 +0200

    adding beggining of QgisProject tests

---
+++ tests/units/classes/Project/qgisProjectForTests.php
@@ -0,0 +1,30 @@
+<?php
+
+use Lizmap\Project\QgisProject;
+
+class qgisProjectForTests extends QgisProject
+{
+    public function __construct()
+    {
+    }
+
+    public function readWMSInfoTest($xml)
+    {
+        return $this->readWMSInformation($xml);
+    }
+
+    public function readCanvasColorTest($xml)
+    {
+        return $this->readCanvasColor($xml);
+    }
+
+    public function readAllProj4Test($xml)
+    {
+        return $this->readAllProj4($xml);
+    }
+
+    public function readUseLayersIDsTest($xml)
+    {
+        return $this->readUseLayerIDs($xml);
+    }
+}
