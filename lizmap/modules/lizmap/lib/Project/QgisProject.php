d9bf071fc666be15af339f58b8020fbb3a1e736f
alagroy- <alagroy-@student.42.fr>
2020-09-01 12:26:27 +0200

    fixing get XmlLayer2 bug

--- lizmap/modules/lizmap/lib/Project/QgisProject.php
+++ lizmap/modules/lizmap/lib/Project/QgisProject.php
@@ -502,7 +502,13 @@ class QgisProject
      */
     protected function getXmlLayer2($xml, $layerId)
     {
-        return $xml->xpath("//maplayer[id='{$layerId}']");
+        $layerList = $xml->xpath("//maplayer");
+        foreach ($layerList as $layer) {
+            if ((string)$layer->id === $layerId) {
+                return $layer;
+            }
+        }
+        return null;
     }
 
     /**
