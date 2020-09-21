9c0f805a8a1b81b5baa2337b2ffe05736861fc9b
Nicolas Boisteault <nboisteault@3liz.com>
2020-09-21 17:27:26 +0200

    Add coords-unit selector

--- assets/src/components/MousePosition.js
+++ assets/src/components/MousePosition.js
@@ -13,9 +13,18 @@ export default class MousePosition extends HTMLElement {
 
     mainTemplate(lon, lat){
         return html`
-                <div>
+                <div class="mouse-position">
                     <input type="number" step="any" class="input-small" placeholder="longitude" .value=${lon}>
                     <input type="number" step="any" class="input-small" placeholder="latitude" .value=${lat}>
+                </div>
+                <div class="coords-unit">
+                    <select title="${lizDict['mouseposition.select']}">
+                        <option value="m">${lizDict['mouseposition.units.m']}</option>
+                        <option value="f">${lizDict['mouseposition.units.f']}</option>
+                        <option value="d">${lizDict['mouseposition.units.d']}</option>
+                        <option value="dm">${lizDict['mouseposition.units.dm']}</option>
+                        <option value="dms">${lizDict['mouseposition.units.dms']}</option>
+                    </select>
                 </div>`;
     }
 
