6093acea6467ff87b7c43a7459fdb38ebf1102e2
Nicolas Boisteault <nboisteault@3liz.com>
2020-09-22 11:48:12 +0200

    Add dm, dms, units Those units are not editable

--- assets/src/components/MousePosition.js
+++ assets/src/components/MousePosition.js
@@ -7,27 +7,34 @@ export default class MousePosition extends HTMLElement {
     constructor() {
         super();
 
-        this.numDigits = 0;
-        this.displayUnit = 'm';
+        this._numDigits = 0;
+        this._displayUnit = 'm';
 
         this._lastLon;
         this._lastLat;
     }
 
-    // Render positionTemplate apart because values change a lot
-    positionTemplate(lon, lat){
+    // Render editablePositionTemplate and readonlyPositionTemplate apart because values change a lot
+    editablePositionTemplate(lon, lat){
         return html`
             <input type="number" step="any" class="input-small" placeholder="longitude" .value=${lon}>
             <input type="number" step="any" class="input-small" placeholder="latitude" .value=${lat}>`;
     }
 
+    readonlyPositionTemplate(lon, lat) {
+        return html`
+            <span>${lon}</span>
+            <span>${lat}</span>`;
+    }
+
     mainTemplate(lon, lat){
         return html`
             <div class="mouse-position">
-                ${this.positionTemplate(lon, lat)}
+                <div class="editable-position ${['m', 'd'].includes(this._displayUnit) ? '' : 'hide'}">${this.editablePositionTemplate(lon, lat)}</div>
+                <div class="readonly-position ${['m', 'd'].includes(this._displayUnit) ? 'hide' : ''}">${this.readonlyPositionTemplate(lon, lat)}</div>
             </div>
             <div class="coords-unit">
-                <select title="${lizDict['mouseposition.select']}" @change=${(event) => { this._displayUnit = event.target.value }}>
+                <select title="${lizDict['mouseposition.select']}" @change=${(event) => { this.displayUnit = event.target.value }}>
                     <option value="m">${lizDict['mouseposition.units.m']}</option>
                     <option value="f">${lizDict['mouseposition.units.f']}</option>
                     <option value="d">${lizDict['mouseposition.units.d']}</option>
@@ -37,11 +44,14 @@ export default class MousePosition extends HTMLElement {
             </div>`;
     }
 
-    set _displayUnit(unit){
-        unit === 'm' ? this.numDigits = 0 : this.numDigits = 5;
-        this.displayUnit = unit;
+    /**
+     * @param {string} unit
+     */
+    set displayUnit(unit){
+        unit === 'm' ? this._numDigits = 0 : this._numDigits = 5;
+        this._displayUnit = unit;
 
-        this.redraw(this._lastLon, this._lastLat);
+        render(this.mainTemplate(this._lastLon, this._lastLat), this);
     }
 
     // Callback to map's mousemove event
@@ -62,24 +72,94 @@ export default class MousePosition extends HTMLElement {
     redraw(lon, lat){
         let lonLatToDisplay = [lon, lat];
 
-        // Ex formatOutput() code
         const qgisProjectProjection = getProjection(mainLizmap.qgisProjectProjection);
 
         // Display coordinates in QGIS project projection if in meter and meter chosen
-        if (qgisProjectProjection && qgisProjectProjection.getUnits() === 'm' && this.displayUnit === 'm') {
+        if (qgisProjectProjection && qgisProjectProjection.getUnits() === 'm' && this._displayUnit === 'm') {
             lonLatToDisplay = transform(lonLatToDisplay, mainLizmap.projection, mainLizmap.qgisProjectProjection);
-        }
 
-        if (this.displayUnit.indexOf('d') === 0 && mainLizmap.lizmap3.map.projection.getUnits() !== 'degrees') {
+            render(this.editablePositionTemplate(lonLatToDisplay[0].toFixed(this._numDigits), lonLatToDisplay[1].toFixed(this._numDigits)),
+                this.querySelector('.mouse-position > .editable-position'));
+
+        } // Display in degree, degree minute, degree minute second
+        else if (this._displayUnit.indexOf('d') === 0 && mainLizmap.lizmap3.map.projection.getUnits() !== 'degrees') {
             lonLatToDisplay = transform(lonLatToDisplay, mainLizmap.projection, 'EPSG:4326');
-        }
 
-        render(this.positionTemplate(lonLatToDisplay[0].toFixed(this.numDigits),lonLatToDisplay[1].toFixed(this.numDigits)),
-            this.querySelector('.mouse-position'));
+            // If in degree, lon/lat are editable
+            if (this._displayUnit === 'd'){
+                render(this.editablePositionTemplate(lonLatToDisplay[0].toFixed(this._numDigits), lonLatToDisplay[1].toFixed(this._numDigits)),
+                    this.querySelector('.mouse-position > .editable-position'));
+
+            }else{
+                lonLatToDisplay[0] = this.getFormattedLonLat(lonLatToDisplay[0], 'lon', this._displayUnit);
+                lonLatToDisplay[1] = this.getFormattedLonLat(lonLatToDisplay[1], 'lat', this._displayUnit);
+
+                render(this.readonlyPositionTemplate(lonLatToDisplay[0], lonLatToDisplay[1]),
+                    this.querySelector('.mouse-position > .readonly-position'));
+            }
+        }
     }
 
     reset(){
 
+    }
+
+    getFormattedLonLat (coordinate, axis, dmsOption) {
+        if (!dmsOption) {
+            dmsOption = 'dms';    //default to show degree, minutes, seconds
+        }
+
+        coordinate = (coordinate + 540) % 360 - 180; // normalize for sphere being round
+
+        var abscoordinate = Math.abs(coordinate);
+        var coordinatedegrees = Math.floor(abscoordinate);
+
+        var coordinateminutes = (abscoordinate - coordinatedegrees) / (1 / 60);
+        var tempcoordinateminutes = coordinateminutes;
+        coordinateminutes = Math.floor(coordinateminutes);
+        var coordinateseconds = (tempcoordinateminutes - coordinateminutes) / (1 / 60);
+        coordinateseconds = Math.round(coordinateseconds * 10);
+        coordinateseconds /= 10;
+
+        if (coordinateseconds >= 60) {
+            coordinateseconds -= 60;
+            coordinateminutes += 1;
+            if (coordinateminutes >= 60) {
+                coordinateminutes -= 60;
+                coordinatedegrees += 1;
+            }
+        }
+
+        if (coordinatedegrees < 10) {
+            coordinatedegrees = "0" + coordinatedegrees;
+        }
+        var str = coordinatedegrees + "\u00B0";
+
+        if (dmsOption.indexOf('dms') >= 0) {
+            if (coordinateminutes < 10) {
+                coordinateminutes = "0" + coordinateminutes;
+            }
+            str += coordinateminutes + "'";
+
+            if (coordinateseconds < 10) {
+                coordinateseconds = "0" + coordinateseconds;
+            }
+            str += coordinateseconds + '"';
+        } else if (dmsOption.indexOf('dm') >= 0) {
+            coordinateminutes = Math.round(tempcoordinateminutes * 1000);
+            coordinateminutes = coordinateminutes / 1000;
+            if (coordinateminutes < 10) {
+                coordinateminutes = "0" + coordinateminutes;
+            }
+            str += coordinateminutes + "'";
+        }
+
+        if (axis == "lon") {
+            str += coordinate < 0 ? "W" : "E";
+        } else {
+            str += coordinate < 0 ? "S" : "N";
+        }
+        return str;
     }
 
     connectedCallback() {
