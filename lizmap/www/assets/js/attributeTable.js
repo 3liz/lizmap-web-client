3liz/lizmap-web-client

commit ccd9cc4e26e2ca280564be19d31e0246248eeab0

Update attributeTable.js

lizmap/www/assets/js/attributeTable.js
@@ -1280,6 +1280,20 @@ var lizAttributeTable = function() {
 
                 return false;
             }
+            
+            function valueMapInAttributeTable( aName, data, type, full, meta ){
+            // Translate field ( language translation OR code->label translation )
+                var colMeta = meta.settings.aoColumns[meta.col];
+                var colName = colMeta.mData
+                var translation_dict = null;
+                var tdata = data;
+                if(data || data === 0)
+                    tdata = lizMap.translateWfsFieldValues(aName, colName, data.toString(), translation_dict);
+                if( tdata === null )
+                    tdata = data;
+                
+                return tdata;
+            }
 
             function createDatatableColumns(aName, atFeatures, geometryType, canEdit, canDelete, isChild, isPivot, hiddenFields, cAliases, cTypes){
                 var columns = [];
@@ -1332,36 +1346,11 @@ var lizAttributeTable = function() {
                             case 'unsignedInt':
                             case 'long':
                             case 'unsignedLong':
-                                colConf['mRender'] = function( data, type, full, meta ){
+                                colConf['mRender'] = function(data, type, full, meta ){
                                     // Translate field ( language translation OR code->label translation )
-                                    var colMeta = meta.settings.aoColumns[meta.col];
-                                    var colName = colMeta.mData
-                                    var translation_dict = null;
-                                    var tdata = data;
-                                    if(data || data === 0)
-                                        tdata = lizMap.translateWfsFieldValues(aName, colName, data.toString(), translation_dict);
-                                    if( tdata === null )
-                                        tdata = data;
-
-                                    // Replace media and URL with links
-                                    if( !tdata || !( typeof tdata === 'string') )
-                                        return tdata;
-                                    if( tdata.substr(0,6) == 'media/' || tdata.substr(0,7) == '/media/' || tdata.substr(0,9) == '../media/'){
-                                        var rdata = tdata;
-                                        if( tdata.substr(0,7) == '/media/' )
-                                            rdata = tdata.slice(1);
-                                        return '<a href="' + mediaLinkPrefix + '&path=' + rdata + '" target="_blank">' + colMeta.title + '</a>';
-                                    }
-                                    else if( tdata.substr(0,4) == 'http' || tdata.substr(0,3) == 'www' ){
-                                        var rdata = tdata;
-                                        if(tdata.substr(0,3) == 'www')
-                                            rdata = 'http://' + tdata;
-                                        return '<a href="' + rdata + '" target="_blank">' + tdata + '</a>';
-                                    }
-                                    else
-                                        return tdata;
-                                    //return parseInt(data);
-                                }
+                                    return valueMapInAttributeTable( aName, data, type, full, meta );  
+                                };
+                                
                                 colConf['className'] = 'text-right';
                                 break;
                             case 'decimal':
@@ -1377,14 +1366,7 @@ var lizAttributeTable = function() {
                             default:
                                 colConf['mRender'] = function( data, type, full, meta ){
                                     // Translate field ( language translation OR code->label translation )
-                                    var colMeta = meta.settings.aoColumns[meta.col];
-                                    var colName = colMeta.mData
-                                    var translation_dict = null;
-                                    var tdata = data;
-                                    if(data)
-                                        tdata = lizMap.translateWfsFieldValues(aName, colName, data.toString(), translation_dict);
-                                    if( tdata === null )
-                                        tdata = data;
+                                    var tdata = valueMapInAttributeTable( aName, data, type, full, meta );
 
                                     // Replace media and URL with links
                                     if( !tdata || !( typeof tdata === 'string') )

commit 6af4637945a2c5cc574c6ca04f770630643486b3

Fix attributeTable.js, link name causing error Error introduced by  https://github.com/3liz/lizmap-web-client/commit/ccd9cc4e26e2ca280564be19d31e0246248eeab0

lizmap/www/assets/js/attributeTable.js
@@ -1378,6 +1378,7 @@ var lizAttributeTable = function() {
                                         return tdata;
                                     if( tdata.substr(0,6) == 'media/' || tdata.substr(0,7) == '/media/' || tdata.substr(0,9) == '../media/'){
                                         var rdata = tdata;
+                                        var colMeta = meta.settings.aoColumns[meta.col];
                                         if( tdata.substr(0,7) == '/media/' )
                                             rdata = tdata.slice(1);
                                         return '<a href="' + mediaLinkPrefix + '&path=' + rdata + '" target="_blank">' + colMeta.title + '</a>';