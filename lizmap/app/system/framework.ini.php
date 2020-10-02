eebd2303fe6909d7e1e92cd1ca2a5e75c228da94
Laurent Jouanneau <dev@ljouanneau.com>
2020-10-02 13:06:45 +0200

    reorganize mainconfig.ini.php

--- lizmap/app/system/framework.ini.php
+++ lizmap/app/system/framework.ini.php
@@ -2,9 +2,11 @@
 [entrypoint:index.php]
 config="index/config.ini.php"
 type=classic
+
 [entrypoint:admin.php]
 config="admin/config.ini.php"
 type=classic
+
 [entrypoint:script.php]
 config="cmdline/script.ini.php"
 type=cmdline
