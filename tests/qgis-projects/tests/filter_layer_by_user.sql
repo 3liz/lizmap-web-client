970f040c51ad30052e7035da3a7dbede0a5aa3ba
Nicolas Boisteault <nboisteault@3liz.com>
2020-11-16 11:02:46 +0100

    Add dump for one test project as as single file

--- tests/qgis-projects/tests/filter_layer_by_user.sql
+++ tests/qgis-projects/tests/filter_layer_by_user.sql
@@ -3,9 +3,7 @@
 --
 
 -- Dumped from database version 11.2 (Debian 11.2-1.pgdg90+1)
--- Dumped by pg_dump version 13.0 (Ubuntu 13.0-1.pgdg18.04+1)
-
--- Started on 2020-11-12 15:02:00 CET
+-- Dumped by pg_dump version 11.10 (Ubuntu 11.10-1.pgdg18.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,20 +16,11 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- TOC entry 13 (class 2615 OID 20843)
--- Name: tests_projects; Type: SCHEMA; Schema: -; Owner: lizmap
---
-
-CREATE SCHEMA tests_projects;
-
-
-ALTER SCHEMA tests_projects OWNER TO lizmap;
-
 SET default_tablespace = '';
 
+SET default_with_oids = false;
+
 --
--- TOC entry 289 (class 1259 OID 29010)
 -- Name: filter_layer_by_user; Type: TABLE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -46,7 +35,6 @@ CREATE TABLE tests_projects.filter_layer_by_user (
 ALTER TABLE tests_projects.filter_layer_by_user OWNER TO lizmap;
 
 --
--- TOC entry 291 (class 1259 OID 29031)
 -- Name: filter_layer_by_user_edition_only; Type: TABLE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -61,7 +49,6 @@ CREATE TABLE tests_projects.filter_layer_by_user_edition_only (
 ALTER TABLE tests_projects.filter_layer_by_user_edition_only OWNER TO lizmap;
 
 --
--- TOC entry 290 (class 1259 OID 29029)
 -- Name: filter_layer_by_user_edition_only_gid_seq; Type: SEQUENCE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -77,8 +64,6 @@ CREATE SEQUENCE tests_projects.filter_layer_by_user_edition_only_gid_seq
 ALTER TABLE tests_projects.filter_layer_by_user_edition_only_gid_seq OWNER TO lizmap;
 
 --
--- TOC entry 4702 (class 0 OID 0)
--- Dependencies: 290
 -- Name: filter_layer_by_user_edition_only_gid_seq; Type: SEQUENCE OWNED BY; Schema: tests_projects; Owner: lizmap
 --
 
@@ -86,7 +71,6 @@ ALTER SEQUENCE tests_projects.filter_layer_by_user_edition_only_gid_seq OWNED BY
 
 
 --
--- TOC entry 288 (class 1259 OID 29008)
 -- Name: filter_layer_by_user_gid_seq; Type: SEQUENCE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -102,8 +86,6 @@ CREATE SEQUENCE tests_projects.filter_layer_by_user_gid_seq
 ALTER TABLE tests_projects.filter_layer_by_user_gid_seq OWNER TO lizmap;
 
 --
--- TOC entry 4703 (class 0 OID 0)
--- Dependencies: 288
 -- Name: filter_layer_by_user_gid_seq; Type: SEQUENCE OWNED BY; Schema: tests_projects; Owner: lizmap
 --
 
@@ -111,7 +93,6 @@ ALTER SEQUENCE tests_projects.filter_layer_by_user_gid_seq OWNED BY tests_projec
 
 
 --
--- TOC entry 293 (class 1259 OID 29042)
 -- Name: layer_with_no_filter; Type: TABLE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -124,7 +105,6 @@ CREATE TABLE tests_projects.layer_with_no_filter (
 ALTER TABLE tests_projects.layer_with_no_filter OWNER TO lizmap;
 
 --
--- TOC entry 292 (class 1259 OID 29040)
 -- Name: layer_with_no_filter_gid_seq; Type: SEQUENCE; Schema: tests_projects; Owner: lizmap
 --
 
@@ -140,8 +120,6 @@ CREATE SEQUENCE tests_projects.layer_with_no_filter_gid_seq
 ALTER TABLE tests_projects.layer_with_no_filter_gid_seq OWNER TO lizmap;
 
 --
--- TOC entry 4704 (class 0 OID 0)
--- Dependencies: 292
 -- Name: layer_with_no_filter_gid_seq; Type: SEQUENCE OWNED BY; Schema: tests_projects; Owner: lizmap
 --
 
@@ -149,7 +127,6 @@ ALTER SEQUENCE tests_projects.layer_with_no_filter_gid_seq OWNED BY tests_projec
 
 
 --
--- TOC entry 4554 (class 2604 OID 29013)
 -- Name: filter_layer_by_user gid; Type: DEFAULT; Schema: tests_projects; Owner: lizmap
 --
 
@@ -157,7 +134,6 @@ ALTER TABLE ONLY tests_projects.filter_layer_by_user ALTER COLUMN gid SET DEFAUL
 
 
 --
--- TOC entry 4555 (class 2604 OID 29034)
 -- Name: filter_layer_by_user_edition_only gid; Type: DEFAULT; Schema: tests_projects; Owner: lizmap
 --
 
@@ -165,7 +141,6 @@ ALTER TABLE ONLY tests_projects.filter_layer_by_user_edition_only ALTER COLUMN g
 
 
 --
--- TOC entry 4556 (class 2604 OID 29045)
 -- Name: layer_with_no_filter gid; Type: DEFAULT; Schema: tests_projects; Owner: lizmap
 --
 
@@ -173,8 +148,6 @@ ALTER TABLE ONLY tests_projects.layer_with_no_filter ALTER COLUMN gid SET DEFAUL
 
 
 --
--- TOC entry 4692 (class 0 OID 29010)
--- Dependencies: 289
 -- Data for Name: filter_layer_by_user; Type: TABLE DATA; Schema: tests_projects; Owner: lizmap
 --
 
@@ -186,8 +159,6 @@ COPY tests_projects.filter_layer_by_user (gid, "user", "group", geom) FROM stdin
 
 
 --
--- TOC entry 4694 (class 0 OID 29031)
--- Dependencies: 291
 -- Data for Name: filter_layer_by_user_edition_only; Type: TABLE DATA; Schema: tests_projects; Owner: lizmap
 --
 
@@ -199,8 +170,6 @@ COPY tests_projects.filter_layer_by_user_edition_only (gid, "user", "group", geo
 
 
 --
--- TOC entry 4696 (class 0 OID 29042)
--- Dependencies: 293
 -- Data for Name: layer_with_no_filter; Type: TABLE DATA; Schema: tests_projects; Owner: lizmap
 --
 
@@ -210,8 +179,6 @@ COPY tests_projects.layer_with_no_filter (gid, geom) FROM stdin;
 
 
 --
--- TOC entry 4705 (class 0 OID 0)
--- Dependencies: 290
 -- Name: filter_layer_by_user_edition_only_gid_seq; Type: SEQUENCE SET; Schema: tests_projects; Owner: lizmap
 --
 
@@ -219,8 +186,6 @@ SELECT pg_catalog.setval('tests_projects.filter_layer_by_user_edition_only_gid_s
 
 
 --
--- TOC entry 4706 (class 0 OID 0)
--- Dependencies: 288
 -- Name: filter_layer_by_user_gid_seq; Type: SEQUENCE SET; Schema: tests_projects; Owner: lizmap
 --
 
@@ -228,8 +193,6 @@ SELECT pg_catalog.setval('tests_projects.filter_layer_by_user_gid_seq', 3, true)
 
 
 --
--- TOC entry 4707 (class 0 OID 0)
--- Dependencies: 292
 -- Name: layer_with_no_filter_gid_seq; Type: SEQUENCE SET; Schema: tests_projects; Owner: lizmap
 --
 
@@ -237,7 +200,6 @@ SELECT pg_catalog.setval('tests_projects.layer_with_no_filter_gid_seq', 1, true)
 
 
 --
--- TOC entry 4560 (class 2606 OID 29039)
 -- Name: filter_layer_by_user_edition_only filter_layer_by_user_edition_only_pkey; Type: CONSTRAINT; Schema: tests_projects; Owner: lizmap
 --
 
@@ -246,7 +208,6 @@ ALTER TABLE ONLY tests_projects.filter_layer_by_user_edition_only
 
 
 --
--- TOC entry 4558 (class 2606 OID 29018)
 -- Name: filter_layer_by_user filter_layer_by_user_pkey; Type: CONSTRAINT; Schema: tests_projects; Owner: lizmap
 --
 
@@ -255,15 +216,12 @@ ALTER TABLE ONLY tests_projects.filter_layer_by_user
 
 
 --
--- TOC entry 4562 (class 2606 OID 29050)
 -- Name: layer_with_no_filter layer_with_no_filter_pkey; Type: CONSTRAINT; Schema: tests_projects; Owner: lizmap
 --
 
 ALTER TABLE ONLY tests_projects.layer_with_no_filter
     ADD CONSTRAINT layer_with_no_filter_pkey PRIMARY KEY (gid);
 
-
--- Completed on 2020-11-12 15:02:01 CET
 
 --
 -- PostgreSQL database dump complete
