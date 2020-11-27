a8368525560086070ce91365b4084af00ca644bf
alagroy- <alagroy-@student.42.fr>
2020-11-27 14:31:05 +0100

    moving getMap and build methods to more logic places

--- lizmap/modules/lizmap/controllers/service.classic.php
+++ lizmap/modules/lizmap/controllers/service.classic.php
@@ -74,7 +74,7 @@ class serviceCtrl extends jController
             }
         }
 
-        $ogcRequest = lizmapOGCRequest::build($this->project, $this->params, $requestXml);
+        $ogcRequest = \Lizmap\Request\Proxy::build($this->project, $this->params, $requestXml);
         if ($ogcRequest === null) {
             // Error message
             jMessage::add('Service unknown or unsupported.', 'ServiceNotSupported');
