331f249fc8904f3993210e4f8ad11c58c0fa2926
alagroy- <alagroy-@student.42.fr>
2020-11-30 09:47:37 +0100

    lint

--- lizmap/modules/lizmap/lib/Request/OGCRequest.php
+++ lizmap/modules/lizmap/lib/Request/OGCRequest.php
@@ -260,25 +260,26 @@ abstract class OGCRequest
         );
     }
 
-    protected function logXmlError($xmldata)
+    protected function loadXmlString($xmldata, $name)
     {
-         // Get data from XML
-         $use_errors = libxml_use_internal_errors(true);
-         $errorlist = array();
-         // Create a DOM instance
-         $xml = simplexml_load_string($xmldata);
-         if (!$xml) {
-             foreach (libxml_get_errors() as $error) {
-                 $errorlist[] = $error;
-             }
-             $errormsg = 'An error has been raised when loading GetFeatureInfoHtml:';
-             $errormsg .= '\n'.http_build_query($this->params);
-             $errormsg .= '\n'.$xmldata;
-             $errormsg .= '\n'.implode('\n', $errorlist);
-             \jLog::log($errormsg, 'error');
-             // return empty html string
-             return null;
-         }
-         return $xml;
+        // Get data from XML
+        $use_errors = libxml_use_internal_errors(true);
+        $errorlist = array();
+        // Create a DOM instance
+        $xml = simplexml_load_string($xmldata);
+        if (!$xml) {
+            foreach (libxml_get_errors() as $error) {
+                $errorlist[] = $error;
+            }
+            $errormsg = 'An error has been raised when loading '.$name.':';
+            $errormsg .= '\n'.http_build_query($this->params);
+            $errormsg .= '\n'.$xmldata;
+            $errormsg .= '\n'.implode('\n', $errorlist);
+            \jLog::log($errormsg, 'error');
+            // return empty html string
+            return null;
+        }
+
+        return $xml;
     }
 }
