b7f00ef2e8ad08b6203665f952cc1ee759e9bc0d
Nicolas Boisteault <nboisteault@3liz.com>
2020-12-14 15:58:29 +0100

    Fix scale values to match those defined in plugin Scales are those defined in Lizmap plugin when there is  no EPSG:3857 base layers defined Fix #1938

--- assets/src/components/Scaleline.js
+++ assets/src/components/Scaleline.js
@@ -7,11 +7,18 @@ export default class Scaleline extends HTMLElement {
     }
 
     connectedCallback() {
+
+        // DPI is adjusted here because this commit https://github.com/openlayers/openlayers/commit/857f4e01ac9c6c27d83f7cc1f5eda86c53405228
+        // has not been released yet. Also we use 25.40005080010160020 as it is more precise
+        // TODO : Change this value when commit released and Lizmap up to date with OL 6
+        const ADJUSTED_DPI = (96 * 1000) / (25.40005080010160020 * 39.37);
+
         this._olScaleline = new ScaleLine({
             target: this,
             minWidth: 76,
             bar: true,
-            text: true
+            text: true,
+            dpi: ADJUSTED_DPI
         });
 
         mainLizmap.map._olMap.addControl(
