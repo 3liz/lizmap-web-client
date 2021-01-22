d5aff63061e8524c61574d4c089629c6ed3cc1f0
Laurent Jouanneau <dev@ljouanneau.com>
2021-01-22 16:13:39 +0100

    [install] Fix set_rights.sh in which some directories were missing

--- lizmap/install/set_rights.sh
+++ lizmap/install/set_rights.sh
@@ -12,8 +12,12 @@ if [ "$GROUP" = "" ]; then
     GROUP="www-data"
 fi
 
+VARDIR="$SCRIPTDIR/../var"
+WWWDIR="$SCRIPTDIR/../www"
 
-DIRS="$SCRIPTDIR/../var/config $SCRIPTDIR/../var/db $SCRIPTDIR/../var/log $SCRIPTDIR/../var/mails $SCRIPTDIR/../var/uploads $SCRIPTDIR/../var/lizmap-theme-config $SCRIPTDIR/../../temp/lizmap"
+DIRS="$VARDIR/config $VARDIR/db $VARDIR/log"
+DIRS="$DIRS $VARDIR/mails $VARDIR/uploads $VARDIR/lizmap-theme-config"
+DIRS="$DIRS $SCRIPTDIR/../../temp/lizmap $WWWDIR/cache/ $WWWDIR/document/ $WWWDIR/live/"
 
 chown -R $USER:$GROUP $DIRS
 chmod -R ug+w $DIRS
