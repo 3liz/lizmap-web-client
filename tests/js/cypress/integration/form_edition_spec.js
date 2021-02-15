5c2bc5019a88dbb836d00ce7e69f1d3400dfee60
Nicolas Boisteault <nboisteault@3liz.com>
2021-02-15 11:56:52 +0100

    [e2e tests] Assert basic form submition

---
+++ tests/js/cypress/integration/form_edition_spec.js
@@ -0,0 +1,19 @@
+describe('Form edition', function () {
+    before(function () {
+        // runs once before the first test in this block
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=end2end_form_edition')
+        // Todo wait for map to be fully loaded
+        cy.wait(2000)
+    })
+
+    it('submits form and gets success message', function () {
+        cy.get('#button-edition').click()
+        cy.get('#edition-draw').click()
+        cy.get('#jforms_view_edition_value').type('42')
+        cy.get('#jforms_view_edition__submit_submit').click()
+
+        // Assert success message is displayed
+        cy.get('#message .jelix-msg-item-success').should('be.visible')
+    })
+
+})
