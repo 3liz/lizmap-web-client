cf59409410aae7ae13cb7ae05e720c542a0ba492
Laurent Jouanneau <dev@ljouanneau.com>
2021-02-19 15:36:33 +0100

    Fix geobookmark sql: remove explicit public schema
    The table should not be created explicitely into the public schema.
    All Lizmap tables must be installed in the same schema, e.g. in the
    first schema indicated into the search_path of the connection.

--- lizmap/modules/lizmap/install/sql/lizgeobookmark.pgsql.sql
+++ lizmap/modules/lizmap/install/sql/lizgeobookmark.pgsql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS public.geobookmark(
+CREATE TABLE IF NOT EXISTS geobookmark(
   id serial PRIMARY KEY,
   usr_login text NOT NULL,
   bname text NOT NULL,
@@ -7,13 +7,13 @@ CREATE TABLE IF NOT EXISTS public.geobookmark(
 );
 
 DROP INDEX IF EXISTS geobookmark_usr_login_idx;
-ALTER TABLE public.geobookmark
+ALTER TABLE geobookmark
 DROP CONSTRAINT IF EXISTS geobookmark_usr_login_fkey
 ;
 
-ALTER TABLE public.geobookmark
+ALTER TABLE geobookmark
 ADD CONSTRAINT geobookmark_usr_login_fkey FOREIGN KEY (usr_login)
 REFERENCES jlx_user (usr_login) MATCH SIMPLE
 ON UPDATE CASCADE ON DELETE CASCADE;
 
-CREATE INDEX geobookmark_usr_login_idx ON public.geobookmark( usr_login );
+CREATE INDEX geobookmark_usr_login_idx ON geobookmark( usr_login );
