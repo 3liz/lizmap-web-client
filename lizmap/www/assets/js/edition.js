3liz/lizmap-web-client

commit d72938dc05c4d7ac9ce3a8b7ae53cc205b444ebe

UX: Display form first in edition mode for desktop and mobile

lizmap/www/assets/js/edition.js
@@ -724,7 +724,7 @@ OpenLayers.Geometry.pointOnSegment = function(point, segment) {
             // edit layer events
             editLayer.events.on({
 
-                featureadded: function(evt) {
+                featureadded: function() {
                     // Deactivate draw control
                     if( !editCtrls )
                         return false;
@@ -763,13 +763,6 @@ OpenLayers.Geometry.pointOnSegment = function(point, segment) {
 
                     // Inform user he can now modify
                     addEditionMessage(lizDict['edition.select.modify.activate'],'info',true);
-
-                    var btn = $('#button-edition');
-                    var dockVisible = btn.parent().hasClass('active');
-                    if( lizMap.checkMobile() && !dockVisible ){
-                        btn.click();
-                    }
-
                 },
 
                 featuremodified: function(evt) {
@@ -1466,7 +1459,11 @@ OpenLayers.Geometry.pointOnSegment = function(point, segment) {
                         if ( !ctrl.active ) {
                             ctrl.activate();
 
-                            addEditionMessage(lizDict['edition.draw.activate'],'info',true);
+                            if(lizMap.checkMobile()){
+                                addEditionMessage(lizDict['edition.draw.activate.mobile'], 'info', true);
+                            }else{
+                                addEditionMessage(lizDict['edition.draw.activate'],'info',true);
+                            }
                         }
                         // Need to get geometry from form and add feature to the openlayer layer
                         var feat = getFeatureFromGeometryColumn();
@@ -1569,21 +1566,6 @@ OpenLayers.Geometry.pointOnSegment = function(point, segment) {
         }
 
         $('#edition-waiter').hide();
-
-        // Show the dock if needed
-        var btn = $('#button-edition');
-        var dockVisible = btn.parent().hasClass('active');
-
-        if (form.length != 0) {
-            $('#button-edition').show();
-            if (!lizMap.checkMobile()) {
-                if (!dockVisible)
-                    btn.click();
-            } else {
-                if (dockVisible)
-                    btn.click();
-            }
-        }
 
         // Hide popup
         if( $('#liz_layer_popup_close').length )

lizmap/modules/view/locales/en_US/dictionnary.UTF-8.properties
@@ -27,6 +27,7 @@ geolocation.survey.average=Average
 
 edition.title=Edition
 edition.draw.activate=Draw the feature you want to create on the map.
+edition.draw.activate.mobile=You can hide this form with the close button whenever you need to draw the geometry.
 edition.select.activate=Click on one of the layer's features to select it.
 edition.select.modify.activate=You can modify the feature you have selected, then submit.
 edition.datepicker.open=Open the calendar

commit ed3cf341b6fd0dcdbed4c008b6335cc2a764fc17

[Fix] Activate edition dock if not yet This fix a regression introduced by d72938dc05c4d7ac9ce3a8b7ae53cc205b444ebe

lizmap/www/assets/js/edition.js
@@ -1567,6 +1567,9 @@ OpenLayers.Geometry.pointOnSegment = function(point, segment) {
 
         $('#edition-waiter').hide();
 
+        // Activate edition dock if not yet
+        $('li.edition:not(.active) #button-edition').click();
+
         // Hide popup
         if( $('#liz_layer_popup_close').length )
             $('#liz_layer_popup_close').click();