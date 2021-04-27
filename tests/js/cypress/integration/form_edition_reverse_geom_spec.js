e836b5367a7aeef6dd01d3cf9b61804543e62d7c
Nicolas Boisteault <nboisteault@3liz.com>
2021-04-27 17:43:38 +0200

    [e2e] Reverse geom

---
+++ tests/js/cypress/integration/form_edition_reverse_geom_spec.js
@@ -0,0 +1,22 @@
+describe('Form edition reverse geom', function () {
+    beforeEach(function () {
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=reverse_geom')
+        // Todo wait for map to be fully loaded
+        cy.wait(1000)
+    })
+
+    it('must reverse geom', function () {
+        //Launch edition via the feature popup
+        cy.get('#map').click(678, 430)
+        cy.get('#popupcontent .popup-layer-feature-edit').click()
+
+        // Go to digitization tab
+        cy.get('#edition a[href="#tabdigitization"]').click()
+        
+        // Click reverse geom button
+        cy.get('#tabdigitization lizmap-reverse-geom').click()
+
+        // TODO: test screenshot before and after reverse
+    })
+
+})
