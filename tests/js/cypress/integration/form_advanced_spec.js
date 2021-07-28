52a3e33da677fd053633524509aa6717167af1e3
Nicolas Boisteault <nboisteault@3liz.com>
2021-07-28 12:32:27 +0200

    e2e: drill down and filter based on current geom

--- tests/js/cypress/integration/form_advanced_spec.js
+++ tests/js/cypress/integration/form_advanced_spec.js
@@ -5,6 +5,10 @@ describe('Advanced form', function () {
 
         cy.get('#button-edition').click()
         cy.get('#edition-draw').click()
+
+        cy.wait(300)
+        // Click on map as form needs a geometry
+        cy.get('#map').click(600, 250)
     })
 
     it('should toggle tab visibility when toggling checkbox', function () {
@@ -26,9 +30,6 @@ describe('Advanced form', function () {
     })
 
     it('should have expression constraint on field', function () {
-        cy.wait(200)
-        // Click on map as form needs a geometry
-        cy.get('#map').click(650, 400)
 
         // Type string not valid for expression constraint
         cy.get('#jforms_view_edition_website').type('a')
@@ -48,6 +49,40 @@ describe('Advanced form', function () {
         // A message should confirm form had been saved and form selector should be displayed back
         cy.get('#lizmap-edition-message').should('be.visible')
         cy.get('#edition-layer').should('be.visible')
+
+    })
+
+    it('should change selected quartier based on drawn point', function () {
+
+        cy.get('#jforms_view_edition_quartier option').should('have.length', 1)
+        cy.get('#jforms_view_edition_quartier option').should('have.text', 'HOPITAUX-FACULTES')
+
+        // Cancel and open form
+        cy.on('window:confirm', () => true);
+        cy.get('#jforms_view_edition__submit_cancel').click()
+        cy.get('#edition-draw').click()
+        cy.wait(300)
+
+        // Assert quartier value is good for another drawn point
+        cy.get('#map').click(600, 350)
+        cy.get('#jforms_view_edition_quartier option').should('have.text', 'MONTPELLIER CENTRE')
+
+        // nboisteault : I tried to drag and drop the point but did not achieve to have this behavior
+        // TODO: Try again with OpenLayers >= 6.x
+        // cy.get('#map')
+        //     .trigger('mousedown', 600, 250, { button: 0, force: true })
+        //     .trigger('mousemove', 600, 450, { button: 0, force: true })
+        //     .trigger('mouseup', 600, 450, { button: 0, force: true })
+
+        // cy.get('#OpenLayers_Layer_Vector_147 canvas')
+        //     .trigger('dragstart', 600, 250 )
+        //     .trigger('drop', 600, 450 )
+        //     .trigger('dragend', 600, 450 )
+
+        // cy.get('#map')
+        //     .trigger('pointerdown', 600, 250, { isPrimary: true})
+        //     .trigger('pointermove', 600, 450)
+        //     .trigger('pointerup', 600, 450)
 
     })
 })
