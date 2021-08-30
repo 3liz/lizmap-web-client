7f33744a872d933b229af0c4dc785ec42bdd9e2c
Laurent Jouanneau <dev@ljouanneau.com>
2021-08-30 12:37:53 +0200

    [tests] Install server plugins during the docker build

--- tests/add_server_plugins.sh
+++ tests/add_server_plugins.sh
@@ -3,3 +3,4 @@
 echo "Lizmap"
 wget https://packages.3liz.org/pub/lizmap-qgis-plugin/master/lizmap-qgis-plugin.master.zip
 unzip lizmap-qgis-plugin.master.zip -d qgis-server-plugins/
+rm lizmap-qgis-plugin.master.zip
