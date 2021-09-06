b2f56605ada055fe46ecfae15bda4a7fbe0127df
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-06 11:17:29 +0200

    e2e: redo selection tests Use lighter project than Montpellier

--- tests/qgis-projects/tests/selection.sql
+++ tests/qgis-projects/tests/selection.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.10 (Debian 11.10-1.pgdg100+1)
--- Dumped by pg_dump version 13.1 (Ubuntu 13.1-1.pgdg18.04+1)
+-- Dumped from database version 11.12 (Debian 11.12-1.pgdg100+1)
+-- Dumped by pg_dump version 13.4 (Ubuntu 13.4-1.pgdg18.04+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -54,10 +54,51 @@ ALTER SEQUENCE tests_projects.selection_id_seq OWNED BY tests_projects.selection
 
 
 --
+-- Name: selection_polygon; Type: TABLE; Schema: tests_projects; Owner: lizmap
+--
+
+CREATE TABLE tests_projects.selection_polygon (
+    id integer NOT NULL,
+    geom public.geometry(Polygon,2154)
+);
+
+
+ALTER TABLE tests_projects.selection_polygon OWNER TO lizmap;
+
+--
+-- Name: selection_polygon_id_seq; Type: SEQUENCE; Schema: tests_projects; Owner: lizmap
+--
+
+CREATE SEQUENCE tests_projects.selection_polygon_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE tests_projects.selection_polygon_id_seq OWNER TO lizmap;
+
+--
+-- Name: selection_polygon_id_seq; Type: SEQUENCE OWNED BY; Schema: tests_projects; Owner: lizmap
+--
+
+ALTER SEQUENCE tests_projects.selection_polygon_id_seq OWNED BY tests_projects.selection_polygon.id;
+
+
+--
 -- Name: selection id; Type: DEFAULT; Schema: tests_projects; Owner: lizmap
 --
 
 ALTER TABLE ONLY tests_projects.selection ALTER COLUMN id SET DEFAULT nextval('tests_projects.selection_id_seq'::regclass);
+
+
+--
+-- Name: selection_polygon id; Type: DEFAULT; Schema: tests_projects; Owner: lizmap
+--
+
+ALTER TABLE ONLY tests_projects.selection_polygon ALTER COLUMN id SET DEFAULT nextval('tests_projects.selection_polygon_id_seq'::regclass);
 
 
 --
@@ -71,10 +112,27 @@ COPY tests_projects.selection (id, "group", geom) FROM stdin;
 
 
 --
+-- Data for Name: selection_polygon; Type: TABLE DATA; Schema: tests_projects; Owner: lizmap
+--
+
+COPY tests_projects.selection_polygon (id, geom) FROM stdin;
+1	01030000206A08000001000000050000001CEC9B0989AE03419C58A1D8A97E2B410886917FAFB8064104FBD00802822B41FDDA4DDC5CB3064118886F09A5392C415C77FE088CA003414DAF0201413D2C411CEC9B0989AE03419C58A1D8A97E2B41
+2	01030000206A080000010000000500000035C390A8C6630D412034B65258892B416332D21140811041AB6D51FC538D2B41298191CC9A731041229009EAA92D2C41787F6CA730590D41B5BF8C8FD6312C4135C390A8C6630D412034B65258892B41
+\.
+
+
+--
 -- Name: selection_id_seq; Type: SEQUENCE SET; Schema: tests_projects; Owner: lizmap
 --
 
 SELECT pg_catalog.setval('tests_projects.selection_id_seq', 33, true);
+
+
+--
+-- Name: selection_polygon_id_seq; Type: SEQUENCE SET; Schema: tests_projects; Owner: lizmap
+--
+
+SELECT pg_catalog.setval('tests_projects.selection_polygon_id_seq', 2, true);
 
 
 --
@@ -83,6 +141,14 @@ SELECT pg_catalog.setval('tests_projects.selection_id_seq', 33, true);
 
 ALTER TABLE ONLY tests_projects.selection
     ADD CONSTRAINT selection_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: selection_polygon selection_polygon_pkey; Type: CONSTRAINT; Schema: tests_projects; Owner: lizmap
+--
+
+ALTER TABLE ONLY tests_projects.selection_polygon
+    ADD CONSTRAINT selection_polygon_pkey PRIMARY KEY (id);
 
 
 --
