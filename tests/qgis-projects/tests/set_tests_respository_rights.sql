743fcc78e199c8b3435a4f0451d051d7eb258885
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-06 15:29:08 +0200

    CI e2e: add rights for testsrepository to admins

--- tests/qgis-projects/tests/set_tests_respository_rights.sql
+++ tests/qgis-projects/tests/set_tests_respository_rights.sql
@@ -1,6 +1,13 @@
--- This script adds rights to test Lizmap projects without being logged in first (anonymous group)
+-- This script adds rights to test Lizmap projects...
 INSERT INTO lizmap.jacl2_rights 
-VALUES ('lizmap.repositories.view', '__anonymous', 'testsrepository', 0),
+VALUES 
+-- ...without being logged in first (anonymous group)
+('lizmap.repositories.view', '__anonymous', 'testsrepository', 0),
 ('lizmap.tools.displayGetCapabilitiesLinks', '__anonymous', 'testsrepository', 0),
 ('lizmap.tools.edition.use', '__anonymous', 'testsrepository', 0),
-('lizmap.tools.layer.export', '__anonymous', 'testsrepository', 0);
+('lizmap.tools.layer.export', '__anonymous', 'testsrepository', 0),
+-- ...for users in admins group
+('lizmap.repositories.view', 'admins', 'testsrepository', 0),
+('lizmap.tools.displayGetCapabilitiesLinks', 'admins', 'testsrepository', 0),
+('lizmap.tools.edition.use', 'admins', 'testsrepository', 0),
+('lizmap.tools.layer.export', 'admins', 'testsrepository', 0);
