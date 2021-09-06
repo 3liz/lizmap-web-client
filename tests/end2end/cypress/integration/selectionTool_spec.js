294bc2d142738af0c8a408040a08cc18416c7fc0
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-06 11:17:29 +0200

    e2e: redo selection tests Use lighter project than Montpellier

--- tests/end2end/cypress/integration/selectionTool_spec.js
+++ tests/end2end/cypress/integration/selectionTool_spec.js
@@ -13,6 +13,45 @@ describe('Selection tool', function () {
         cy.get('#button-selectiontool').click()
         cy.get('#selectiontool').should('not.be.visible')
     })
+
+    it('selects features intersecting a polygon', function () {
+        cy.get('#button-selectiontool').click()
+
+        // Activate polygon tool
+        cy.get('#selectiontool .digitizing-buttons .dropdown-toggle').first().click()
+        cy.get('#selectiontool .digitizing-polygon').click()
+
+        // Select single layer and intersects geom operator
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selection_polygon')
+        cy.get('lizmap-selection-tool .selection-geom-operator').select('intersects')
+
+
+        // It should select two features
+        cy.get('#map')
+            .click(200, 200)
+            .click(900, 200)
+            .dblclick(550, 650)
+
+        cy.get('.selectiontool-results').should(($div) => {
+            const text = $div.text()
+
+            expect(text).to.match(/^2/)
+        })
+
+        // It should not select any features
+        cy.get('#map')
+            .click(750, 350)
+            .click(700, 400)
+            .dblclick(700, 350)
+
+        cy.get('.selectiontool-results').should(($div) => {
+            const text = $div.text()
+
+            expect(text).not.to.match(/^[0-9]/)
+        })
+    })
+
+    // TODO : tests other geom operators, unselection...
 })
 
 describe('Selection tool connectected as admin', function () {
@@ -30,9 +69,14 @@ describe('Selection tool connectected as admin', function () {
 
     it('should select the single point on map which turns yellow', function () {
         cy.get('#button-selectiontool').click()
+
         // Activate polygon tool
         cy.get('#selectiontool .digitizing-buttons .dropdown-toggle').first().click()
         cy.get('#selectiontool .digitizing-polygon').click()
+
+        // Select single layer and intersects geom operator
+        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selection')
+        cy.get('lizmap-selection-tool .selection-geom-operator').select('intersects')
 
         cy.intercept('GET', '*REQUEST=GetMap*').as('new-selection')
 
