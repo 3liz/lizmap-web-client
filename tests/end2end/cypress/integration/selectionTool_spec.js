32fa4e83c241679a3b06d32299dfc2ec776c80a9
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-06 09:38:20 +0200

    e2e: selection tool as admin This test intercepts GetMap request to compare its image response to a fixture

--- tests/end2end/cypress/integration/selectionTool_spec.js
+++ tests/end2end/cypress/integration/selectionTool_spec.js
@@ -1,317 +1,65 @@
 describe('Selection tool', function () {
-    before(function () {
-        // runs once before the first test in this block
-        cy.visit('/index.php/view/map/?repository=montpellier&project=montpellier')
-        // Todo wait for map to be fully loaded
-        cy.wait(30000)
-
-        cy.get('#button-selectiontool').click()
-
-        // Activate polygon tool
-        cy.get('#selectiontool .digitizing-buttons .dropdown-toggle').first().click()
-        cy.get('#selectiontool .digitizing-polygon').click()
-    })
-
-    // Refresh before. Use intersects operator as default
     beforeEach(function () {
-        cy.get('.selectiontool-type-refresh').click()
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('intersects')
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=selection')
     })
 
-    it('opens selection tool', function () {
+    it('should toggle selection tool', function () {
+        cy.get('#button-selectiontool').click()
         cy.get('#selectiontool').should('be.visible')
 
         cy.get('.selectiontool-unselect').should('have.attr', 'disabled')
         cy.get('.selectiontool-filter').should('have.attr', 'disabled')
-    })
-
-    it('selects features within a polygon', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('Quartiers')
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('within')
-
-        // It should not select any features
-        cy.get('#map')
-            .click(750, 350)
-            .click(700, 400)
-            .dblclick(700, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).not.to.match(/^[0-9]/)
-        })
-
-        // It should select only one feature
-        cy.get('#map')
-            .click(800, 200)
-            .click(850, 450)
-            .click(550, 450)
-            .dblclick(550, 200)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^1/)
-        })
-    })
-
-    it('selects features overlapping a polygon', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('Quartiers')
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('overlaps')
-
-        // It should not select any features
-        cy.get('#map')
-            .click(750, 350)
-            .click(700, 400)
-            .dblclick(700, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).not.to.match(/^[0-9]/)
-        })
-
-        // It should select only one feature
-        cy.get('#map')
-            .click(800, 350)
-            .click(750, 400)
-            .dblclick(750, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^1/)
-        })
-    })
-
-    it('selects features containing a polygon', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('Quartiers')
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('contains')
-
-        // It should select only one feature
-        cy.get('#map')
-            .click(750, 350)
-            .click(700, 400)
-            .dblclick(700, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-            expect(text).to.match(/^1/)
-        })
-
-        // It should not select any features
-        cy.get('#map')
-            .click(800, 350)
-            .click(700, 400)
-            .dblclick(700, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-            expect(text).not.to.match(/^[0-9]/)
-        })
-    })
-
-    it('selects features crossing a polygon', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('tramway')
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('crosses')
-
-        // It should select two features
-        cy.get('#map')
-            .click(750, 150)
-            .click(700, 200)
-            .dblclick(700, 150)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-            expect(text).to.match(/^2/)
-        })
-    })
-
-    it('selects features disjointing a polygon', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('Quartiers')
-        cy.get('lizmap-selection-tool .selection-geom-operator').select('disjoint')
-
-        // It should select two features
-        cy.get('#map')
-            .click(800, 350)
-            .click(700, 400)
-            .dblclick(700, 350)
-
-        cy.wait(500)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-            expect(text).to.match(/^6/)
-        })
-    })
-
-    // TODO : test touches geometric operator
-    it('selects one feature then two more features then unselects one with polygon', function () {
-        // Select one feature...
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('tramstop')
-
-        cy.get('#map')
-            .click(750, 150)
-            .click(700, 200)
-            .dblclick(700, 150)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^1/)
-        })
-
-        cy.get('.selectiontool-unselect').should('not.have.attr', 'disabled')
-        cy.get('.selectiontool-filter').should('not.have.attr', 'disabled')
-
-        // ...then two more features...
-        cy.get('.selectiontool-type-plus').click()
-
-        cy.get('#map')
-            .click(750, 180)
-            .click(700, 210)
-            .dblclick(750, 220)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^3/)
-        })
-
-        // ...then unselects one feature
-        cy.get('.selectiontool-type-minus').click()
-
-        cy.get('#map')
-            .click(750, 150)
-            .click(700, 200)
-            .dblclick(700, 150)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^2/)
-        })
-    })
-
-    it('selects two features then unselects all feature', function () {
-        // Select two feature...
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('tramstop')
-
-        cy.get('#map')
-            .click(750, 180)
-            .click(700, 210)
-            .dblclick(750, 220)
-
-        // ..then unselects all
-        cy.get('.selectiontool-unselect').click()
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).not.to.match(/^[0-9]/)
-        })
-    })
-
-    it('selects one feature then revert selection', function () {
-        // Select one feature...
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('points of interest')
-
-        cy.get('#map')
-            .click(650, 200)
-            .click(600, 230)
-            .dblclick(650, 240)
-
-        // ...then invert selection
-        cy.get('.selectiontool-invert').click()
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^5/)
-        })
-    })
-
-    it('selects multiple selectable layers', function () {
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selectable-layers')
-
-        cy.get('#map')
-            .click(750, 150)
-            .click(700, 200)
-            .dblclick(800, 300)
-
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
-
-            expect(text).to.match(/^10/)
-        })
-    })
-
-    it('filters one feature from attribute table', function () {
-        cy.get('#button-attributeLayers').click()
-        cy.get('#attribute-layer-list-table button[value="tramstop"]').click({ force: true })
-
-        // this event will automatically be unbound when this
-        // test ends because it's attached to 'cy'
-        // TODO : fix the error in datatables "Uncaught TypeError: Cannot read property 'style' of undefined"
-        cy.on('uncaught:exception', (err, runnable) => {
-
-            // return false to prevent the error from
-            // failing this test
-            return false
-        })
-
-        cy.get('#attribute-layer-table-tramstop .attribute-layer-feature-select').first().click({ force: true })
-
-        cy.get('lizmap-selection-invert button').should('not.have.attr', 'disabled')
-
-        cy.get('.btn-filter-attributeTable').click({ force: true })
-
-        cy.wait(1000)
-
-        cy.get('.selectiontool-filter').should('not.have.attr', 'disabled')
-        cy.get('.selectiontool-filter').should('have.class', 'active')
-    })
-
-    it('selects multiple selectable and visible layers', function () {
-        // Stub getProjectConfig request to have a new Lizmap configuration with less visible layers
-        cy.server()
-        cy.route('GET', 'index.php/lizmap/service/getProjectConfig?repository=montpellier&project=montpellier', 'fixture:getProjectConfig.json')
-        // runs once before the first test in this block
-        cy.visit('/index.php/view/map/?repository=montpellier&project=montpellier')
-        // Todo wait for map to be fully loaded
-        cy.wait(3000)
 
         cy.get('#button-selectiontool').click()
+        cy.get('#selectiontool').should('not.be.visible')
+    })
+})
 
-        cy.get('lizmap-selection-tool .selectiontool-layer-list').select('selectable-visible-layers')
+describe('Selection tool connectected as admin', function () {
+    beforeEach(function () {
+        // Login as admin and get redirected to selection project
+        // TODO: log with request() and not via UI
+        cy.visit('/admin.php/auth/login/?auth_url_return=%2Findex.php%2Fview%2Fmap%2F%3Frepository%3Dtestsrepository%26project%3Dselection')
 
+        cy.get('#jforms_jcommunity_login_auth_login').type('admin')
+        cy.get('#jforms_jcommunity_login_auth_password').type('admin')
+        cy.get('form').submit()
+
+        cy.get('#info-user-login').should('have.text', 'admin')
+    })
+
+    it('should select the single point on map which turns yellow', function () {
+        cy.get('#button-selectiontool').click()
         // Activate polygon tool
         cy.get('#selectiontool .digitizing-buttons .dropdown-toggle').first().click()
         cy.get('#selectiontool .digitizing-polygon').click()
 
+        cy.intercept('GET', '*REQUEST=GetMap*').as('new-selection')
+
         cy.get('#map')
-            .click(750, 150)
-            .click(700, 200)
-            .dblclick(800, 300)
+            .click(380, 280)
+            .click(400, 380)
+            .dblclick(500, 380)
 
-        cy.get('.selectiontool-results').should(($div) => {
-            const text = $div.text()
+        cy.wait('@new-selection')
 
-            expect(text).to.match(/^9/)
+        cy.get('@new-selection').should(({ request, response }) => {
+
+            function _arrayBufferToBase64(buffer) {
+                var binary = '';
+                var bytes = new Uint8Array(buffer);
+                var len = bytes.byteLength;
+                for (var i = 0; i < len; i++) {
+                    binary += String.fromCharCode(bytes[i]);
+                }
+                return window.btoa(binary);
+            }
+            const responseBodyAsBase64 = _arrayBufferToBase64(response.body)
+
+            cy.fixture('images/selection_yellow.png').then((image) => {
+                // image encoded as base64
+                expect(image, 'expect selection in yellow').to.equal(responseBodyAsBase64)
+            })
         })
     })
 })
