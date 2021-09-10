4fce2ed7308db8fe6af1c1aa75a341133cba3550
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-10 14:34:26 +0200

    Remove JS resizing project thumbnails

--- lizmap/www/assets/js/view.js
+++ lizmap/www/assets/js/view.js
@@ -1,19 +1,3 @@
-(function ($) {
-    $.fn.uniformHeight = function () {
-        var maxHeight = 0,
-            wrapper,
-            wrapperHeight;
-        return this.each(function () {
-            // Applying a wrapper to the contents of the current element to get reliable height
-            wrapper = $(this).wrapInner('<div class="wrapper" />').children('.wrapper');
-            wrapperHeight = wrapper.outerHeight();
-            maxHeight = Math.max(maxHeight, wrapperHeight);
-            // Remove the wrapper
-            wrapper.children().unwrap();
-        }).height(maxHeight);
-    }
-})(jQuery);
-
 var addDescriptionSlider = function(){
     $('.liz-project-img').parent().mouseenter(function(){
       var self = $(this);
@@ -26,15 +10,6 @@ var addDescriptionSlider = function(){
       var self = $(this);
       window.location = self.parent().find('a.liz-project-view').attr('href');
       return false;
-    });
-}
-
-var resizeThumbnails = function(){
-    $(".thumbnail h5").height('auto');
-    $('.thumbnails').height('auto');
-    $('.thumbnails').each(function () {
-        $(this).find('.thumbnail h5').height(Math.max.apply(null, $(this).find('.thumbnail h5').map(function() { return $(this).height(); })));
-        $(this).find('.thumbnail').uniformHeight();
     });
 }
 
@@ -314,8 +289,5 @@ var searchProjects = function(){
 
 window.addEventListener('load', function () {
     addDescriptionSlider();
-    resizeThumbnails();
     searchProjects();
 });
-
-window.addEventListener('resize', resizeThumbnails);
