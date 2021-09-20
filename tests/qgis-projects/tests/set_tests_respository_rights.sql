9c15a4d3c8b882d64c22defcf3a0b046dc76b9cd
Nicolas Boisteault <nboisteault@3liz.com>
2021-09-20 17:29:57 +0200

    e2e: add missing rights for filter by polygon test

--- tests/qgis-projects/tests/set_tests_respository_rights.sql
+++ tests/qgis-projects/tests/set_tests_respository_rights.sql
@@ -1,4 +1,24 @@
--- This script adds rights to test Lizmap projects...
+-- This script adds users, groups and rights to test Lizmap projects...
+
+-- Groups
+INSERT INTO lizmap.jacl2_group(
+	id_aclgrp, name, grouptype, ownerlogin)
+	VALUES ('group_a', 'group_a', 0, null),
+    ('__priv_user_in_group_a', 'user_in_group_a', 2, 'user_in_group_a');
+
+-- Users
+INSERT INTO lizmap.jlx_user(
+	usr_login, usr_email, usr_password, status, create_date)
+	VALUES ('user_in_group_a', 'user_in_group_a@nomail.nomail', '$2y$10$d2KZfxeYJP0l3YbNyDMZYe2vGSA3JWa8kFJSdecmSEIqInjnunTJ.', 1, NOW());
+
+-- Users in Groups
+INSERT INTO lizmap.jacl2_user_group(
+	login, id_aclgrp)
+	VALUES ('user_in_group_a', 'group_a'),
+	('user_in_group_a', '__priv_user_in_group_a'),
+	('user_in_group_a', 'users');
+
+-- Rights
 INSERT INTO lizmap.jacl2_rights 
 VALUES 
 -- ...without being logged in first (anonymous group)
@@ -10,4 +30,9 @@ VALUES
 ('lizmap.repositories.view', 'admins', 'testsrepository', 0),
 ('lizmap.tools.displayGetCapabilitiesLinks', 'admins', 'testsrepository', 0),
 ('lizmap.tools.edition.use', 'admins', 'testsrepository', 0),
-('lizmap.tools.layer.export', 'admins', 'testsrepository', 0);
+('lizmap.tools.layer.export', 'admins', 'testsrepository', 0),
+-- ...for users in group_a group
+('lizmap.repositories.view', 'group_a', 'testsrepository', 0),
+('lizmap.tools.displayGetCapabilitiesLinks', 'group_a', 'testsrepository', 0),
+('lizmap.tools.edition.use', 'group_a', 'testsrepository', 0),
+('lizmap.tools.layer.export', 'group_a', 'testsrepository', 0);
