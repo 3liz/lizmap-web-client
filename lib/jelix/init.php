f2abcc9546c33ebd6aee1bcd8042b0c5b653bc6a
Laurent Jouanneau <dev@ljouanneau.com>
2021-10-25 16:24:46 +0200

    Update Jelix 1.6

--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -16,7 +16,7 @@
  * Version number of Jelix
  * @name  JELIX_VERSION
  */
-define ('JELIX_VERSION', '1.6.34-pre');
+define ('JELIX_VERSION', '1.6.35-pre');
 
 /**
  * base of namespace path used in xml files of jelix
