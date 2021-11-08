c378a51e998c46a5276e58b0bbe1aacbde7fb455
Laurent Jouanneau <dev@ljouanneau.com>
2021-11-08 23:00:13 +0100

    Configuration: fix parameters for the SAML module

--- lizmap/var/config/mainconfig.ini.php
+++ lizmap/var/config/mainconfig.ini.php
@@ -314,10 +314,5 @@ useJAuthDbAdminRights=on
 
 ;------- some parameters for the "saml" module
 [saml:sp]
-; list of dao properties that can be used for mapping and that are required
-daoRequiredPropertiesForMapping="login,email"
-
-;list of dao properties that can be used for mapping and that are optional
-;If empty, all dao properties are optional, except if daoRequiredPropertiesForMapping
-;is not empty.
-daoOptionalPropertiesForMapping="firstname,lastname,phonenumber"
+; list of dao properties that can be used for mapping
+daoPropertiesForMapping="login,email,firstname,lastname,phonenumber"
