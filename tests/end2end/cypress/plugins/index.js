00700e0c6957b39fce9d22c11c2b324104d173c7
Nicolas Boisteault <nboisteault@3liz.com>
2021-11-27 11:02:49 +0100

    e2e: update Cypress to 9.1.0. Remove cypress-image-snapshot

--- tests/end2end/cypress/plugins/index.js
+++ tests/end2end/cypress/plugins/index.js
@@ -19,12 +19,3 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
-
-// Adds for Cypress Image Snapshot
-const {
-  addMatchImageSnapshotPlugin,
-} = require('cypress-image-snapshot/plugin');
-
-module.exports = (on, config) => {
-  addMatchImageSnapshotPlugin(on, config);
-};
