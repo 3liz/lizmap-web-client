af07ee72a26fe2964bbd21ad765c732a29422e82
Michael Douchin <mdouchin@3liz.com>
2021-12-14 15:13:55 +0100

    Server metadata - Get more information from Lizmap plugin & display them in the admin panel

--- lizmap/modules/view/controllers/app.classic.php
+++ lizmap/modules/view/controllers/app.classic.php
@@ -19,44 +19,9 @@ class appCtrl extends jController
     public function metadata()
     {
         $rep = $this->getResponse('json');
-        $data = array();
 
-        // Get Lizmap version from project.xml
-        $xmlPath = jApp::appPath('project.xml');
-        $xmlLoad = simplexml_load_file($xmlPath);
-
-        // Version
-        $data['info'] = array();
-        $data['info']['version'] = (string) $xmlLoad->info->version;
-        $data['info']['date'] = (string) $xmlLoad->info->version->attributes()->date;
-
-        // Dependencies
-        $data['dependencies'] = array();
-        $data['dependencies']['jelix'] = array();
-        $data['dependencies']['jelix']['minversion'] = (string) $xmlLoad->dependencies->jelix->attributes()->minversion;
-        $data['dependencies']['jelix']['maxversion'] = (string) $xmlLoad->dependencies->jelix->attributes()->maxversion;
-
-        // Try a request to QGIS Server
-        $data['qgis_server'] = array();
-        $params = array(
-            'service' => 'WMS',
-            'request' => 'GetCapabilities',
-        );
-        $url = \Lizmap\Request\Proxy::constructUrl($params, lizmap::getServices());
-        list($resp, $mime, $code) = \Lizmap\Request\Proxy::getRemoteData($url);
-        if (preg_match('#ServerException#i', $resp)
-            || preg_match('#ServiceExceptionReport#i', $resp)
-            || preg_match('#WMS_Capabilities#i', $resp)) {
-            $data['qgis_server']['test'] = 'OK';
-        } else {
-            $data['qgis_server']['test'] = 'ERROR';
-        }
-        $data['qgis_server']['mime_type'] = $mime;
-        if (jAcl2::check('lizmap.admin.access')) {
-            $data['qgis_server']['http_code'] = $code;
-            $data['qgis_server']['response'] = $resp;
-        }
-
+        $server = new \Lizmap\Server\Server();
+        $data = $server->getMetadata();
         $rep->data = $data;
 
         return $rep;
