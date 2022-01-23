39c7598aeb89869a74ec7e68150c83c294873e22
arqvector <sergiuspinheiro@gmail.com>
2022-01-23 19:51:01 +0000

    Update
    PHP-CS-Fixer

--- lizmap/modules/admin/graph/i.php
+++ lizmap/modules/admin/graph/i.php
@@ -1,6 +1,4 @@
 <?php
 // Connecting, selecting database
 $dbconn = pg_connect("host=localhost dbname=lizmap user=lizmap_view password=password")
-    or die('Could not connect: ' . pg_last_error());
-?>
-
+    or die('Could not connect: '.pg_last_error());
