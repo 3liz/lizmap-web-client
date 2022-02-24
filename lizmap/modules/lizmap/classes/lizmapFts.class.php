ab5df1843cb5d4319d800e450dfdfcf44164699c
rldhont <rldhont@gmail.com>
2022-02-24 16:16:06 +0100

    Search without private group

--- lizmap/modules/lizmap/classes/lizmapFts.class.php
+++ lizmap/modules/lizmap/classes/lizmapFts.class.php
@@ -61,7 +61,13 @@ class lizmapFts
         $isConnected = jAuth::isConnected();
         if ($isConnected) {
             // Ok if any group matches
-            $userGroups = jAcl2DbUserGroup::getGroups();
+            $userGroups = array();
+            $uGroups = jAcl2DbUserGroup::getGroups();
+            foreach ($uGroups as $uGroup) {
+                if (substr($uGroup, 0, 7) != '__priv_') {
+                    $userGroups[$uGroup] = $uGroup;
+                }
+            }
             foreach ($userGroups as $g) {
                 $sql .= " OR item_filter = '".$g."'";
             }
