3da89cdf9b58f15b88d0bc63b8dd23c5c8ec9cbf
rldhont <rldhont@gmail.com>
2022-04-11 15:02:42 +0200

    [Bugfix] WFS GetFeature request from Postgres - add BBox
    The Feature Bounding box was missing when lizmap building the GeoJSON from Postgres.

--- lizmap/modules/lizmap/lib/Request/WFSRequest.php
+++ lizmap/modules/lizmap/lib/Request/WFSRequest.php
@@ -781,11 +781,24 @@ class WFSRequest extends OGCRequest
         }
 
         if (!empty($geosql)) {
+            // Define BBOX SQL
+            $bboxsql = 'ST_Envelope(lg.geosource::geometry)';
             // For new QGIS versions, export into EPSG:4326
             $lizservices = $this->services;
             if (version_compare($lizservices->qgisServerVersion, '2.18', '>=')) {
                 $geosql = 'ST_Transform('.$geosql.', 4326)';
+                $bboxsql = 'ST_Transform('.$bboxsql.', 4326)';
             }
+
+            // Transform BBOX into JSON
+            $sql .= '
+                        array_to_json(ARRAY[
+                            ST_XMin('.$bboxsql.'),
+                            ST_YMin('.$bboxsql.'),
+                            ST_XMax('.$bboxsql.'),
+                            ST_YMax('.$bboxsql.')
+                        ]) As bbox,
+            ';
 
             // Transform into GeoJSON
             $sql .= '
