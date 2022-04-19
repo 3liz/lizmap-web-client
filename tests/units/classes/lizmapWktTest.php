d648bd1484f68cec18f0b9a0176fc176dd1d0970
rldhont <rldhont@gmail.com>
2022-04-19 17:45:06 +0200

    [Bugfix] Lizmap WKT parser: support Multi
    Multi geometries like multi point, multi linestring and multi polygon were not supported.
    So expression based on layer with multi geometries does not work at all.

--- tests/units/classes/lizmapWktTest.php
+++ tests/units/classes/lizmapWktTest.php
@@ -12,6 +12,48 @@ class lizmapWktTest extends TestCase {
         $this->assertEquals($geom['type'], 'Point');
         $this->assertArrayHasKey('coordinates', $geom);
         $this->assertCount(2, $geom['coordinates']);
+
+        $wkt = 'POINT(30 10)';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'Point');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(2, $geom['coordinates']);
+    }
+
+    function testMultiPoint() {
+        $wkt = 'MULTIPOINT ((30 10))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPoint');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(2, $geom['coordinates'][0]);
+
+        $wkt = 'MULTIPOINT ((30 10), (40 40))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPoint');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(2, $geom['coordinates']);
+        $this->assertCount(2, $geom['coordinates'][0]);
+        $this->assertCount(2, $geom['coordinates'][1]);
+
+        $wkt = 'MULTIPOINT((30 10))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPoint');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(2, $geom['coordinates'][0]);
     }
 
     function testLineString() {
@@ -23,6 +65,48 @@ class lizmapWktTest extends TestCase {
         $this->assertEquals($geom['type'], 'LineString');
         $this->assertArrayHasKey('coordinates', $geom);
         $this->assertCount(3, $geom['coordinates']);
+
+        $wkt = 'LINESTRING(30 10, 10 30, 40 40)';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'LineString');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(3, $geom['coordinates']);
+    }
+
+    function testMultiLineString() {
+        $wkt = 'MULTILINESTRING ((30 10, 10 30, 40 40))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiLineString');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(3, $geom['coordinates'][0]);
+
+        $wkt = 'MULTILINESTRING ((30 10, 10 30, 40 40), (20 30, 35 35))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiLineString');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(2, $geom['coordinates']);
+        $this->assertCount(3, $geom['coordinates'][0]);
+        $this->assertCount(2, $geom['coordinates'][1]);
+
+        $wkt = 'MULTILINESTRING((30 10, 10 30, 40 40))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiLineString');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(3, $geom['coordinates'][0]);
     }
 
     function testPolygon() {
@@ -46,5 +130,51 @@ class lizmapWktTest extends TestCase {
         $this->assertCount(2, $geom['coordinates']);
         $this->assertCount(5, $geom['coordinates'][0]);
         $this->assertCount(4, $geom['coordinates'][1]);
+
+        $wkt = 'POLYGON((30 10, 40 40, 20 40, 10 20, 30 10))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'Polygon');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(5, $geom['coordinates'][0]);
+    }
+
+    function testMultiPolygon() {
+        $wkt = 'MULTIPOLYGON (((30 10, 40 40, 20 40, 10 20, 30 10)))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPolygon');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(1, $geom['coordinates'][0]);
+        $this->assertCount(5, $geom['coordinates'][0][0]);
+
+        $wkt = 'MULTIPOLYGON ((35 10, 45 45, 15 40, 10 20, 35 10), (20 30, 35 35, 30 20, 20 30))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPolygon');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(2, $geom['coordinates'][0]);
+        $this->assertCount(5, $geom['coordinates'][0][0]);
+        $this->assertCount(4, $geom['coordinates'][0][1]);
+
+        $wkt = 'MULTIPOLYGON(((30 10, 40 40, 20 40, 10 20, 30 10)))';
+        $geom = lizmapWkt::parse($wkt);
+
+        $this->assertNotNull($geom);
+        $this->assertArrayHasKey('type', $geom);
+        $this->assertEquals($geom['type'], 'MultiPolygon');
+        $this->assertArrayHasKey('coordinates', $geom);
+        $this->assertCount(1, $geom['coordinates']);
+        $this->assertCount(1, $geom['coordinates'][0]);
+        $this->assertCount(5, $geom['coordinates'][0][0]);
     }
 }
