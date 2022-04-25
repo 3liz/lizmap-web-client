d9cb95e027d48b8efb26e7164beaeebb6f4bbec1
Laurent Jouanneau <dev@ljouanneau.com>
2022-04-25 11:56:06 +0200

    Fix the cache duration of translated string for JS
    One year is too high.

--- lizmap/modules/view/controllers/translate.classic.php
+++ lizmap/modules/view/controllers/translate.classic.php
@@ -3,7 +3,7 @@
  * Service to provide translation dictionnary.
  *
  * @author    3liz
- * @copyright 2011 3liz
+ * @copyright 2011-2022 3liz
  *
  * @see      http://3liz.com
  *
@@ -14,15 +14,17 @@ class translateCtrl extends jController
     /**
      * Get text/javascript containing all translation for the dictionnary.
      *
-     * @param string $lang Language. Ex: fr_FR (optional)
+     * @urlparam string $lang Language. Ex: fr_FR (optional)
      *
-     * @return javaScript
+     * @return jResponseBinary
      */
     public function index()
     {
+        /** @var jResponseBinary $rep */
         $rep = $this->getResponse('binary');
         $rep->mimeType = 'text/javascript';
         $rep->doDownload = false;
+        $rep->setExpires('+1 month');
 
         $lang = $this->param('lang');
 
@@ -51,13 +53,14 @@ class translateCtrl extends jController
     /**
      * Get JSON containing all translation for a given jelix property file.
      *
-     * @param string $property Name of the property file. Ex: map if searched file is map.UTF-8.properties
-     * @param string $lang     Language. Ex: fr_FR (optional)
+     * @urlparam string $property Name of the property file. Ex: map if searched file is map.UTF-8.properties
+     * @urlparam string $lang     Language. Ex: fr_FR (optional)
      *
-     * @return binary object The image for this project
+     * @return jResponseJson
      */
     public function getDictionary()
     {
+        /** @var jResponseJson $rep */
         $rep = $this->getResponse('json');
 
         // Get the property file
