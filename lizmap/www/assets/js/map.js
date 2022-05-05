59fcbcb5b4914f7306c8cb25b09bad43d475c443
Nicolas Boisteault <nboisteault@3liz.com>
2022-05-05 17:57:10 +0200

    Remove legacy code in downloadFile()

--- lizmap/www/assets/js/map.js
+++ lizmap/www/assets/js/map.js
@@ -4788,38 +4788,27 @@ var lizMap = function() {
                   var matches = filenameRegex.exec(disposition);
                   if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
               }
-              var type = xhr.getResponseHeader('Content-Type');
 
-              var blob = typeof File === 'function'
-                  ? new File([this.response], filename, { type: type })
-                  : new Blob([this.response], { type: type });
-              if (typeof window.navigator.msSaveBlob !== 'undefined') {
-                  // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-                  window.navigator.msSaveBlob(blob, filename);
-              } else {
-                  var URL = window.URL || window.webkitURL;
-                  var downloadUrl = URL.createObjectURL(blob);
+              const type = xhr.getResponseHeader('Content-Type');
+              const blob = new File([this.response], filename, { type: type });
+              const downloadUrl = URL.createObjectURL(blob);
 
-                  if (filename && type != 'application/pdf') {
-                    // use HTML5 a[download] attribute to specify filename
-                    var a = document.createElement("a");
-                    // safari doesn't support this yet
-                    if (typeof a.download === 'undefined') {
-                        window.open(downloadUrl, filename);
-                    } else {
-                        a.href = downloadUrl;
-                        a.download = filename;
-                        a.rel = 'noopener';
-                        a.target = '_blank';
-                        //document.body.appendChild(a);
-                        a.dispatchEvent(new MouseEvent('click'));
-                    }
-                  } else {
+              if (filename) {
+                // use HTML5 a[download] attribute to specify filename
+                const a = document.createElement('a');
+                // old safari doesn't support this
+                if (typeof a.download === 'undefined') {
                     window.open(downloadUrl, filename);
-                  }
-
-                  setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                } else {
+                    a.href = downloadUrl;
+                    a.download = filename;
+                    a.dispatchEvent(new MouseEvent('click'));
+                }
+              } else {
+                window.open(downloadUrl);
               }
+
+              setTimeout(() => URL.revokeObjectURL(downloadUrl), 100); // cleanup
           }
 
           // Note 31/01/2022
