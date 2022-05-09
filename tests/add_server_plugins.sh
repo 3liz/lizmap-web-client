6a4aaf5ed569414ee7d0a607b6ea62d41ce3bfc8
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-05-09 17:41:46 +0200

    Tests - Use the Lizmap server plugin

--- tests/add_server_plugins.sh
+++ tests/add_server_plugins.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Lizmap"
-wget https://packages.3liz.org/pub/lizmap-qgis-plugin/master/lizmap-qgis-plugin.master.zip
-unzip lizmap-qgis-plugin.master.zip -d qgis-server-plugins/
-rm lizmap-qgis-plugin.master.zip
+wget https://packages.3liz.org/pub/lizmap-server-qgis-plugin/lizmap_server.master.zip
+unzip -o lizmap_server.master.zip -d qgis-server-plugins/
+rm lizmap_server.master.zip
+
+# Remove legacy package for a few weeks
+if [ -d qgis-server-plugins/lizmap/ ]; then
+  rm -rf qgis-server-plugins/lizmap
+fi
