9cdc2dc053e7021ef2442dfd63efe2264a9f68c3
rldhont <rldhont@gmail.com>
2022-05-19 14:20:46 +0200

    [Tests] Continuous integration for cache manual tests

--- tests/end2end/cypress/integration/cmdline-wmts-ghaction.js
+++ tests/end2end/cypress/integration/cmdline-wmts-ghaction.js
@@ -42,13 +42,15 @@ describe('WMTS command line', function () {
 
     })
 
-    it('lizmap~wmts:seeding success', function () {
+    it('lizmap~wmts:seeding -dry-run success', function () {
+        // Get tiles number for zoom level 10
         cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f -dry-run testsrepository cache Quartiers EPSG:3857 10 10')
             .its('stdout')
             .should('contain', 'The TileMatrixSet \'EPSG:3857\'!')
             .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "10"')
             .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "10"')
 
+        // Get tiles number for zoom level 10 and the project extent
         //417094.94691622, 5398163.2080343, 445552.52931222, 5412833.0143902
         cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f -dry-run -bbox 417094.94691622,5398163.2080343,445552.52931222,5412833.0143902 testsrepository cache Quartiers EPSG:3857 10 10')
             .its('stdout')
@@ -56,19 +58,138 @@ describe('WMTS command line', function () {
             .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "10"')
             .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "10"')
 
-        // out of 417094.94691622, 5398163.2080343, 445552.52931222, 5412833.0143902
+        // Get tiles number for zoom level 10 and bbox out of the project extent
         cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f -dry-run -bbox 617094.94691622,5598163.2080343,645552.52931222,5612833.0143902 testsrepository cache Quartiers EPSG:3857 10 10')
             .its('stdout')
             .should('contain', 'The TileMatrixSet \'EPSG:3857\'!')
             .should('not.contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "10"')
             .should('contain', '0 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "10"')
 
-        // small 1x1 bbox in 417094.94691622, 5398163.2080343, 445552.52931222, 5412833.0143902
+        // Get tiles number for zoom level 10 and small bbox (1x1) in the project extent
         cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f -dry-run -bbox 433997,5405228,433997,5405228 testsrepository cache Quartiers EPSG:3857 10 10')
             .its('stdout')
             .should('contain', 'The TileMatrixSet \'EPSG:3857\'!')
             .should('contain', '1 tiles to generate for "Quartiers" "EPSG:3857" "10"')
             .should('contain', '1 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "10"')
+    })
+
+    it('lizmap~wmts:cleaning & lizmap~wmts:seeding', function () {
+        // Cleaning the cache
+        cy.exec('./../lizmap-ctl script lizmap~wmts:cleaning -v testsrepository cache Quartiers', {failOnNonZeroExit: false})
+            .its('stdout')
+            .should('contain', 'Start cleaning')
+
+        // Check that the cache is empty
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '0')
+
+        // Seeding the cache for zoom level 10
+        cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f testsrepository cache Quartiers EPSG:3857 10 10')
+            .its('stdout')
+            .should('contain', 'The TileMatrixSet \'EPSG:3857\'!')
+            .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "10"')
+            .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "10"')
+            .should('contain', 'Start generation')
+            .should('contain', 'End generation')
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '4')
+
+        // Seeding the cache for zoom level 10 to 15
+        cy.exec('./../lizmap-ctl script lizmap~wmts:seeding -v -f testsrepository cache Quartiers EPSG:3857 10 15')
+            .its('stdout')
+            .should('contain', 'The TileMatrixSet \'EPSG:3857\'!')
+            .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "10"')
+            .should('contain', '4 tiles to generate for "Quartiers" "EPSG:3857" "11"')
+            .should('contain', '6 tiles to generate for "Quartiers" "EPSG:3857" "12"')
+            .should('contain', '16 tiles to generate for "Quartiers" "EPSG:3857" "13"')
+            .should('contain', '42 tiles to generate for "Quartiers" "EPSG:3857" "14"')
+            .should('contain', '156 tiles to generate for "Quartiers" "EPSG:3857" "15"')
+            .should('contain', '228 tiles to generate for "Quartiers" "EPSG:3857" between "10" and "15"')
+            .should('contain', 'Start generation')
+            .should('contain', 'End generation')
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '228')
+
+        // Go to the map
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=cache&bbox=425367.933686%2C5402026.205439%2C437311.219354%2C5408953.311127&crs=EPSG%3A3857')
+
+        cy.wait(1000)
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '228')
+
+        // Login as admin
+        cy.visit('admin.php/auth/login')
+
+        cy.wait(1000)
+
+        cy.get('#jforms_jcommunity_login_auth_login').type('admin')
+        cy.get('#jforms_jcommunity_login_auth_password').type('admin')
+        cy.get('form').submit()
+
+        cy.wait(1000)
+
+        // Go to the map
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=cache&bbox=425367.933686%2C5402026.205439%2C437311.219354%2C5408953.311127&crs=EPSG%3A3857')
+
+        cy.wait(1000)
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '228')
+
+        // Logout
+        cy.visit('admin.php/auth/login/out')
+
+        cy.wait(1000)
+
+        // Login as user_in_group_a
+        cy.visit('admin.php/auth/login')
+
+        cy.wait(1000)
+
+        cy.get('#jforms_jcommunity_login_auth_login').type('user_in_group_a')
+        cy.get('#jforms_jcommunity_login_auth_password').type('admin')
+        cy.get('form').submit()
+
+        cy.wait(1000)
+
+        // Go to the map
+        cy.visit('/index.php/view/map/?repository=testsrepository&project=cache&bbox=425367.933686%2C5402026.205439%2C437311.219354%2C5408953.311127&crs=EPSG%3A3857')
+
+        cy.wait(1000)
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '228')
+
+        // Logout
+        cy.visit('admin.php/auth/login/out')
+
+        cy.wait(1000)
+
+        cy.visit('index.php')
+
+        // Cleaning the cache
+        cy.exec('./../lizmap-ctl script lizmap~wmts:cleaning -v testsrepository cache Quartiers', {failOnNonZeroExit: false})
+            .its('stdout')
+            .should('contain', 'Start cleaning')
+
+        // Check the cache
+        cy.exec('./../lizmap-ctl docker-exec find /tmp/testsrepository/cache/Quartiers/EPSG_3857/lizmap_/ -type f | wc -l')
+            .its('stdout')
+            .should('contain', '0')
     })
 
     it('lizmap~wmts:capabilities failed', function () {
