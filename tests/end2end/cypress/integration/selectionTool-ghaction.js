94800e8e97dca3158c30d82a3453c3aa8a2e120f
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-06-02 17:39:22 +0200

    Tests - Add common functions for cypress

--- tests/end2end/cypress/integration/selectionTool-ghaction.js
+++ tests/end2end/cypress/integration/selectionTool-ghaction.js
@@ -1,3 +1,5 @@
+import {arrayBufferToBase64} from '../support/function.js'
+
 describe('Selection tool', function () {
     beforeEach(function () {
         cy.visit('/index.php/view/map/?repository=testsrepository&project=selection')
@@ -89,17 +91,7 @@ describe('Selection tool connected as admin', function () {
         cy.wait('@new-selection')
 
         cy.get('@new-selection').should(({ request, response }) => {
-
-            function _arrayBufferToBase64(buffer) {
-                var binary = '';
-                var bytes = new Uint8Array(buffer);
-                var len = bytes.byteLength;
-                for (var i = 0; i < len; i++) {
-                    binary += String.fromCharCode(bytes[i]);
-                }
-                return window.btoa(binary);
-            }
-            const responseBodyAsBase64 = _arrayBufferToBase64(response.body)
+            const responseBodyAsBase64 = arrayBufferToBase64(response.body)
 
             cy.fixture('images/selection_yellow.png').then((image) => {
                 // image encoded as base64
