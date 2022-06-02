1affdcb26dd67af8edff8c92aec5815655be12ea
rldhont <rldhont@gmail.com>
2022-06-02 11:51:12 +0200

    [Bugfix] Dataviz: provide a minimu config with locale for WPS
    The WPS module reuses the lizmap buildPlot method which need the locale from config.
    
    Fixes https://github.com/3liz/lizmap-wps-web-client-module/issues/26

---
+++ tests/end2end/cypress/integration/requests-getprojectconfig-ghaction.js
@@ -0,0 +1,47 @@
+describe('Request Lizmap GetProjectConfig', function () {
+    it('As anonymous', function () {
+        cy.logout();
+
+        cy.request({
+            url: '/index.php/lizmap/service/getProjectConfig?repository=testsrepository&project=selection',
+            failOnStatusCode: false,
+        }).then((resp) => {
+            expect(resp.status).to.eq(200)
+            expect(resp.headers['content-type']).to.eq('application/json')
+
+            expect(resp.body).to.have.property('options')
+            expect(resp.body).to.have.property('layers')
+
+            expect(resp.body).to.have.property('datavizLayers')
+            expect(resp.body.datavizLayers).to.have.property('locale')
+            expect(resp.body.datavizLayers.locale).to.be.not.empty
+            expect(resp.body.datavizLayers).to.have.property('layers')
+            expect(resp.body.datavizLayers.layers).to.be.empty
+            expect(resp.body.datavizLayers).to.have.property('dataviz')
+            expect(resp.body.datavizLayers.dataviz).to.be.empty
+        })
+    })
+
+    it('As anonymous with dataviz', function () {
+        cy.logout();
+
+        cy.request({
+            url: '/index.php/lizmap/service/getProjectConfig?repository=testsrepository&project=dataviz_filtered_in_popup',
+            failOnStatusCode: false,
+        }).then((resp) => {
+            expect(resp.status).to.eq(200)
+            expect(resp.headers['content-type']).to.eq('application/json')
+
+            expect(resp.body).to.have.property('options')
+            expect(resp.body).to.have.property('layers')
+
+            expect(resp.body).to.have.property('datavizLayers')
+            expect(resp.body.datavizLayers).to.have.property('locale')
+            expect(resp.body.datavizLayers.locale).to.be.not.empty
+            expect(resp.body.datavizLayers).to.have.property('layers')
+            expect(resp.body.datavizLayers.layers).to.be.not.empty
+            expect(resp.body.datavizLayers).to.have.property('dataviz')
+            expect(resp.body.datavizLayers.dataviz).to.be.not.empty
+        })
+    })
+})
