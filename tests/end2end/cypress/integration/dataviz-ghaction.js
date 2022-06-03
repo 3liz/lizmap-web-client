874af069c447e0114e9defe7c45e96b79fa9203c
rldhont <rldhont@gmail.com>
2022-06-03 14:08:03 +0200

    Update Tests - Remove the manual test about filtered popup

--- tests/end2end/cypress/integration/dataviz-ghaction.js
+++ tests/end2end/cypress/integration/dataviz-ghaction.js
@@ -7,22 +7,31 @@ describe('Dataviz tests', function () {
 
         cy.visit('/index.php/view/map/?repository=testsrepository&project=dataviz_filtered_in_popup')
 
-        // In a perfect world, this button must be hidden, there isn't any plot to display ...
-        cy.get('#button-dataviz').click()
-
-        cy.get('#right-dock').should('be.visible')
-
-        cy.get('#dataviz-content').invoke('text').invoke('trim').should('equal', '')
+        // Dataviz button does not exist because every dataviz has to be displayed in popup
+        cy.get('#button-dataviz').should('not.exist')
 
         // Popup
         cy.mapClick(550, 400)
         cy.get('#popupcontent').should('be.visible')
 
-        cy.get('.lizmapPopupChildren > h4').should('have.text', 'Number of bakeries by polygon')
+        cy.get('#popupcontent .lizmapPopupChildren.lizdataviz > h4').should('have.text', 'Number of bakeries by polygon')
+
+        cy.get('#popupcontent .lizmapPopupChildren.lizdataviz .dataviz-waiter').should('not.be.visible')
+        cy.get('#popupcontent .lizmapPopupChildren.lizdataviz .plot-container')
+            .should('be.visible').should('have.class', 'plotly')
+            .children().first() // <div class="svg-container" style="position:relative; width: 100%; height: 400px;">
+            .should('have.class', 'svg-container')
+            .should('have.css', 'height', '400px')
 
         cy.get('[data-title="Download plot as a png"]').click()
 
-        cy.fixture('images/plotly/plot_montpellier_bakeries.png').then((expected) => {
+        let expected_diff = 0
+        let fixture_path = 'images/plotly/plot_montpellier_bakeries.png'
+        if (Cypress.isBrowser('firefox')) {
+            fixture_path = 'images/plotly/plot_montpellier_bakeries_firefox.png'
+            expected_diff = 235
+        }
+        cy.fixture(fixture_path).then((expected) => {
             // newplot.png is maybe not the last one if the download folder is not empty ...
             cy.readFile(path.join(downloadsFolder, "newplot.png"), 'base64').then((image) => {
 
@@ -31,12 +40,7 @@ describe('Dataviz tests', function () {
                 const { width, height } = img1;
                 const img2 = PNG.sync.read(Buffer.from(expected, 'base64'));
 
-                let expected_diff = 1
-                // On GH Action, the value is way different ...
-                if (Cypress.env('CI') == 'TRUE') {
-                    expected_diff = 235
-                }
-                expect(pixelmatch(img1.data, img2.data, null, width, height, { threshold: 0.1 }),'expect plot').to.lessThan(expected_diff)
+                expect(pixelmatch(img1.data, img2.data, null, width, height, { threshold: 0.1 }),'expect plot').to.lessThan(expected_diff+1)
             })
         })
     })
