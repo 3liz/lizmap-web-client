189a9d4d9c2ca49d329b45fae42c33ca2a656ab0
rldhont <rldhont@gmail.com>
2022-06-03 17:57:34 +0200

    Tests - Add condition for QGIS 3.16

--- tests/end2end/cypress/integration/legend-ghaction.js
+++ tests/end2end/cypress/integration/legend-ghaction.js
@@ -5,6 +5,7 @@ describe('Legend tests', function () {
         cy.visit('/index.php/view/map/?repository=testsrepository&project=layer_legends')
 
         // We do not want to test straight the GetLegendGraphic, because we want to check how the URL is built
+        // so we better just check the URL and not the output
 
         const checks = ['layer_legend_single_symbol', 'layer_legend_categorized']
 
@@ -16,7 +17,13 @@ describe('Legend tests', function () {
                 expect(interception.response.headers['content-type'], 'expect mime type to be image/png').to.equal('image/png')
 
                 const responseBodyAsBase64 = arrayBufferToBase64(interception.response.body)
-                cy.fixture('images/treeview/' + check +'.png').then((image) => {
+
+                let expected_path = 'images/treeview/' + check +'.png'
+                if (Cypress.env('QGIS_SERVER_INT') <= 31600 && check == 'layer_legend_categorized'){
+                    expected_path = 'images/treeview/' + check +'_316.png'
+                }
+
+                cy.fixture(expected_path).then((image) => {
                     expect(image, 'expect legend to be displayed').to.equal(responseBodyAsBase64)
                 })
             })
