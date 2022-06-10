f2a175fefc7f4d274cb3de62c3056aab4956be36
nboisteault <nboisteault@3liz.com>
2022-06-10 17:03:07 +0200

    Minify Lizmap JS files Minified files can still be debugged with source maps

--- assets/webpack.common.js
+++ assets/webpack.common.js
@@ -2,7 +2,19 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        lizmap: './src/index.js'
+        lizmap: './src/index.js',
+        map: '../lizmap/www/assets/js/map.js',
+        attributeTable: '../lizmap/www/assets/js/attributeTable.js',
+        edition: '../lizmap/www/assets/js/edition.js',
+        filter: '../lizmap/www/assets/js/filter.js',
+        atlas: '../lizmap/www/assets/js/atlas.js',
+        'switcher-layers-actions': '../lizmap/www/assets/js/switcher-layers-actions.js',
+        timemanager: '../lizmap/www/assets/js/timemanager.js',
+        search: '../lizmap/www/assets/js/search.js',
+        view: '../lizmap/www/assets/js/view.js',
+        action: '../lizmap/www/assets/js/action.js',
+        'bottom-dock': '../lizmap/www/assets/js/bottom-dock.js',
+        popupQgisAtlas: '../lizmap/www/assets/js/popupQgisAtlas.js',
     },
     output: {
         filename: '../../lizmap/www/assets/js/[name].js',
