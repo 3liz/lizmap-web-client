1c3443bae778a5599e427860140a8289c1fca281
rldhont <rldhont@gmail.com>
2022-07-26 12:07:49 +0200

    fix: The `editionLayers` of `$configJson` in `Project::getUpdatedConfig` can be `unset`
    If there is no edition layers for the user, the `editionLayers` of the `$configJson` is `unset`.
    So when lizmap removed layers from config to provide only available configuration for user, the code does not check if the `editionLayers` of `$configJson` is still here.
    
    Funded by 3Liz

--- lizmap/modules/lizmap/lib/Project/Project.php
+++ lizmap/modules/lizmap/lib/Project/Project.php
@@ -1685,7 +1685,8 @@ class Project
                 unset($configJson->tooltipLayers->{$key});
             }
             // editionLayers
-            if (property_exists($configJson->editionLayers, $key)) {
+            if ($this->hasEditionLayersForCurrentUser()
+                && property_exists($configJson->editionLayers, $key)) {
                 unset($configJson->editionLayers->{$key});
             }
             // datavizLayers
