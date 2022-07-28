50be91bf739ec98b041e0f55af601f686568a880
Laurent Jouanneau <dev@ljouanneau.com>
2022-07-28 22:45:49 +0200

    Jelix migration: uninstaller.ini.php is missing

---
+++ lizmap/install/uninstall/uninstaller.ini.php
@@ -0,0 +1,4 @@
+;<?php die(''); ?>
+; for security reasons , don't remove or modify the first line
+; don't modify this file if you don't know what you do. it is generated automatically by jInstaller
+
