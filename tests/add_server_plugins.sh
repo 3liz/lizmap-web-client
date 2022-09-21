5d21a5aff1d4a173d04e3fc3be784c4ac5d46375
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-09-21 14:40:18 +0200

    Revert of the previous commit, qgis-plugin-manager is not available in QGIS 3.16

--- tests/add_server_plugins.sh
+++ tests/add_server_plugins.sh
@@ -4,21 +4,16 @@
 echo "QGIS Server Lizmap plugin"
 
 # Latest commit
-echo "Latest commit from https://packages.3liz.org"
-wget https://packages.3liz.org/pub/lizmap-server-qgis-plugin/lizmap_server.master.zip -O /tmp/lizmap_server.master.zip
+wget https://packages.3liz.org/pub/lizmap-server-qgis-plugin/lizmap_server.master.zip -O lizmap_server.master.zip
 
 # Latest release
 # VERSION=1.1.1
-# echo "Stable release from GitHub"
-# wget https://github.com/3liz/qgis-lizmap-server-plugin/releases/latest/download/lizmap_server.${VERSION}.zip -O /tmp/lizmap_server.master.zip
+# wget https://github.com/3liz/qgis-lizmap-server-plugin/releases/latest/download/lizmap_server.${VERSION}.zip -O lizmap_server.master.zip
 
-unzip -o /tmp/lizmap_server.master.zip -d /srv/plugins/
-rm /tmp/lizmap_server.master.zip
+unzip -o lizmap_server.master.zip -d qgis-server-plugins/
+rm lizmap_server.master.zip
 
-echo "QGIS Server WfsOutputExtension and AlasPrint plugins"
-echo "Stable releases from http://plugins.qgis.org"
-qgis-plugin-manager init
-qgis-plugin-manager update
-# qgis-plugin-manager install "Lizmap server"
-qgis-plugin-manager install atlasprint
-qgis-plugin-manager install wfsOutputExtension
+# Remove legacy package for a few weeks
+if [ -d qgis-server-plugins/lizmap/ ]; then
+  rm -rf qgis-server-plugins/lizmap
+fi
