0aa11fde2e99237215179c752231a5f6af37aa21
nboisteault <nboisteault@3liz.com>
2022-09-29 10:02:31 +0200

    100KM follow up

--- assets/src/modules/MGRS.js
+++ assets/src/modules/MGRS.js
@@ -80,114 +80,7 @@ class MGRS extends Graticule {
         this.maxLat_ = 72;
         this.minLat_ = -80;
 
-        let interval = this.getInterval_(resolution);
-        if (interval == -1) {
-            this.meridians_.length = 0;
-            this.parallels_.length = 0;
-            if (this.meridiansLabels_) {
-                this.meridiansLabels_.length = 0;
-            }
-            if (this.parallelsLabels_) {
-                this.parallelsLabels_.length = 0;
-            }
-            return;
-        }
-
-        let wrapX = false;
-        const projectionExtent = this.projection_.getExtent();
-        const worldWidth = getWidth(projectionExtent);
-        if (
-            this.getSource().getWrapX() &&
-            this.projection_.canWrapX() &&
-            !containsExtent(projectionExtent, extent)
-        ) {
-            if (getWidth(extent) >= worldWidth) {
-                extent[0] = projectionExtent[0];
-                extent[2] = projectionExtent[2];
-            } else {
-                wrapX = true;
-            }
-        }
-
-        // Constrain the center to fit into the extent available to the graticule
-
-        const validCenterP = [
-            clamp(center[0], this.minX_, this.maxX_),
-            clamp(center[1], this.minY_, this.maxY_),
-        ];
-
-        // Transform the center to lon lat
-        // Some projections may have a void area at the poles
-        // so replace any NaN latitudes with the min or max value closest to a pole
-
-        const centerLonLat = this.toLonLatTransform_(validCenterP);
-        if (isNaN(centerLonLat[1])) {
-            centerLonLat[1] =
-                Math.abs(this.maxLat_) >= Math.abs(this.minLat_)
-                    ? this.maxLat_
-                    : this.minLat_;
-        }
-        let centerLon = clamp(centerLonLat[0], this.minLon_, this.maxLon_);
-        let centerLat = clamp(centerLonLat[1], this.minLat_, this.maxLat_);
-        const maxLines = this.maxLines_;
-        let cnt, idx, lat, lon;
-
-        // Limit the extent to fit into the extent available to the graticule
-
-        let validExtentP = extent;
-        if (!wrapX) {
-            validExtentP = [
-                clamp(extent[0], this.minX_, this.maxX_),
-                clamp(extent[1], this.minY_, this.maxY_),
-                clamp(extent[2], this.minX_, this.maxX_),
-                clamp(extent[3], this.minY_, this.maxY_),
-            ];
-        }
-
-        // Transform the extent to get the lon lat ranges for the edges of the extent
-
-        const validExtent = applyTransform(
-            validExtentP,
-            this.toLonLatTransform_,
-            undefined,
-            8
-        );
-
-        let maxLat = validExtent[3];
-        let maxLon = validExtent[2];
-        let minLat = validExtent[1];
-        let minLon = validExtent[0];
-
-        if (!wrapX) {
-            // Check if extremities of the world extent lie inside the extent
-            // (for example the pole in a polar projection)
-            // and extend the extent as appropriate
-
-            if (containsCoordinate(validExtentP, this.bottomLeft_)) {
-                minLon = this.minLon_;
-                minLat = this.minLat_;
-            }
-            if (containsCoordinate(validExtentP, this.bottomRight_)) {
-                maxLon = this.maxLon_;
-                minLat = this.minLat_;
-            }
-            if (containsCoordinate(validExtentP, this.topLeft_)) {
-                minLon = this.minLon_;
-                maxLat = this.maxLat_;
-            }
-            if (containsCoordinate(validExtentP, this.topRight_)) {
-                maxLon = this.maxLon_;
-                maxLat = this.maxLat_;
-            }
-
-            // The transformed center may also extend the lon lat ranges used for rendering
-
-            maxLat = clamp(maxLat, centerLat, this.maxLat_);
-            maxLon = clamp(maxLon, centerLon, this.maxLon_);
-            minLat = clamp(minLat, this.minLat_, centerLat);
-            minLon = clamp(minLon, this.minLon_, centerLon);
-        }
-
+        let lat, lon;
 
         // Get code inside grid
         const delta = 0.01;
@@ -201,28 +94,69 @@ class MGRS extends Graticule {
         for (lon = this.minLon_; lon <= this.maxLon_; lon += lonInterval) {
             for (lat = this.minLat_; lat <= this.maxLat_; lat += latInterval) {
 
-                // 100KM grid
-                if (lon === -180 && lat === 0) {
-                    const leftBottom = forward([lon, lat], 1).substring(0, 4);
-                    const rightTop = forward([lon + lonInterval - delta, lat + latInterval - delta], 1).substring(0, 4);
+                // The northmost latitude band, X, is 12° high
+                if (lat == 72) {
+                    latInterval = 12
+                } else {
+                    latInterval = 8;
+                }
 
-                    let rowLetter = leftBottom.charCodeAt(2);
-                    while ((rowLetter += 1) <= rightTop.charCodeAt(2)) {
+                idxParallels = this.addParallel_(
+                    lat,
+                    lon,
+                    lon + lonInterval,
+                    squaredTolerance,
+                    extent,
+                    idxParallels
+                );
+
+                // Special cases
+                // Norway
+                if (lat === 56 && lon === 6) {
+                    continue;
+                }
+
+                // Svalbard
+                if (lat === 72 && lon >= 6 && lon <= 36) {
+                    continue;
+                }
+
+                idxMeridians = this.addMeridian_(
+                    lon,
+                    lat,
+                    lat + latInterval,
+                    squaredTolerance,
+                    extent,
+                    idxMeridians
+                );
+
+                // 100KM grid
+                if (resolution < 2000 && lon == -180 && lat >= 48) {
+                    const leftBottom = forward([lon, lat], 5).substring(0, 4);
+                    const rightTop = forward([lon + lonInterval - delta, lat + latInterval - delta], 5).substring(0, 4);
+
+                    let columnLetter = leftBottom.charCodeAt(2);
+                    while ((columnLetter += 1) <= rightTop.charCodeAt(2)) {
 
                         // Discard I and O
-                        if(rowLetter === 73 || rowLetter === 79 ){
+                        if(columnLetter === 73 || columnLetter === 79 ){
                             continue;
                         }
 
-                        let columnLetter = leftBottom.charCodeAt(3) -1 ;
-                        while ((columnLetter += 1) <= rightTop.charCodeAt(3)) {
+                        let rowLetter = leftBottom.charCodeAt(3) -1 ;
+                        while ((rowLetter += 1) != rightTop.charCodeAt(3)) {
+
+                            // Row letter stops at 'V' => after we go back to 'A'
+                            if(rowLetter === 87){
+                                rowLetter = 65;
+                            }
 
                             // Discard I and O
-                            if(columnLetter === 73 || columnLetter === 79 ){
+                            if(rowLetter === 73 || rowLetter === 79 ){
                                 continue;
                             }
 
-                            let temp = leftBottom.slice(0, 2) + String.fromCharCode(rowLetter) + String.fromCharCode(columnLetter);
+                            let temp = leftBottom.slice(0, 2) + String.fromCharCode(columnLetter) + String.fromCharCode(rowLetter);
 
                             const bbox = inverse(temp);
 
@@ -263,46 +197,7 @@ class MGRS extends Graticule {
                             );
                         }
                     }
-
-
-
                 }
-
-                // The northmost latitude band, X, is 12° high
-                if (lat == 72) {
-                    latInterval = 12
-                } else {
-                    latInterval = 8;
-                }
-
-                idxParallels = this.addParallel_(
-                    lat,
-                    lon,
-                    lon + lonInterval,
-                    squaredTolerance,
-                    extent,
-                    idxParallels
-                );
-
-                // Special cases
-                // Norway
-                if (lat === 56 && lon === 6) {
-                    continue;
-                }
-
-                // Svalbard
-                if (lat === 72 && lon >= 6 && lon <= 36) {
-                    continue;
-                }
-
-                idxMeridians = this.addMeridian_(
-                    lon,
-                    lat,
-                    lat + latInterval,
-                    squaredTolerance,
-                    extent,
-                    idxMeridians
-                );
             }
         }
 
