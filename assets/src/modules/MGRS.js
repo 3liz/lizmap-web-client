9ab310f53009fcd71e4c0fefbb4d0e2134a5871c
nboisteault <nboisteault@3liz.com>
2022-09-29 14:32:54 +0200

    100KM follow up with polygons

--- assets/src/modules/MGRS.js
+++ assets/src/modules/MGRS.js
@@ -1,20 +1,43 @@
 import Graticule from 'ol/layer/Graticule';
 
 import Point from 'ol/geom/Point.js';
+import Polygon from 'ol/geom/Polygon';
+import Feature from 'ol/Feature.js';
 
 import {
     applyTransform,
     containsCoordinate,
     containsExtent,
     getWidth,
+    equals,
+    getCenter,
+    isEmpty,
+    getIntersection,
     intersects,
     wrapX as wrapExtentX,
 } from 'ol/extent.js';
+
+import {
+    equivalent as equivalentProjection, transform, transformExtent,
+  } from 'ol/proj.js';
 
 import { clamp } from 'ol/math.js';
 
 import { forward, inverse } from 'mgrs';
 class MGRS extends Graticule {
+
+    /**
+   * @param {Options} [options] Options.
+   */
+    constructor(options) {
+        super(options);
+
+        /**
+         * @type {Array<Polygon>}
+         * @private
+         */
+        this.cells100km_ = [];
+    }
 
     latLabelFormatter_ = () => {
         return '';
@@ -67,6 +90,113 @@ class MGRS extends Graticule {
         return index;
     }
 
+    addCell100km_(minLat, maxLat, minLon, maxLon, extent, index) {
+        // const cell100km = new Polygon([[[minLat, minLon], [minLat, maxLon], [maxLat, maxLon], [maxLat, minLon], [minLat, minLon]]]);
+        const cell100km = new Polygon([[
+            transform([minLon, minLat], 'EPSG:4326', this.projection_),
+            transform([maxLon, minLat], 'EPSG:4326', this.projection_),
+            transform([maxLon, maxLat], 'EPSG:4326', this.projection_),
+            transform([minLon, maxLat], 'EPSG:4326', this.projection_),
+            transform([minLon, minLat], 'EPSG:4326', this.projection_),
+        ]]);
+
+
+        if (intersects(cell100km.getExtent(), extent)) {
+            this.cells100km_[index++] = cell100km;
+        }
+        return index;
+    }
+
+    /**
+   * Update geometries in the source based on current view
+   * @param {import("../extent").Extent} extent Extent
+   * @param {number} resolution Resolution
+   * @param {import("../proj/Projection.js").default} projection Projection
+   */
+    loaderFunction(extent, resolution, projection) {
+        this.loadedExtent_ = extent;
+        const source = this.getSource();
+
+        // only consider the intersection between our own extent & the requested one
+        const layerExtent = this.getExtent() || [
+            -Infinity,
+            -Infinity,
+            Infinity,
+            Infinity,
+        ];
+        const renderExtent = getIntersection(layerExtent, extent);
+
+        if (
+            this.renderedExtent_ &&
+            equals(this.renderedExtent_, renderExtent) &&
+            this.renderedResolution_ === resolution
+        ) {
+            return;
+        }
+        this.renderedExtent_ = renderExtent;
+        this.renderedResolution_ = resolution;
+
+        // bail out if nothing to render
+        if (isEmpty(renderExtent)) {
+            return;
+        }
+
+        // update projection info
+        const center = getCenter(renderExtent);
+        const squaredTolerance = (resolution * resolution) / 4;
+
+        const updateProjectionInfo =
+            !this.projection_ || !equivalentProjection(this.projection_, projection);
+
+        if (updateProjectionInfo) {
+            this.updateProjectionInfo_(projection);
+        }
+
+        this.createGraticule_(renderExtent, center, resolution, squaredTolerance);
+
+        // first make sure we have enough features in the pool
+        let featureCount = this.meridians_.length + this.parallels_.length + this.cells100km_.length;
+        if (this.meridiansLabels_) {
+            featureCount += this.meridians_.length;
+        }
+        if (this.parallelsLabels_) {
+            featureCount += this.parallels_.length;
+        }
+
+        let feature;
+        while (featureCount > this.featurePool_.length) {
+            feature = new Feature();
+            this.featurePool_.push(feature);
+        }
+
+        const featuresColl = source.getFeaturesCollection();
+        featuresColl.clear();
+        let poolIndex = 0;
+
+        // add features for the lines & labels
+        let i, l;
+        for (i = 0, l = this.meridians_.length; i < l; ++i) {
+            feature = this.featurePool_[poolIndex++];
+            feature.setGeometry(this.meridians_[i]);
+            feature.setStyle(this.lineStyle_);
+            featuresColl.push(feature);
+        }
+        for (i = 0, l = this.parallels_.length; i < l; ++i) {
+            feature = this.featurePool_[poolIndex++];
+            feature.setGeometry(this.parallels_[i]);
+            feature.setStyle(this.lineStyle_);
+            featuresColl.push(feature);
+        }
+
+        // 100km
+        for (i = 0, l = this.cells100km_.length; i < l; ++i) {
+            feature = this.featurePool_[poolIndex++];
+            feature.setGeometry(this.cells100km_[i]);
+            // feature.setStyle(this.lineStyle_);
+            featuresColl.push(feature);
+        }
+    }
+
     /**
     * @param {import("../extent.js").Extent} extent Extent.
     * @param {import("../coordinate.js").Coordinate} center Center.
@@ -90,6 +220,7 @@ class MGRS extends Graticule {
 
         let idxParallels = 0;
         let idxMeridians = 0;
+        let idxCells100km = 0;
 
         for (lon = this.minLon_; lon <= this.maxLon_; lon += lonInterval) {
             for (lat = this.minLat_; lat <= this.maxLat_; lat += latInterval) {
@@ -131,7 +262,7 @@ class MGRS extends Graticule {
                 );
 
                 // 100KM grid
-                if (resolution < 2000 && lon == -180 && lat >= 48) {
+                if (resolution < 2000 && lon == -180 && lat >= 0) {
                     const leftBottom = forward([lon, lat], 5).substring(0, 4);
                     const rightTop = forward([lon + lonInterval - delta, lat + latInterval - delta], 5).substring(0, 4);
 
@@ -139,20 +270,20 @@ class MGRS extends Graticule {
                     while ((columnLetter += 1) <= rightTop.charCodeAt(2)) {
 
                         // Discard I and O
-                        if(columnLetter === 73 || columnLetter === 79 ){
+                        if (columnLetter === 73 || columnLetter === 79) {
                             continue;
                         }
 
-                        let rowLetter = leftBottom.charCodeAt(3) -1 ;
+                        let rowLetter = leftBottom.charCodeAt(3) - 1;
                         while ((rowLetter += 1) != rightTop.charCodeAt(3)) {
 
                             // Row letter stops at 'V' => after we go back to 'A'
-                            if(rowLetter === 87){
+                            if (rowLetter === 87) {
                                 rowLetter = 65;
                             }
 
                             // Discard I and O
-                            if(rowLetter === 73 || rowLetter === 79 ){
+                            if (rowLetter === 73 || rowLetter === 79) {
                                 continue;
                             }
 
@@ -160,41 +291,50 @@ class MGRS extends Graticule {
 
                             const bbox = inverse(temp);
 
-                            idxParallels = this.addParallel_(
+                            idxCells100km = this.addCell100km_(
                                 bbox[1],
-                                bbox[0],
-                                bbox[2],
-                                squaredTolerance,
-                                extent,
-                                idxParallels
-                            );
-
-                            idxParallels = this.addParallel_(
                                 bbox[3],
                                 bbox[0],
                                 bbox[2],
-                                squaredTolerance,
                                 extent,
-                                idxParallels
+                                idxCells100km
                             );
 
-                            idxMeridians = this.addMeridian_(
-                                bbox[0],
-                                bbox[1],
-                                bbox[3],
-                                squaredTolerance,
-                                extent,
-                                idxMeridians
-                            );
+                            // idxParallels = this.addParallel_(
+                            //     bbox[1],
+                            //     bbox[0],
+                            //     bbox[2],
+                            //     squaredTolerance,
+                            //     extent,
+                            //     idxParallels
+                            // );
 
-                            idxMeridians = this.addMeridian_(
-                                bbox[2],
-                                bbox[1],
-                                bbox[3],
-                                squaredTolerance,
-                                extent,
-                                idxMeridians
-                            );
+                            // idxParallels = this.addParallel_(
+                            //     bbox[3],
+                            //     bbox[0],
+                            //     bbox[2],
+                            //     squaredTolerance,
+                            //     extent,
+                            //     idxParallels
+                            // );
+
+                            // idxMeridians = this.addMeridian_(
+                            //     bbox[0],
+                            //     bbox[1],
+                            //     bbox[3],
+                            //     squaredTolerance,
+                            //     extent,
+                            //     idxMeridians
+                            // );
+
+                            // idxMeridians = this.addMeridian_(
+                            //     bbox[2],
+                            //     bbox[1],
+                            //     bbox[3],
+                            //     squaredTolerance,
+                            //     extent,
+                            //     idxMeridians
+                            // );
                         }
                     }
                 }
@@ -233,6 +373,8 @@ class MGRS extends Graticule {
         if (this.meridiansLabels_) {
             this.meridiansLabels_.length = idxMeridians;
         }
+
+        this.cells100km_.length = idxCells100km;
     }
 }
 
