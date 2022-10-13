e1e9bfb77263ded6bc8c1a2b485b1471b1782dc9
rmartin <rmartin@3liz.com>
2022-10-13 11:10:13 +0200

    formwidget check datatype to provide input type : number method overload with a datatype check

--- lizmap/plugins/formwidget/input_htmlbootstrap/input_htmlbootstrap.formwidget.php
+++ lizmap/plugins/formwidget/input_htmlbootstrap/input_htmlbootstrap.formwidget.php
@@ -8,9 +8,32 @@
  *
  * @license Mozilla Public License : http://www.mozilla.org/MPL/
  */
-require_once JELIX_LIB_PATH.'plugins/formwidget/input_html/input_html.formwidget.php';
+require_once JELIX_LIB_PATH . 'plugins/formwidget/input_html/input_html.formwidget.php';
 
-class input_htmlbootstrapFormWidget extends input_htmlFormWidget
-{
+class input_htmlbootstrapFormWidget extends input_htmlFormWidget {
     use \Lizmap\Form\WidgetTrait;
+
+    public function outputControl() {
+        // same code as input_htmlFormWidget 
+        $attr = $this->getControlAttributes();
+
+        if ($this->ctrl->size != 0) {
+            $attr['size'] = $this->ctrl->size;
+        }
+        $maxl = $this->ctrl->datatype->getFacet('maxLength');
+        if ($maxl !== null) {
+            $attr['maxlength'] = $maxl;
+        }
+        $attr['value'] = $this->getValue();
+        // new code : check datatype to provide a better input type
+        if ( $this->ctrl->datatype instanceof jDatatypeInteger) {
+            $attr['type'] = 'number';
+        } else {
+            $attr['type'] = 'text';
+        }
+        echo '<input';
+        $this->_outputAttr($attr);
+        echo "/>\n";
+        $this->outputJs();
+    }
 }
