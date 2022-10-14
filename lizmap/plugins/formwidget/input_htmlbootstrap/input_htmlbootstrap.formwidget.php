6fbbf260f991463e5f7a8bb9bf30b716d7fb73dc
rmartin <rmartin@3liz.com>
2022-10-14 14:48:12 +0200

    php cs fixer

--- lizmap/plugins/formwidget/input_htmlbootstrap/input_htmlbootstrap.formwidget.php
+++ lizmap/plugins/formwidget/input_htmlbootstrap/input_htmlbootstrap.formwidget.php
@@ -8,12 +8,14 @@
  *
  * @license Mozilla Public License : http://www.mozilla.org/MPL/
  */
-require_once JELIX_LIB_PATH . 'plugins/formwidget/input_html/input_html.formwidget.php';
+require_once JELIX_LIB_PATH.'plugins/formwidget/input_html/input_html.formwidget.php';
 
-class input_htmlbootstrapFormWidget extends input_htmlFormWidget {
+class input_htmlbootstrapFormWidget extends input_htmlFormWidget
+{
     use \Lizmap\Form\WidgetTrait;
 
-    public function outputControl() {
+    public function outputControl()
+    {
         // same code as input_htmlFormWidget
         $attr = $this->getControlAttributes();
 
