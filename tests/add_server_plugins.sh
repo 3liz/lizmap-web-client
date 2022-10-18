655304ced7f2e609e7a0eb65614ca45e4f5c8d9b
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-10-18 16:31:40 +0200

    CI - Update Lizmap server plugin to 1.2.0

--- tests/add_server_plugins.sh
+++ tests/add_server_plugins.sh
@@ -3,12 +3,9 @@
 
 echo "QGIS Server Lizmap plugin"
 
-# Latest commit
-wget https://packages.3liz.org/pub/lizmap-server-qgis-plugin/lizmap_server.master.zip -O lizmap_server.master.zip
-
 # Latest release
-# VERSION=1.1.1
-# wget https://github.com/3liz/qgis-lizmap-server-plugin/releases/latest/download/lizmap_server.${VERSION}.zip -O lizmap_server.master.zip
+VERSION=1.2.0
+wget https://github.com/3liz/qgis-lizmap-server-plugin/releases/download/${VERSION}/lizmap_server.${VERSION}.zip -O lizmap_server.master.zip
 
 unzip -o lizmap_server.master.zip -d qgis-server-plugins/
 rm lizmap_server.master.zip
