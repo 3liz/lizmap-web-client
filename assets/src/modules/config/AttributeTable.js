7c18eadca4eeb5cb3d0d3d085378d856054fb1ed
rldhont <rldhont@gmail.com>
2022-10-24 08:40:56 +0200

    [JavaScript] Define AttributeTable module for config to provide AttributeLayersConfig class

---
+++ assets/src/modules/config/AttributeTable.js
@@ -0,0 +1,77 @@
+import { BaseLayerConfig, BaseLayersConfig } from './Base.js';
+
+const requiredProperties = {
+    'primaryKey': {type: 'string'},
+    'hiddenFields': {type: 'string'},
+    'pivot': {type: 'boolean'},
+    'hideAsChild': {type: 'boolean'},
+    'hideLayer': {type: 'boolean'}
+};
+
+const optionalProperties = {
+};
+
+export class AttributeLayerConfig extends BaseLayerConfig {
+    /**
+     * @param {String} layerName - the layer name
+     * @param {Object} cfg - the lizmap config object for tooltip layer
+     */
+    constructor(layerName, cfg) {
+        super(layerName, cfg, requiredProperties, optionalProperties)
+    }
+
+    /**
+     * The layer primary key
+     *
+     * @type {String}
+     **/
+    get primaryKey() {
+        return this._primaryKey;
+    }
+
+    /**
+     * The layer hidden fields
+     *
+     * @type {String}
+     **/
+    get hiddenFields() {
+        return this._hiddenFields;
+    }
+
+    /**
+     * The layer is pivot table
+     *
+     * @type {Boolean}
+     **/
+    get pivot() {
+        return this._pivot;
+    }
+
+    /**
+     * The layer is hide as child
+     *
+     * @type {Boolean}
+     **/
+    get hideAsChild() {
+        return this._hideAsChild;
+    }
+
+    /**
+     * The layer is hide in attribute table list
+     *
+     * @type {Boolean}
+     **/
+    get hideLayer() {
+        return this._hideLayer;
+    }
+}
+
+export class AttributeLayersConfig extends BaseLayersConfig {
+
+    /**
+     * @param {Object} cfg - the lizmap tooltipLayers config object
+     */
+    constructor(cfg) {
+        super(AttributeLayerConfig, cfg)
+    }
+}
