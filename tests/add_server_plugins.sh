6a1f10deed1fc2c514de5bbaea590607688df29f
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-11-04 12:22:45 +0100

    Tests - With QGIS 3.22, use QPM to install plugins

--- tests/add_server_plugins.sh
+++ tests/add_server_plugins.sh
@@ -2,32 +2,19 @@
 
 set -e
 
-echo "QGIS Server Lizmap plugin"
-
-# Latest commit on alpha release
-VERSION=1.1.2
-wget https://packages.3liz.org/pub/lizmap_server-qgis-plugin/unstable/lizmap_server.${VERSION}-alpha.zip -O lizmap_server.master.zip
-
-# Latest release
-# VERSION=1.1.1
-# wget https://github.com/3liz/qgis-lizmap-server-plugin/releases/download/${VERSION}/lizmap_server.${VERSION}.zip -O lizmap_server.master.zip
-
-unzip -o lizmap_server.master.zip -d qgis-server-plugins/
-rm lizmap_server.master.zip
-
-# Remove legacy package for a few weeks
-if [ -d qgis-server-plugins/lizmap/ ]; then
-  rm -rf qgis-server-plugins/lizmap
+# Remove legacy folders about qgis-plugin-manager
+if [ -d /srv/plugins/.cache_qgis_plugin_manager ]; then
+  rm -rf /srv/plugins/.cache_qgis_plugin_manager
+fi
+if [ -f /srv/plugins/sources.list ]; then
+  rm -f /srv/plugins/sources.list
 fi
 
-# AtlasPrint
-VERSION=3.3.1
-wget https://github.com/3liz/qgis-atlasprint/releases/download/${VERSION}/atlasprint.${VERSION}.zip -O atlasprint.zip
-unzip -o atlasprint.zip -d qgis-server-plugins/
-rm atlasprint.zip
-
-# WfsOutputExtension
-VERSION=1.7.1
-wget https://github.com/3liz/qgis-wfsOutputExtension/releases/download/${VERSION}/wfsOutputExtension.${VERSION}.zip -O wfsoutputextension.zip
-unzip -o wfsoutputextension.zip -d qgis-server-plugins/
-rm wfsoutputextension.zip
+echo "QGIS Server Lizmap, WfsOutputExtension and AtlasPrint plugins"
+echo "Unstable from https://packages.3liz.org"
+# qgis-plugin-manager init
+echo "https://packages.3liz.org/pub/server-plugins-repository/unstable/plugins.[VERSION].xml" > /tmp/sources-plugin-manager.list
+qgis-plugin-manager update
+qgis-plugin-manager install "Lizmap server"
+qgis-plugin-manager install atlasprint
+qgis-plugin-manager install wfsOutputExtension
