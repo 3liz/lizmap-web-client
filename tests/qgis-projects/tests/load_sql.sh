293cc9b6818c56bfb098c836a6716aa60d61325b
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-11-10 17:16:27 +0100

    Tests - Minor update in readme.md and make SQL command exit if there is an error

--- tests/qgis-projects/tests/load_sql.sh
+++ tests/qgis-projects/tests/load_sql.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 SCRIPTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+set -e
+
+echo "* Removing existing schema about test data"
+PGPASSWORD=lizmap1234! psql -h localhost -p 8132 -U lizmap -c "DROP SCHEMA IF EXISTS tests_projects CASCADE"
+
 echo "* Run file $SCRIPTDIR/tests_dataset.sql"
 PGPASSWORD=lizmap1234! psql -h localhost -p 8132 -U lizmap -f $SCRIPTDIR/tests_dataset.sql
 
