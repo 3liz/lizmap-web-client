40f002ceba5048693ec0439f8ebe53820761bddb
nboisteault <nboisteault@3liz.com>
2023-01-17 15:58:09 +0100

    Display custom labels

--- assets/src/components/Print.js
+++ assets/src/components/Print.js
@@ -9,7 +9,7 @@ export default class Print extends HTMLElement {
     }
 
     connectedCallback() {
-        this._templates = mainLizmap.config?.printTemplates?.filter(template => template?.atlas?.enabled === '0');
+        this._printTemplates = mainLizmap.config?.printTemplates?.filter(template => template?.atlas?.enabled === '0');
 
         this._maskWidth = 0;
         this._maskHeight = 0;
@@ -24,7 +24,16 @@ export default class Print extends HTMLElement {
         this._maskLayer.getScale = (frameState) => 100000;
         mainLizmap.map.addLayer(this._maskLayer);
 
-        const template = () => html`
+        render(this._template(), this);
+
+    }
+
+    disconnectedCallback() {
+
+    }
+
+    _template() {
+        return html`
             <table id="print-parameters" class="table table-condensed">
                 <tr>
                     <td>${lizDict['print.toolbar.template']}</td>
@@ -34,7 +43,7 @@ export default class Print extends HTMLElement {
                 <tr>
                     <td>
                         <select id="print-template" @change=${(event) => { this.printTemplate = event.target.value }}>
-                            ${this._templates.map((template, index) => html`<option value="${index}">${template.title}</option>`)}
+                            ${this._printTemplates.map((template, index) => html`<option value="${index}">${template.title}</option>`)}
                         </select>
                     </td>
                     <td><select id="print-scale" class="btn-print-scales"></select></td>
@@ -47,10 +56,19 @@ export default class Print extends HTMLElement {
                     </td>
                 </tr>
             </table>
-            <div class="print-labels">
-                <input type="text" class="print-label"><br>
-                <textarea class="print-label"></textarea>
-            </div>
+            ${this._printTemplates?.[this.printTemplate]?.labels?.length
+                ? html`
+                    <div class="print-labels">
+                        <p>${lizDict['print.labels']}</p>
+
+                        ${this._printTemplates[this.printTemplate].labels.map((label) => 
+                        label?.htmlState ?
+                            html`<textarea name="${label.id}" class="print-label" placeholder="${label.text}" .value=${label.text}></textarea><br>`
+                            : html`<input  name="${label.id}" class="print-label" placeholder="${label.text}" value="${label.text}" type="text"><br>`
+                            )}
+                    </div>`
+                : ''
+            }
             <div class="flex">
                 <select id="print-format" title="${lizDict['print.toolbar.format']}" class="btn-print-format">
                     <option value="pdf">PDF</option>
@@ -60,28 +78,30 @@ export default class Print extends HTMLElement {
                 </select>
                 <button id="print-launch" class="btn-print-launch btn btn-primary flex-grow-1"><span class="icon"></span>&nbsp;${lizDict['print.toolbar.title']}</button>
             </div>`;
-
-        render(template(), this);
-
     }
 
-    disconnectedCallback() {
-
+    get printTemplate() {
+        return this._printTemplate;
     }
 
     /**
      * @param {string | number} index
      */
     set printTemplate(index){
+
+        this._printTemplate = index;
+
         // Change mask size
         // Width/height are in mm by default. Convert to pixels
         const INCHES_PER_METER = 39.37;
         const DOTS_PER_INCH = 72;
 
-        this._maskWidth = this._templates?.[index]?.maps?.[0]?.width / 1000 * INCHES_PER_METER * DOTS_PER_INCH;
-        this._maskHeight = this._templates?.[index]?.maps?.[0]?.height / 1000 * INCHES_PER_METER * DOTS_PER_INCH;
+        this._maskWidth = this._printTemplates?.[index]?.maps?.[0]?.width / 1000 * INCHES_PER_METER * DOTS_PER_INCH;
+        this._maskHeight = this._printTemplates?.[index]?.maps?.[0]?.height / 1000 * INCHES_PER_METER * DOTS_PER_INCH;
 
         mainLizmap.map.getView().changed();
+
+        render(this._template(), this);
     }
 
 }
