766961ea1abd7d45caae2eb22addcf0816eb6cf0
nworr <43475951+nworr@users.noreply.github.com>
2023-02-09 15:42:11 +0100

    create repository cli command refacto to use  jelix and symfony/console (#3446)
    new CliHelper/RepositoryCreator
    legacy script and  symfony console command use RepositoryCreator
    ouput txt in deprecated cmdline controller to inform about console.php alternative 
    code quality

--- lizmap/modules/lizmap/controllers/repository.cmdline.php
+++ lizmap/modules/lizmap/controllers/repository.cmdline.php
@@ -50,95 +50,29 @@ class repositoryCtrl extends jControllerCmdLine
 
     public function create()
     {
+        /**
+         * @var jResponseCmdline $rep
+         */
         $rep = $this->getResponse(); // cmdline response by default
+        $rep->addContent("Using this command is deprecated, all commands are now unified in console.php
+In lizmap folder, use 'php console.php repository:create <key> <label> <path>'\n\n");
 
-        $key = $this->param('key');
-
-        $lrep = lizmap::getRepository($key);
-        if ($lrep) {
-            // Error message
-            $rep->addContent("The repository already exists!\n");
-            $rep->addContent("The repository can't be created!\n");
+        try {
+            $cli = new \Lizmap\CliHelpers\RepositoryCreator();
+            $cli->create(
+                $this->param('key'),
+                $this->param('label'),
+                $this->param('path'),
+                $this->param('allowUserDefinedThemes')
+            );
+        } catch (\Exception $e) {
+            $rep->addContent("The repository can't be created ! : \n");
+            $rep->addContent($e->getMessage()."\n");
             $rep->setExitCode(1);
 
             return $rep;
         }
-
-        // Repository data
-        $data = array();
-        foreach (lizmapRepository::getProperties() as $prop) {
-            $data[$prop] = $this->param($prop);
-            // Check paths
-            if ($prop == 'path') {
-                $path = $data[$prop];
-                // Testing relative path and updating it if needed
-                if ($path[0] != '/' and $path[1] != ':') {
-                    // Get services data
-                    $services = lizmap::getServices();
-                    $rootRepositories = $services->getRootRepositories();
-                    if ($rootRepositories != '') {
-                        if (!preg_match('#/$#', $rootRepositories)) {
-                            $rootRepositories .= '/';
-                        }
-                        $npath = realpath($rootRepositories.$path);
-                        if (substr($npath, 0, strlen($rootRepositories)) !== $rootRepositories) {
-                            // Error message
-                            $rep->addContent("The path provided is not authorized!\n");
-                            $rep->addContent("The repository can't be created!\n");
-                            $rep->setExitCode(1);
-
-                            return $rep;
-                        }
-                        $path = $npath;
-                    }
-                }
-                if (!file_exists($path) or !is_dir($path)) {
-                    // Error message
-                    $rep->addContent("The path provided doesn't exist or is not a directory!\n");
-                    $rep->addContent("The repository can't be created!\n");
-                    $rep->setExitCode(1);
-
-                    return $rep;
-                }
-                // Add a trailing / if needed
-                if (!preg_match('#/$#', $path)) {
-                    $path .= '/';
-                }
-                $data[$prop] = $path;
-            }
-            // Check allowUserDefinedThemes
-            if ($prop == 'allowUserDefinedThemes') {
-                $value = $data[$prop];
-                if (empty($value)) {
-                    $data[$prop] = false;
-                } else {
-                    $strVal = strtolower($value);
-                    if ($strVal === 'true' || $strVal === 't' || intval($value) === 1
-                        && $strVal === 'on' || $strVal === '1' || $value === true) {
-                        $data[$prop] = true;
-                    } else {
-                        $data[$prop] = false;
-                    }
-                }
-            }
-        }
-
-        if (empty($data)) {
-            // Error message
-            $rep->addContent("The data is empty!\n");
-            $rep->addContent("The repository can't be created!\n");
-            $rep->setExitCode(1);
-
-            return $rep;
-        }
-
-        $lrep = lizmap::createRepository($key, $data);
-        if (!$lrep) {
-            $rep->addContent("The repository can't be created!\n");
-            $rep->setExitCode(1);
-        } else {
-            $rep->addContent("The repository has been created!\n");
-        }
+        $rep->addContent("The repository has been created!\n");
 
         return $rep;
     }
