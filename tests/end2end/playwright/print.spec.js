3480eb7cfa87bdaa0cd0ff837d3f3459db81ae3d
nboisteault <nboisteault@3liz.com>
2023-02-14 18:24:35 +0100

    e2e: test print requests

--- tests/end2end/playwright/print.spec.js
+++ tests/end2end/playwright/print.spec.js
@@ -36,4 +36,21 @@ test.describe('Print', () => {
         expect(await page.locator('.btn-print-dpis').inputValue()).toBe('200');
 
     });
+
+    test('Print requests', async ({ page }) => {
+        // Test `print_labels` template
+        page.once('request', request => {
+            expect(request.postData()).toBe('SERVICE=WMS&REQUEST=GetPrint&VERSION=1.3.0&FORMAT=pdf&TRANSPARENT=true&SRS=EPSG%3A2154&DPI=100&TEMPLATE=print_labels&map0%3AEXTENT=759249.549002605%2C6271892.11637865%2C781949.549002605%2C6286892.11637865&map0%3ASCALE=100000&map0%3ALAYERS=quartiers%2Csousquartiers&map0%3ASTYLES=d%C3%A9faut%2Cd%C3%A9faut&map0%3AOPACITIES=255%2C255&simple_label=simple%20label&multiline_label=Multiline%20label');
+        });
+        await page.locator('#print-launch').click();
+
+        // Test `print_map` template
+        await page.locator('#print-template').selectOption('1');
+
+        page.once('request', request => {
+            expect(request.postData()).toBe('SERVICE=WMS&REQUEST=GetPrint&VERSION=1.3.0&FORMAT=jpeg&TRANSPARENT=true&SRS=EPSG%3A2154&DPI=200&TEMPLATE=print_map&map0%3AEXTENT=765699.549002605%2C6271792.11637865%2C775499.549002605%2C6286992.11637865&map0%3ASCALE=100000&map0%3ALAYERS=quartiers%2Csousquartiers&map0%3ASTYLES=d%C3%A9faut%2Cd%C3%A9faut&map0%3AOPACITIES=255%2C255');
+        });
+
+        await page.locator('#print-launch').click();
+    });
 });
