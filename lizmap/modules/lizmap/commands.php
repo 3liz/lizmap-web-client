b75c2e7c15dac6fa8c54bf812199821f083108e3
nworr <43475951+nworr@users.noreply.github.com>
2023-02-16 11:36:58 +0100

    All project WMS test commands in console.php (#3480)
    * cli Helpers RepositoryWMSChecker
    perform check as the original project.cmdline
    * both cli command (jelix, symfony) use CliHelper
    * add message about new console.php usage

--- lizmap/modules/lizmap/commands.php
+++ lizmap/modules/lizmap/commands.php
@@ -4,4 +4,5 @@ if (isset($application)) {
     $application->add(new \Lizmap\Commands\CreateRepository());
     $application->add(new \Lizmap\Commands\DbMigrateLog());
     $application->add(new \Lizmap\Commands\DbMigrateUsers());
+    $application->add(new \Lizmap\Commands\ProjectLoad());
 }
