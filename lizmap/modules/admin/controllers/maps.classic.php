6e4850565705a502ba68cc9ed0345cac2afd4a36
rldhont <rldhont@gmail.com>
2023-02-25 10:16:16 +0100

    [Bugfix] Admin Maps management menu selected

--- lizmap/modules/admin/controllers/maps.classic.php
+++ lizmap/modules/admin/controllers/maps.classic.php
@@ -30,6 +30,9 @@ class mapsCtrl extends jController
     // used to get only non admin subjects
     protected $lizmapClientPrefix = 'lizmap.repositories|lizmap.tools';
 
+    // The selected admin menu item
+    protected $selectedMenuItem = 'lizmap_maps';
+
     /**
      * Display the list of repositories and maps.
      *
@@ -90,7 +93,7 @@ class mapsCtrl extends jController
         $tpl->assign('subjects', $subjects);
         $tpl->assign('labels', $labels);
         $rep->body->assign('MAIN', $tpl->fetch('maps'));
-        $rep->body->assign('selectedMenuItem', 'lizmap_maps');
+        $rep->body->assign('selectedMenuItem', $this->selectedMenuItem);
 
         return $rep;
     }
@@ -357,7 +360,7 @@ class mapsCtrl extends jController
             $tpl = new jTpl();
             $tpl->assign('form', $form);
             $rep->body->assign('MAIN', $tpl->fetch('config_section'));
-            $rep->body->assign('selectedMenuItem', 'lizmap_configuration');
+            $rep->body->assign('selectedMenuItem', $this->selectedMenuItem);
 
             return $rep;
         }
