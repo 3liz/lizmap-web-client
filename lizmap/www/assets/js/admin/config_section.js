2d6003c72d3a6df28121ae3e69d0b14d1f9c5a92
Etienne Trimaille <gustrimaille@yahoo.fr>
2023-03-16 17:00:51 +0100

    UX - Transfrom _ and - to a space when creating a repository

--- lizmap/www/assets/js/admin/config_section.js
+++ lizmap/www/assets/js/admin/config_section.js
@@ -5,7 +5,7 @@ window.onload = () => {
     }
     document.getElementById('jforms_admin_config_section_path').addEventListener('change', (evt) => {
         const repVal = evt.target.value;
-        const repName = repVal.trim().replace(/^\/+/, '').replace(/\/+$/, '').split('/').pop();
+        const repName = repVal.trim().replace('_', ' ').replace('-', ' ').replace(/^\/+/, '').replace(/\/+$/, '').split('/').pop();
         document.getElementById('jforms_admin_config_section_label').value = repName[0].toUpperCase() + repName.slice(1).toLowerCase();
         document.getElementById('jforms_admin_config_section_repository').value = repName.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replaceAll(/[^a-z0-9]/g, '');
     });
