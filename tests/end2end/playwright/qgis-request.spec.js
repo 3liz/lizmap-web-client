1fc5fbad271858c80c74cb2220c5cf4ba7307a02
rmartin <rmartin@3liz.com>
2023-04-17 12:39:51 +0200

    getLegend improve & remove cypress

--- tests/end2end/playwright/qgis-request.spec.js
+++ tests/end2end/playwright/qgis-request.spec.js
@@ -3,22 +3,8 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('QGIS Requests', () => {
 
-  test('QGIS legend Content-type', async ({ page }) => {
-    const url = '/index.php/view/map?repository=testsrepository&project=layer_legends';
-    await page.goto(url, { waitUntil: 'networkidle' });
 
-
-    const layers_to_check = ['layer_legend_single_symbol', 'layer_legend_categorized']
-    for (const layer_name of layers_to_check) {
-      // Start waiting for respone before clicking. Note no await.
-      const legendPromise = page.waitForResponse(/GetLegend/);
-      await page.locator('#layer-' + layer_name + ' a[class="expander"]').click();
-      let legendResponse = await legendPromise;
-      expect(await legendResponse.headerValue('Content-Type')).toBe('image/png');
-    }
-  });
-
-  test('QGIS legend Request param', async ({ page }) => {
+  test('QGIS legend Request Content-type and param', async ({ page }) => {
     const url = '/index.php/view/map?repository=testsrepository&project=layer_legends';
     await page.goto(url, { waitUntil: 'networkidle' });
 
@@ -28,6 +14,10 @@ test.describe('QGIS Requests', () => {
       const legendRequestPromise = page.waitForRequest(/GetLegend/);
       await page.locator('#layer-' + layer_name + ' a[class="expander"]').click();
       let legendRequest = await legendRequestPromise;
+      // check response is type image/png
+      let legendResponse = await legendRequest.response();
+      expect(await legendResponse?.headerValue('Content-Type')).toBe('image/png');
+      // get Original WMS request
       let echoLegend = await page.request.get(legendRequest.url() + '&__echo__');
       let legendParams = [/SERVICE=WMS/i,
         /VERSION=1\.3\.0/i,
