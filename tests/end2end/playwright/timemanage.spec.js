132380b83c22dfb760f217158b987bad7c7bac09
rmartin <rmartin@3liz.com>
2023-04-18 18:38:05 +0200

    doc & improve timemanager e2e test

--- tests/end2end/playwright/timemanage.spec.js
+++ tests/end2end/playwright/timemanage.spec.js
@@ -6,44 +6,64 @@ test.describe('Time Manager', () => {
     const url = '/index.php/view/map?repository=testsrepository&project=time_manager';
     await page.goto(url, { waitUntil: 'networkidle' });
 
+    // when Time manager is running, 2 requests are send for each time range
+    // - getFilterToken with method POST, returning a json with a token
+    // - getMap that use the token 
+    // there is 3 time range in the test data, we'll check each
     const timeRequest = [
       { 'start': '2007-01-01', 'end': '2011-12-31' },
       { 'start': '2012-01-01', 'end': '2016-12-31' },
       { 'start': '2017-01-01', 'end': '2021-12-31' }
     ];
+    const responseMatchGetFilterTokenFunc = function (response) {
+      return (response.request().method() == 'POST' &&  response.request().postData().match(/GetFilterToken/i));
+    };
+
     let firstRun = true;
     for (let timeObj of timeRequest) {
-      let getFilterTokenPromise = page.waitForResponse(rep => rep.request().method() == 'POST');
+      // will catch getFilterToken response
+      let getFilterTokenPromise = page.waitForResponse(responseMatchGetFilterTokenFunc);
+
+      // will catch GetMapRequest
       let getMapRequestPromise = page.waitForRequest(/GetMap/);
+
       if (firstRun) {
+        // promises are setup, launch the timemanager
         await page.locator('#button-timemanager').click();
         await page.locator('#tmTogglePlay').click();
         firstRun = false;
       }
+      // wait for the getFilterToken response
       let getFiltertokenResponse = await getFilterTokenPromise;
+      // check the json response contains token prop
       expect((await getFiltertokenResponse.json())).toHaveProperty('token');
-      let echoGetMap = await page.request.get((await getMapRequestPromise).url() + '&__echo__');
-      let getMapParams = [/SERVICE=WMS/i,
-        /VERSION=1\.3\.0/i,
-        /REQUEST=GetMap/i,
-        new RegExp('filter=time_manager:\\s\\(\\s\\(\\s"test_date"\\s>=\\s\'' + timeObj.start + "'\\s\\)\\sAND\\s\\(\\s\"test_date\"\\s<=\\s'" + timeObj.end + "'\\s\\)\\s\\)", 'i'),
-        /EXCEPTIONS=application\/vnd\.ogc\.se_inimage/i,
-        /FORMAT=image\/png/i,
-        /TRANSPARENT=TRUE/i,
-      ];
+      let getMapRequest = await getMapRequestPromise;
+      // check request is build with token
+      expect(getMapRequest.url()).toMatch(/FILTERTOKEN/);
+      // re-send the request with additionnal echo param to retreive the WMS Request
+      let echoGetMap = await page.request.get(getMapRequest.url() + '&__echo__');
       const originalUrl = decodeURIComponent(await echoGetMap.text());
-      for (let y in getMapParams) {
-        expect(originalUrl).toMatch(getMapParams[y]);
-      }
+      const urlObj = new URLSearchParams(originalUrl);
+      expect(urlObj.get("version")).toBe("1.3.0");
+      expect(urlObj.get("service")).toBe("WMS");
+      expect(urlObj.get("format")).toBe("image/png");
+      expect(urlObj.get("request")).toBe("getmap");
+      expect(urlObj.get("filter")).toBe('time_manager: ( ( "test_date" >= \''+ timeObj.start +'\' ) AND ( "test_date" <= \''+ timeObj.end +'\' ) ) ');
+       
     }
-    
-    let getMapNoFiltertPromise = page.waitForRequest(/GetMap/);
+
+    // back to normal behaviour => no token
     // closing time manager
     await page.locator('.btn-timemanager-clear').click();
+    
+    // will catch GetMapRequest
+    let getMapNoFiltertPromise = page.waitForRequest(/GetMap/);
     // move to force request
     await page.locator('button.zoom-in').click();
     let getMapNoFilter = await getMapNoFiltertPromise;
-    expect(getMapNoFilter.url()).not.toContain('filter=');
+    // assert no more filter token
+    expect(getMapNoFilter.url()).not.toMatch(/FILTERTOKEN/i);
+   
   });
 
 
