fc6149bac9f7a210014808c5f2d7d733c1d75c5b
TeoGoddet <9885447+TeoGoddet@users.noreply.github.com>
2023-04-20 09:44:18 +0200

    Update qgisProject.class.php

--- lizmap/modules/lizmap/classes/qgisProject.class.php
+++ lizmap/modules/lizmap/classes/qgisProject.class.php
@@ -654,6 +654,7 @@ class qgisProject
                     $themes[(string) $themeObj->name]['layers'][(string) $layerObj->id] = array(
                         'style' => (string) $layerObj->style,
                         'expanded' => (string) $layerObj->expanded,
+                        'visible' => (string) $layerObj->visible ?? '1',
                     );
                 }
 
