304643df31e6c2f60324aacdc88aededd0a02aab
nworr <43475951+nworr@users.noreply.github.com>
2023-04-25 15:34:56 +0200

    log OGC request and retreive it for tests (#3576)
    * Add 'echo_ogc' env var in test docker-compose
    
    * add echoproxy log file conf in tests
    
    * Proxy log requests and can echo it (using md5 from url and body)
    
    * [end2end] QgisRequest test
    
    * [end2end] timemanager Request
    
    * remove cypress
    
    * remove timemanager md
    
    * remove useless cypress reference image
    
    Co-authored-by: mdouchin <mdouchin@3liz.com>
    
    ---------
    
    Co-authored-by: mdouchin <mdouchin@3liz.com>

--- tests/end2end/cypress/integration/legend-ghaction.js
+++ tests/end2end/cypress/integration/legend-ghaction.js
@@ -16,41 +16,4 @@ describe('Legend tests', function () {
         cy.get("tr#layer-disabled td a.expander").should('not.exist')
     })
 
-    it('Test GetLegendGraphics', function () {
-        cy.visit('/index.php/view/map/?repository=testsrepository&project=layer_legends')
-        cy.wait(1000) // Let's wait for all GetLegendGraphics first
-        // We do not want to test straight the GetLegendGraphic, because we want to check how the URL is built
-        // so we better just check the URL and not the output
-
-        const checks = ['layer_legend_single_symbol', 'layer_legend_categorized']
-
-        for (const check of checks) {
-            cy.intercept('*GetLegendGraphic*').as('legend')
-            cy.get('tr#layer-' + check +' td a.expander').click()
-
-            cy.wait('@legend').then((interception) => {
-                expect(interception.response.headers['content-type'], 'expect mime type to be image/png').to.equal('image/png')
-
-                serverMetadata().then(metadataResponse => {
-                    const responseBodyAsBase64 = arrayBufferToBase64(interception.response.body)
-
-                    // Default image for QGIS 3.22
-                    let expected_path = 'images/treeview/' + check +'.png'
-
-                    if (metadataResponse.body.qgis_server_info.metadata.version_int < 31700 && check == 'layer_legend_categorized') {
-                        // previous image of the legend which was working for QGIS 3.16
-                        expected_path = 'images/treeview/layer_legend_categorized_316.png';
-                    }
-
-                    // With QGIS 3.28 or 3.22.15, we do not test anymore : https://github.com/qgis/QGIS/pull/50256
-                    // Which has been backported in 3.22.15
-                    if (metadataResponse.body.qgis_server_info.metadata.version_int < 32215) {
-                        cy.fixture(expected_path).then((image) => {
-                            expect(image, 'expect legend to be compared with ' + expected_path).to.equal(responseBodyAsBase64)
-                        })
-                    }
-                })
-            })
-        }
-    })
 })
