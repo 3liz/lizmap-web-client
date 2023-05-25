79524b720ad2ce03ce668241bf0810d4263c51f1
nboisteault <nboisteault@3liz.com>
2023-05-25 15:06:21 +0200

    Treeview: add checkbox to toggle layer/group visibility

--- assets/src/components/Treeview.js
+++ assets/src/components/Treeview.js
@@ -10,14 +10,26 @@ export default class Treeview extends HTMLElement {
 
     connectedCallback() {
 
+        this._onChange = () => {
+            render(this._layerTemplate(mainLizmap.baseLayersMap.overlayLayersGroup), this);
+        };
+
         this._layerTemplate = layerGroup =>
         html`
         <ul>
-            ${layerGroup.getLayers().getArray().reverse().map(layer => html`<li>${layer.get('name')}${when(layer instanceof LayerGroup, () => this._layerTemplate(layer))}</li>`)}
+            ${layerGroup.getLayers().getArray().slice().reverse().map(layer => html`
+            <li>
+                <label class="checkbox">
+                    <input type="checkbox" .checked=${layer.getVisible()} @click=${() => layer.setVisible(!layer.getVisible())} >${layer.get('name')}
+                </label>
+                ${when(layer instanceof LayerGroup, () => this._layerTemplate(layer))}
+            </li>`
+            )}
         </ul>`;
 
         render(this._layerTemplate(mainLizmap.baseLayersMap.overlayLayersGroup), this);
 
+        mainLizmap.baseLayersMap.overlayLayersGroup.on('change', this._onChange);
     }
 
     disconnectedCallback() {
