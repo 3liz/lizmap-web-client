d1446cee21ded1177689524a42dcb59a1eab1ea3
nboisteault <nboisteault@3liz.com>
2023-06-09 11:50:42 +0200

    Handle layer link to doc

--- assets/src/components/Treeview.js
+++ assets/src/components/Treeview.js
@@ -22,6 +22,7 @@ export default class Treeview extends HTMLElement {
                 <input class="${layerGroup.get('mutuallyExclusive') ? 'rounded-checkbox' : ''}" type="checkbox" id="node-${layer.get('name')}" .checked=${layer.getVisible()} @click=${() => layer.setVisible(!layer.getVisible())} >
                 <div class="node ${this._isFiltered(layer) ? 'filtered' : ''}">
                     <label for="node-${layer.get('name')}">${layer.get('name')}</label>
+                    <a href="${this._createLink(layer.get('name'))}" target="_blank" class="link" title="${lizDict['tree.button.link']}"><i class="icon-share"></i></a>
                     <i class="icon-info-sign" @click=${() => this._toggleMetadata(layer.get('name'), layer instanceof LayerGroup)}></i>
                 </div>
                 ${when((layer instanceof LayerGroup) && !layer.get('groupAsLayer'), () => this._layerTemplate(layer))}
@@ -39,6 +40,18 @@ export default class Treeview extends HTMLElement {
 
     _isFiltered(layer) {
         return !(layer instanceof LayerGroup) && layer.getSource().getParams()?.['FILTERTOKEN'];
+    }
+
+    _createLink(layerName) {
+        let url = lizMap.config.layers?.[layerName]?.link;
+
+        // Test if the url is internal
+        const mediaRegex = /^(\/)?media\//;
+        if (mediaRegex.test(url)) {
+            const mediaLink = lizUrls.media + '?' + new URLSearchParams(lizUrls.params);
+            url = mediaLink + '&path=/' + url;
+        }
+        return url;
     }
 
     _toggleMetadata (layerName, isGroup){
