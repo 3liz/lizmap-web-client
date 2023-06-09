0bfc1aaf8cbf1b44abd526ce6320617e11e184cf
nboisteault <nboisteault@3liz.com>
2023-06-09 16:55:53 +0200

    Display a spinner when a layer is loading

--- assets/src/components/Treeview.js
+++ assets/src/components/Treeview.js
@@ -1,7 +1,9 @@
-import {mainLizmap, mainEventDispatcher} from '../modules/Globals.js';
+import { mainLizmap } from '../modules/Globals.js';
 import LayerGroup from 'ol/layer/Group.js';
-import {html, render} from 'lit-html';
-import {when} from 'lit-html/directives/when.js';
+import ImageWMS from 'ol/source/ImageWMS.js';
+import WMTS from 'ol/source/WMTS.js';
+import { html, render } from 'lit-html';
+import { when } from 'lit-html/directives/when.js';
 
 export default class Treeview extends HTMLElement {
     constructor() {
@@ -19,6 +21,7 @@ export default class Treeview extends HTMLElement {
         <ul>
             ${layerGroup.getLayers().getArray().slice().reverse().map(layer => html`
             <li>
+                <div class="loading ${layer.getSource().get('loading') ? 'spinner' : ''}"></div>
                 <input class="${layerGroup.get('mutuallyExclusive') ? 'rounded-checkbox' : ''}" type="checkbox" id="node-${layer.get('name')}" .checked=${layer.getVisible()} @click=${() => layer.setVisible(!layer.getVisible())} >
                 <div class="node ${this._isFiltered(layer) ? 'filtered' : ''}">
                     <label for="node-${layer.get('name')}">${layer.get('name')}</label>
@@ -40,6 +43,31 @@ export default class Treeview extends HTMLElement {
         render(this._layerTemplate(mainLizmap.baseLayersMap.overlayLayersGroup), this);
 
         mainLizmap.baseLayersMap.overlayLayersGroup.on('change', this._onChange);
+
+        // Display a spinner when a layer is loading
+        for (const layer of mainLizmap.baseLayersMap.overlayLayersAndGroups) {
+            const source = layer.getSource();
+
+            if (source instanceof ImageWMS) {
+                source.on('imageloadstart', event => {
+                    event.target.set('loading', true, true);
+                    this._onChange();
+                });
+                source.on(['imageloadend', 'imageloaderror'], event => {
+                    event.target.set('loading', false, true);
+                    this._onChange()
+                });
+            } else if (source instanceof WMTS) {
+                source.on('tileloadstart', event => {
+                    event.target.set('loading', true, true);
+                    this._onChange();
+                });
+                source.on(['tileloadend', 'imageloaderror'], event => {
+                    event.target.set('loading', false, true);
+                    this._onChange()
+                });
+            }
+        }
     }
 
     disconnectedCallback() {
