8f0ea2c453a349f5087348397126999d9200710c
rmartin <rmartin@3liz.com>
2023-06-15 12:23:42 +0200

    simplify tey test

--- tests/end2end/playwright/edition-form.spec.js
+++ tests/end2end/playwright/edition-form.spec.js
@@ -9,16 +9,6 @@ test.describe('Edition Form Validation', () => {
     const url_form_edition_all = '/index.php/view/map?repository=testsrepository&project=form_edition_all_field_type';
     await page.goto(url_form_edition_all, { waitUntil: 'networkidle' });
 
-    const displayDataPromise =  page.waitForRequest(/editableFeatures/);
-    // display attributes panel
-    await page.locator('a#button-attributeLayers').click();
-    await page.locator('#attribute-layer-list-table tr:nth-child(1) button').click();
-    // wait until data fetched
-    await displayDataPromise;
-
-    // count data
-    const datatable = page.locator('#attribute-layer-table-form_edition_all_fields_types tr');
-    const initialLineCount = (await datatable.count());
 
     // display form
     await page.locator('#button-edition').click();
@@ -35,27 +25,8 @@ test.describe('Edition Form Validation', () => {
 
     // submit form
     await page.locator('#jforms_view_edition__submit_submit').click();
-    // will close & add line to data
+    // will close & show message
     await expect(page.locator('#edition-form-container')).toBeHidden();
-    await expect(datatable).toHaveCount(initialLineCount + 1);
-    await expect(page.locator('#attribute-layer-table-form_edition_all_fields_types tr:last-child td:nth-child(3)')).toHaveText('50' );
-
-    // out of range
-    await page.locator('a#edition-draw').click();
-    await page.locator('#jforms_view_edition input[name="integer_field"]').fill('400000');
-    await page.locator('#jforms_view_edition__submit_submit').click();
-    // form doesn't hide
-    await expect(page.locator('#edition-form-container')).toBeVisible();
-    // data count unchanged
-    await expect(datatable).toHaveCount(initialLineCount + 1);
-
-    // input between steps
-    await page.locator('#jforms_view_edition input[name="integer_field"]').fill('42');
-    await page.locator('#jforms_view_edition__submit_submit').click();
-     // form doesn't hide
-    await expect(page.locator('#edition-form-container')).toBeVisible();
-    // data count unchanged
-    await expect(datatable).toHaveCount(initialLineCount + 1);
-
+    await expect(page.locator('#lizmap-edition-message')).toBeVisible();
   })
 })
