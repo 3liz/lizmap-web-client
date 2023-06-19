068105b926fd93facb9cd3891b70077328351be8
rldhont <rldhont@gmail.com>
2023-06-19 17:03:05 +0200

    [JavaScript] Module Map State to store map properties

---
+++ tests/js-units/node/state/map.test.js
@@ -0,0 +1,198 @@
+import { expect } from 'chai';
+
+import { ValidationError, ConversionError } from '../../../../assets/src/modules/Errors.js';
+import { Extent } from '../../../../assets/src/modules/utils/Extent.js';
+import { MapState } from '../../../../assets/src/modules/state/Map.js';
+
+describe('MapState', function () {
+    it('Valid', function () {
+        let mapState = new MapState();
+        expect(mapState).to.be.instanceOf(MapState)
+
+        // Initial state
+        expect(mapState.projection).to.be.eq('EPSG:3857')
+        expect(mapState.center).to.be.an('array').that.have.lengthOf(2).that.deep.equal([0, 0])
+        expect(mapState.resolution).to.be.eq(-1)
+        expect(mapState.size).to.be.an('array').that.have.lengthOf(2).that.deep.equal([0, 0])
+        expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        expect(mapState.scaleDenominator).to.be.eq(-1)
+
+        // Update all properties
+        mapState.update({
+            "type": "map.state.changed",
+            "projection": "EPSG:3857",
+            "center": [
+              432082.33132450003,
+              5404877.667855
+            ],
+            "resolution": 27.673393466176645,
+            "size": [
+              1822,
+              634
+            ],
+            "extent": [
+              397265.26494544884,
+              5392762.398873487,
+              466899.3977035512,
+              5416992.936836514
+            ],
+            "scaleDenominator": 104592.14407328397
+        });
+        expect(mapState.projection).to.be.eq('EPSG:3857')
+        expect(mapState.center).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            432082.33132450003,
+            5404877.667855
+        ])
+        expect(mapState.resolution).to.be.eq(27.673393466176645)
+        expect(mapState.size).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            1822,
+            634
+        ])
+        expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([
+            397265.26494544884,
+            5392762.398873487,
+            466899.3977035512,
+            5416992.936836514
+        ])
+        expect(mapState.scaleDenominator).to.be.eq(104592.14407328397)
+
+        // Update some properties
+        mapState.update({
+            "type": "map.state.changed",
+            "resolution": 13.836702727785784,
+            "extent": [
+                414678.25685631623,
+                5398816.026905431,
+                449495.3232353674,
+                5410931.295886946
+            ],
+            "scaleDenominator": 52296.09469372093
+        });
+        expect(mapState.projection).to.be.eq('EPSG:3857')
+        expect(mapState.center).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            432082.33132450003,
+            5404877.667855
+        ])
+        expect(mapState.resolution).to.be.eq(13.836702727785784)
+        expect(mapState.size).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            1822,
+            634
+        ])
+        expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([
+            414678.25685631623,
+            5398816.026905431,
+            449495.3232353674,
+            5410931.295886946
+        ])
+        expect(mapState.scaleDenominator).to.be.eq(52296.09469372093)
+
+        // Update unknown properties - nothing will change
+        mapState.update({
+            "type": "layer.visibility.changed",
+            "layer": 'Quartiers',
+            "checked": false
+        })
+        expect(mapState.projection).to.be.eq('EPSG:3857')
+        expect(mapState.center).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            432082.33132450003,
+            5404877.667855
+        ])
+        expect(mapState.resolution).to.be.eq(13.836702727785784)
+        expect(mapState.size).to.be.an('array').that.have.lengthOf(2).that.deep.equal([
+            1822,
+            634
+        ])
+        expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([
+            414678.25685631623,
+            5398816.026905431,
+            449495.3232353674,
+            5410931.295886946
+        ])
+        expect(mapState.scaleDenominator).to.be.eq(52296.09469372093)
+    })
+
+    it('ConversionError && ValidationError', function () {
+        let mapState = new MapState();
+        expect(mapState).to.be.instanceOf(MapState)
+
+        // Initial state
+        expect(mapState.projection).to.be.eq('EPSG:3857')
+        expect(mapState.center).to.be.an('array').that.have.lengthOf(2).that.deep.equal([0, 0])
+        expect(mapState.resolution).to.be.eq(-1)
+        expect(mapState.size).to.be.an('array').that.have.lengthOf(2).that.deep.equal([0, 0])
+        expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        expect(mapState.scaleDenominator).to.be.eq(-1)
+
+        try {
+            mapState.update({
+                "center": 'foobar'
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ValidationError')
+            expect(error.message).to.be.eq('The value for `center` has to be an array!')
+            expect(error).to.be.instanceOf(ValidationError)
+        } finally {
+            expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        }
+
+        try {
+            mapState.update({
+                "resolution": 'foobar'
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ConversionError')
+            expect(error.message).to.be.eq('`foobar` is not a number!')
+            expect(error).to.be.instanceOf(ConversionError)
+
+            expect(mapState.resolution).to.be.eq(-1)
+        }
+
+        try {
+            mapState.update({
+                "size": 'foobar'
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ValidationError')
+            expect(error.message).to.be.eq('The value for `size` has to be an array!')
+            expect(error).to.be.instanceOf(ValidationError)
+        } finally {
+            expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        }
+
+        try {
+            mapState.update({
+                "extent": 'foobar'
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ValidationError')
+            expect(error.message).to.be.eq('The value for `extent` has to be an array!')
+            expect(error).to.be.instanceOf(ValidationError)
+        } finally {
+            expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        }
+
+        try {
+            mapState.update({
+                "extent": [-1,-1,'error',1]
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ConversionError')
+            expect(error.message).to.be.eq('`error` is not a number!')
+            expect(error).to.be.instanceOf(ConversionError)
+        } finally {
+            expect(mapState.extent).to.be.instanceOf(Extent).that.have.lengthOf(4).that.deep.equal([0, 0, 0, 0])
+        }
+
+        try {
+            mapState.update({
+                "scaleDenominator": 'foobar'
+            })
+        } catch (error) {
+            expect(error.name).to.be.eq('ConversionError')
+            expect(error.message).to.be.eq('`foobar` is not a number!')
+            expect(error).to.be.instanceOf(ConversionError)
+        } finally {
+            expect(mapState.scaleDenominator).to.be.eq(-1)
+        }
+    })
+})
