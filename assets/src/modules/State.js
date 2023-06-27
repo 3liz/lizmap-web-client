39f43190422c6fdd345e816f8b6c99d5d01bc169
rldhont <rldhont@gmail.com>
2023-06-27 20:27:07 +0200

    [JavaScript] Class State extends EventDispatcher
    The `lizMap.mainLizmap.state` dispatches the events:
    * `map.state.changed`
    * `baselayers.selection.changed`
    * `group.visibility.changed`
    * `layer.visibility.changed`
    * `layer.style.changed`
    * `layer.symbol.checked.changed`
    * `layer.selection.changed`
    * `layer.selection.token.changed`
    * `layer.filter.changed`
    * `layer.filter.token.changed`

--- assets/src/modules/State.js
+++ assets/src/modules/State.js
@@ -1,20 +1,23 @@
-import { mainEventDispatcher } from '../modules/Globals.js';
+import EventDispatcher from './../utils/EventDispatcher.js';
 import { MapState } from './state/Map.js';
+import { BaseLayersState } from './state/BaseLayer.js';
 import { LayersAndGroupsCollection } from './state/Layer.js';
 import { MapGroupState } from './state/MapLayer.js';
 import { LayerTreeGroupState } from './state/LayerTree.js';
 
-export class State {
+export class State extends EventDispatcher {
     /**
      * @param {Config} initialCfg - the lizmap initial config instance
      */
     constructor(initialCfg) {
+        super()
         this._initialConfig = initialCfg;
         this._map = new MapState();
+        this._map.addListener(this.dispatch.bind(this), 'map.state.changed');
+        this._baseLayers = null;
         this._collection = null;
         this._rootMapGroup = null;
         this._layerTree = null;
-        mainEventDispatcher.addListener(this._map.update.bind(this._map), 'map.state.changing');
     }
 
     /**
@@ -27,13 +30,36 @@ export class State {
     }
 
     /**
-     * The map state
+     * The base layers state
      *
-     * @type {MapState}
+     * @type {BaseLayersState}
+     **/
+    get baseLayers() {
+        if (this._baseLayers == null) {
+            this._baseLayers = new BaseLayersState(this._initialConfig.baseLayers);
+            // Dispatch events from base layers
+            this._baseLayers.addListener(this.dispatch.bind(this), 'baselayers.selection.changed');
+        }
+        return this._baseLayers;
+    }
+
+    /**
+     * The layers and groups collection
+     *
+     * @type {LayersAndGroupsCollection}
      **/
     get layersAndGroupsCollection() {
         if (this._collection == null) {
             this._collection = new LayersAndGroupsCollection(this._initialConfig.layerTree, this._initialConfig.layersOrder);
+            // Dispatch events from groups and layers
+            this._collection.addListener(this.dispatch.bind(this), 'group.visibility.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.visibility.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.style.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.symbol.checked.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.selection.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.selection.token.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.filter.changed');
+            this._collection.addListener(this.dispatch.bind(this), 'layer.filter.token.changed');
         }
         return this._collection;
     }
