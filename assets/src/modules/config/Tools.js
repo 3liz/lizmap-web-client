282e295240ae455c60866231dcaf8e6648803e72
rldhont <rldhont@gmail.com>
2023-07-24 10:38:19 +0200

    [JavaScript] Remove old code from config/Tools.js

--- assets/src/modules/config/Tools.js
+++ assets/src/modules/config/Tools.js
@@ -1,4 +1,9 @@
-import { ValidationError, ConversionError } from './../Errors.js';
+/**
+ * @module config/Tools.js
+ * @copyright 2023 3Liz
+ * @author DHONT René-Luc
+ * @license MPL-2.0 - Mozilla Public License 2.0 : http://www.mozilla.org/MPL/
+ **/
 
 /**
  * Freeze in depth an object
@@ -17,74 +22,12 @@ export function deepFreeze(object) {
     for (const name of propNames) {
         const value = object[name];
 
-        if (value && typeof value === "object") {1
+        if (value && typeof value === "object") {
             deepFreeze(value);
         }
     }
 
     return Object.freeze(object);
-}
-
-/**
- * Convert a value to Number
- *
- * @param {*} val - A value to convert to number
- *
- * @returns {Number} the converting value
- *
- * @throws {ConversionError}
- **/
-export function convertNumber(val) {
-    const value = val*1;
-    if (isNaN(value)) {
-        throw new ConversionError('`'+val+'` is not a number!');
-    }
-    return value;
-}
-
-/**
- * Convert a value to boolean
- *
- * @param {*} val - A value to convert to boolean
- *
- * @returns {Boolean} the converting value
- *
- * @throws {ConversionError}
- **/
-export function convertBoolean(val) {
-    switch (typeof val) {
-        case 'boolean':
-            return val;
-        case 'number': {
-            if (val === 1) {
-                return true;
-            } else if (val === 0) {
-                return false;
-            }
-            throw new ConversionError('`'+val+'` is not an expected boolean: 1 or 0!');
-        }
-        case 'string': {
-            const value = val.toLowerCase();
-            if (value === 'true' || value === 't'
-                || value === 'yes' || value === 'y'
-                || value === '1') {
-                return true;
-            } else if (value === 'false' || value === 'f'
-                || value === 'no' || value === 'n'
-                || value === '0' || value === '') {
-                return false;
-            }
-            throw new ConversionError('`'+val+'` is not an expected boolean: true, t, yes, y, 1, false, f, no, n, 0 or empty string ``!');
-        }
-        default: {
-            if (val === null) {
-                return false;
-            } else if (Array.isArray(val)) {
-                throw new ConversionError('The Array `['+val+']` is not an expected boolean!');
-            }
-            throw new ConversionError('The Object is not an expected boolean!');
-        }
-    }
 }
 
 /**
@@ -97,60 +40,4 @@ export function convertBoolean(val) {
  **/
 export function getNotContains(source, target) {
     return source.filter(function(item){ return target.indexOf(item) == -1});
-}
-
-/**
- * Class representing an extent
- * @class
- * @augments Array
- **/
-export class Extent extends Array {
-
-    /**
-     * Create an extent
-     * @param {...(number|string)} args - the 4 values describing the extent
-     *
-     * @throws {ValidationError} for number of args different of 4
-     * @throws {ConversionError} for values not number
-     **/
-    constructor(...args) {
-        if (args.length < 4) {
-            throw new ValidationError('Not enough arguments for Extent constructor!');
-        } else if (args.length > 4) {
-            throw new ValidationError('Too many arguments for Extent constructor!');
-        }
-        let values = [];
-        for (const val of args) {
-            values.push(convertNumber(val));
-        }
-        super(...values);
-    }
-
-    /**
-     * @type {Number}
-     **/
-    get xmin() {
-        return this[0];
-    }
-
-    /**
-     * @type {Number}
-     **/
-    get ymin() {
-        return this[1];
-    }
-
-    /**
-     * @type {Number}
-     **/
-    get xmax() {
-        return this[2];
-    }
-
-    /**
-     * @type {Number}
-     **/
-    get ymax() {
-        return this[3];
-    }
 }
