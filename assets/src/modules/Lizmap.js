91263443a32eeaeb47dc233c24cc1bac4b42a463
rldhont <rldhont@gmail.com>
2023-08-24 18:40:58 +0200

    [JavaScript] Remove mainLizmap from Popup module

--- assets/src/modules/Lizmap.js
+++ assets/src/modules/Lizmap.js
@@ -160,7 +160,7 @@ export default class Lizmap {
                 this.wms = new WMS();
                 this.action = new Action();
                 this.featureStorage = new FeatureStorage();
-                this.popup = new Popup();
+                this.popup = new Popup(this.initialConfig, this.state, this.map);
                 this.legend = new Legend(this.state.layerTree);
                 this.search = new Search();
                 this.tooltip = new Tooltip();
