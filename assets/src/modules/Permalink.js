dae2591a121c9cd52960cb7c128ece6c2d51b5ae
nboisteault <nboisteault@3liz.com>
2023-08-25 17:41:06 +0200

    Permalink: handle items opacity

--- assets/src/modules/Permalink.js
+++ assets/src/modules/Permalink.js
@@ -8,14 +8,15 @@ export default class Permalink {
         // Change `checked`, `style` states based on URL fragment
         if (window.location.hash) {
             const items = mainLizmap.state.layersAndGroupsCollection.layers.concat(mainLizmap.state.layersAndGroupsCollection.groups);
-            const [, itemsInURL, stylesInURL] = window.location.hash.substring(1).split('|').map(part => part.split(','));
+            const [, itemsInURL, stylesInURL, opacitiesInURL] = window.location.hash.substring(1).split('|').map(part => part.split(','));
             for (const item of items){
                 if(itemsInURL.includes(encodeURIComponent(item.name))){
+                    const itemIndex = itemsInURL.indexOf(encodeURIComponent(item.name));
                     item.checked = true;
                     if(item.type === 'layer'){
-                        const styleIndex = itemsInURL.indexOf(encodeURIComponent(item.name));
-                        item.wmsSelectedStyleName = decodeURIComponent(stylesInURL[styleIndex]);
+                        item.wmsSelectedStyleName = decodeURIComponent(stylesInURL[itemIndex]);
                     }
+                    item.opacity = parseFloat(opacitiesInURL[itemIndex]);
                 } else {
                     item.checked = false;
                 }
@@ -31,7 +32,7 @@ export default class Permalink {
 
         mainLizmap.state.rootMapGroup.addListener(
             () => this._writeURLFragment(),
-            ['layer.visibility.changed', 'group.visibility.changed', 'layer.style.changed', 'group.style.changed']
+            ['layer.visibility.changed', 'group.visibility.changed', 'layer.style.changed', 'group.style.changed', 'layer.opacity.changed', 'group.opacity.changed']
         );
     }
 
@@ -49,24 +50,30 @@ export default class Permalink {
         }
         hash = bbox.join();
 
-        // Item's visibility
-        // Only write layer's name when visible
-        let visibleItems = [];
-        let styleItems = [];
+        // Item's visibility, style and opacity
+        // Only write layer's properties when visible
+        let itemsVisibility = [];
+        let itemsStyle = [];
+        let itemsOpacity = [];
 
         for (const item of lizMap.mainLizmap.state.rootMapGroup.findMapLayersAndGroups()) {
             if (item.checked){
-                visibleItems.push(item.name);
-                styleItems.push(item.wmsSelectedStyleName);
+                itemsVisibility.push(item.name);
+                itemsStyle.push(item.wmsSelectedStyleName);
+                itemsOpacity.push(item.opacity);
             }
         }
 
-        if (visibleItems.length) {
-            hash += '|' + visibleItems.join();
+        if (itemsVisibility.length) {
+            hash += '|' + itemsVisibility.join();
         }
 
-        if (styleItems.length) {
-            hash += '|' + styleItems.join();
+        if (itemsStyle.length) {
+            hash += '|' + itemsStyle.join();
+        }
+
+        if (itemsOpacity.length) {
+            hash += '|' + itemsOpacity.join();
         }
 
         // Finally override URL fragment
