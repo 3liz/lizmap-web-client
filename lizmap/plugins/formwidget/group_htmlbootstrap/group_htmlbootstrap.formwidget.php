8017a263f73dda3702a4405662616a39bb678116
Laurent Jouanneau <dev@ljouanneau.com>
2023-09-07 23:34:20 +0200

    [internal] refactor the jforms widget group_htmlbootstrap
    To be compatible with a bug fix in jelix 1.8.3-pre

--- lizmap/plugins/formwidget/group_htmlbootstrap/group_htmlbootstrap.formwidget.php
+++ lizmap/plugins/formwidget/group_htmlbootstrap/group_htmlbootstrap.formwidget.php
@@ -28,55 +28,36 @@ class group_htmlbootstrapFormWidget extends group_htmlFormWidget
         $this->outputLabelAsTitle($label, $attr);
     }
 
-    public function outputControl()
+    protected function displayStartGroup($groupId, $label, $checkBoxAttr = array())
     {
-        $attr = $this->getControlAttributes();
-        $value = $this->getValue();
-        $jFormsJsVarName = $this->builder->getjFormsJsVarName();
-
-        if ($this->ctrl->hasCheckbox) {
-            echo '<fieldset id="',$attr['id'],'" class="jforms-ctrl-group"><legend>',
-            '<input ';
-            $chkattr = $attr;
-            $chkattr['class'] = str_replace('jforms-ctrl-group', '', $chkattr['class']);
-            $chkattr['type'] = 'checkbox';
-            $chkattr['id'] = $attr['id'].'_checkbox';
-            $chkattr['value'] = $this->ctrl->valueOnCheck;
-            $chkattr['onclick'] = $jFormsJsVarName.'.getForm(\''.$this->builder->getName().'\').getControl(\''.$this->ctrl->ref.'\').showActivate()';
-            if ($value == $this->ctrl->valueOnCheck) {
-                $chkattr['checked'] = 'true';
-            }
-            $this->_outputAttr($chkattr);
-            echo '> <label for="'.$attr['id'].'_checkbox'.'">',htmlspecialchars($this->ctrl->label),"</label></legend>\n";
-            $this->jsGroupInternal($this->ctrl);
+        if (count($checkBoxAttr) == 0) {
+            echo '<fieldset id="',$groupId,'" class="jforms-ctrl-group"><legend>',htmlspecialchars($label),"</legend>\n";
         } else {
-            echo '<fieldset id="',$attr['id'],'" class="jforms-ctrl-group"><legend>',htmlspecialchars($this->ctrl->label),"</legend>\n";
+            echo '<fieldset id="',$groupId,'" class="jforms-ctrl-group"><legend>',
+            '<input ';
+            $this->_outputAttr($checkBoxAttr);
+            echo '> <label for="'.$checkBoxAttr['id'].'">',htmlspecialchars($label),"</label></legend>\n";
         }
-
         echo '<div class="jforms-table-group">',"\n";
-        foreach ($this->ctrl->getChildControls() as $ctrlref => $c) {
-            if ($c->type == 'submit' || $c->type == 'reset' || $c->type == 'hidden') {
-                continue;
-            }
-            if (!$this->builder->getForm()->isActivated($ctrlref)) {
-                continue;
-            }
-            $widget = $this->builder->getWidget($c, $this);
-            $widget->setLabelAttributes(array('class' => 'control-label'));
-            echo '<div class="control-group">';
-            $widget->outputLabel();
-            echo '<div class="controls">';
-            $widget->outputControl();
-            $widget->outputHelp();
-            echo "</div>\n</div>\n";
-            if ($this->ctrl->hasCheckbox) {
-                $this->parentWidget->addJs("c2.addControl(c);\n");
-            }
-        }
+    }
+
+    /**
+     * @param \Jelix\Forms\HtmlWidget\WidgetInterface $widget
+     */
+    protected function displayChildControl($widget)
+    {
+        $widget->setLabelAttributes(array('class' => 'control-label'));
+        echo '<div class="control-group">';
+        $widget->outputLabel();
+        echo '<div class="controls">';
+        $widget->outputControl();
+        $widget->outputHelp();
+        echo "</div>\n</div>\n";
+    }
+
+    protected function displayEndGroup()
+    {
         echo "</div></fieldset>\n";
-        if ($this->ctrl->hasCheckbox) {
-            $this->parentWidget->addJs("c2.showActivate();\n");
-        }
     }
 
     public function outputControlValue()
