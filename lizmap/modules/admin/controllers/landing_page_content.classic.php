01c48f0e2352f4511d2cd42746a28d71d7fdeb06
Laurent Jouanneau <dev@ljouanneau.com>
2023-10-13 12:07:42 +0200

    Landing page: possibility to add contents at the bottom of the page

--- lizmap/modules/admin/controllers/landing_page_content.classic.php
+++ lizmap/modules/admin/controllers/landing_page_content.classic.php
@@ -3,7 +3,7 @@
  * Lizmap administration : landing page content.
  *
  * @author    3liz
- * @copyright 2016 3liz
+ * @copyright 2016-2023 3liz
  *
  * @see      http://3liz.com
  *
@@ -26,15 +26,27 @@ class landing_page_contentCtrl extends jController
         /** @var jResponseHtml $rep */
         $rep = $this->getResponse('html');
 
-        // Create the form
-        $form = jForms::create('admin~landing_page_content');
+        // Create the form, or get the existing one if we are here because of
+        // a failed check
+        $form = jForms::get('admin~landing_page_content');
+        if (!$form) {
+            $form = jForms::create('admin~landing_page_content');
+        }
 
         // Get HTML content
-        $HTMLContentFile = jApp::varPath('lizmap-theme-config/landing_page_content.html');
-        if (file_exists($HTMLContentFile)) {
-            $HTMLContent = jFile::read($HTMLContentFile);
+        $TopHTMLContentFile = jApp::varPath('lizmap-theme-config/landing_page_content.html');
+        if (file_exists($TopHTMLContentFile)) {
+            $HTMLContent = jFile::read($TopHTMLContentFile);
             if ($HTMLContent) {
                 $form->setData('HTMLContent', $HTMLContent);
+            }
+        }
+
+        $BottomHTMLContentFile = jApp::varPath('lizmap-theme-config/landing_page_content_bottom.html');
+        if (file_exists($BottomHTMLContentFile)) {
+            $HTMLContent = jFile::read($BottomHTMLContentFile);
+            if ($HTMLContent) {
+                $form->setData('BottomHTMLContent', $HTMLContent);
             }
         }
 
@@ -54,66 +66,33 @@ class landing_page_contentCtrl extends jController
      */
     public function save()
     {
-        /** @var null|jFormsBase $form */
-        $form = jForms::get('admin~landing_page_content');
+        try {
+            $form = jForms::fill('admin~landing_page_content');
+            if (!$form || !$form->check()) {
+                // no form... this was a direct call to the save action
+                // or content is invalid
+                return $this->redirect('landing_page_content:index');
+            }
+        } catch (\jException $e) {
+            // invalid CSRF token or other technical errors
+            jMessage::add(jLocale::get('admin~admin.landingPageContent.error.submit'), 'error');
 
-        // token
-        $token = $this->param('__JFORMS_TOKEN__');
-        // redirection vers la page d'erreur
-        if (!$token) {
-            /** @var jResponseRedirect $rep */
-            $rep = $this->getResponse('redirect');
-            $rep->action = 'landing_page_content:index';
-
-            return $rep;
-        }
-
-        // If the form is not defined, redirection
-        if (!$form) {
-            /** @var jResponseRedirect $rep */
-            $rep = $this->getResponse('redirect');
-            $rep->action = 'landing_page_content:index';
-
-            return $rep;
-        }
-
-        // Set the other form data from the request data
-        $form->initFromRequest();
-
-        // Check the form
-        $ok = true;
-        if (!$form->check()) {
-            $ok = false;
-        }
-
-        // Errors : redirection
-        if (!$ok) {
-            /** @var jResponseRedirect $rep */
-            $rep = $this->getResponse('redirect');
-            $rep->action = 'landing_page_content:index';
-            $rep->params['errors'] = '1';
-
-            return $rep;
+            return $this->redirect('landing_page_content:index');
         }
 
         // Save HTML content
         $fileWriteOK = jFile::write(jApp::varPath('lizmap-theme-config/landing_page_content.html'), $form->getData('HTMLContent'));
-
-        // Errors : redirection
         if (!$fileWriteOK) {
-            /** @var jResponseRedirect $rep */
-            $rep = $this->getResponse('redirect');
-            $rep->action = 'landing_page_content:index';
-            $rep->params['errors'] = '1';
-
-            return $rep;
+            $form->setErrorOn('HTMLContent', jLocale::get('admin~admin.landingPageContent.error.save'));
+        }
+        $fileWriteOK2 = jFile::write(jApp::varPath('lizmap-theme-config/landing_page_content_bottom.html'), $form->getData('BottomHTMLContent'));
+        if (!$fileWriteOK2) {
+            $form->setErrorOn('BottomHTMLContent', jLocale::get('admin~admin.landingPageContent.error.save'));
+        }
+        if ($fileWriteOK && $fileWriteOK2) {
+            jMessage::add(jLocale::get('admin~admin.landingPageContent.saved'), 'ok');
         }
 
-        /** @var jResponseRedirect $rep */
-        $rep = $this->getResponse('redirect');
-        // Redirect to the edition page
-        $rep->action = 'landing_page_content:index';
-
-        return $rep;
+        return $this->redirect('landing_page_content:index');
     }
 }
