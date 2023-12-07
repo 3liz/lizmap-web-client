8a9a7f7c99929a46f4324fa24ffa834909e6c08a
rldhont <rldhont@gmail.com>
2023-12-07 16:32:36 +0100

    Fix switcher layers because of removing OL2 codes

--- tests/end2end/cypress/integration/zoom-to-layer-ghaction.js
+++ tests/end2end/cypress/integration/zoom-to-layer-ghaction.js
@@ -1,4 +1,53 @@
 describe('Zoom to layer', function() {
+    beforeEach(function () {
+        // Runs before each tests in the block
+        cy.intercept('*REQUEST=GetMap*',
+        { middleware: true },
+        (req) => {
+            req.on('before:response', (res) => {
+                // force all API responses to not be cached
+                // It is needed when launching tests multiple time in headed mode
+                res.headers['cache-control'] = 'no-store'
+            })
+        }).as('getMap')
+
+        cy.intercept('POST','*service*', (req) => {
+            if (typeof req.body == 'string') {
+                const req_body = req.body.toLowerCase()
+                if (req_body.includes('service=wms') ) {
+                    if (req_body.includes('request=getfeatureinfo'))
+                        req.alias = 'postGetFeatureInfo'
+                    else if (req_body.includes('request=getselectiontoken'))
+                        req.alias = 'postGetSelectionToken'
+                    else
+                        req.alias = 'postToService'
+                } else if (req_body.includes('service=wfs') ) {
+                    if (req_body.includes('request=getfeature'))
+                        req.alias = 'postGetFeature'
+                    else if (req_body.includes('request=describefeaturetype'))
+                        req.alias = 'postDescribeFeatureType'
+                    else
+                        req.alias = 'postToService'
+                } else
+                    req.alias = 'postToService'
+            } else
+                req.alias = 'postToService'
+          })
+
+        cy.intercept({
+            method: 'POST',
+            url: '**/lizmap/service',
+            middleware: true
+        },
+        (req) => {
+            req.on('before:response', (res) => {
+                // force all API responses to not be cached
+                // It is needed when launching tests multiple time in headed mode
+                res.headers['cache-control'] = 'no-store'
+            })
+        }).as('getFeature')
+    })
+
     it('Projection EPSG:4326', function(){
         // Go to world 4326
         cy.visit('/index.php/view/map/?repository=testsrepository&project=world-4326')
@@ -9,6 +58,7 @@ describe('Zoom to layer', function() {
 
         // Click on the map to get a popup
         cy.mapClick(480,340)
+        cy.wait('@postGetFeatureInfo')
 
         // Check popup displayed
         cy.get('#mapmenu li.nav-dock.popupcontent').should('have.class', 'active')
@@ -21,6 +71,7 @@ describe('Zoom to layer', function() {
 
         // Click on the map to get no popup
         cy.mapClick(480,340)
+        cy.wait('@postGetFeatureInfo')
 
         // Check no popup displayed
         cy.get('#mapmenu li.nav-dock.popupcontent').should('have.class', 'active')
@@ -40,6 +91,7 @@ describe('Zoom to layer', function() {
 
         // Click on the map to get a popup
         cy.mapClick(480,340)
+        cy.wait('@postGetFeatureInfo')
 
         // Check popup displayed
         cy.get('#mapmenu li.nav-dock.popupcontent').should('have.class', 'active')
@@ -52,6 +104,7 @@ describe('Zoom to layer', function() {
 
         // Click on the map to get no popup
         cy.mapClick(480,340)
+        cy.wait('@postGetFeatureInfo')
 
         // Check no popup displayed
         cy.get('#mapmenu li.nav-dock.popupcontent').should('have.class', 'active')
