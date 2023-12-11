4089de0db39924be0493bb5147b063cd57ffaa30
nworr <43475951+nworr@users.noreply.github.com>
2023-12-11 16:09:03 +0100

    [end2end] cors and projet filename test converted from cypress to playwright (#4009)
    * cors and projet filename converted from cypress to playwright
    
    * create dedicated end2end conf to ignore local tests

---
+++ tests/end2end/playwright/localonly/cors.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Cors', function () {
+
+  test('send authorized request', async function ({ page }, testInfo) {
+    await page.goto('http://othersite.local:8130');
+    await page.locator('#launch-request').click();
+    await expect(page.locator('#status')).toHaveText('200');
+    await expect(page.locator('#response')).not.toBeEmpty();
+    expect(testInfo.stderr).toHaveLength(0);
+
+  });
+
+
+  test('send unauthorized request', async function ({ page }, testInfo) {
+    await page.goto(
+      'http://othersite.local:8130');
+    await page.locator('#launch-request-bad').click();
+    await expect(page.locator('#status_bad')).toBeEmpty();
+    await expect(page.locator('#response_bad')).toBeEmpty();
+    expect(testInfo.stderr).not.toBe('');
+  });
+
+});
