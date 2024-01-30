f8e459092d32030e6218acf6d70045ef2d5438c3
rmartin <rmartin@3liz.com>
2024-01-30 13:41:26 +0100

    [end2end] assert badge is displayed for bad repository

---
+++ tests/end2end/playwright/bad-repository.spec.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('Bad repository in conf', () => {
+
+  test.use({ storageState: 'playwright/.auth/admin.json' });
+
+  test.beforeEach(async ({ page }) => {
+    // Go to repos page
+    await page.goto('/admin.php/admin/maps', { waitUntil: 'networkidle' });
+  });
+
+  test('Badge displayed for bad repository', async ({ page }) => {
+    await expect( page.locator('legend').filter({ hasText: 'badrepository' })).toBeVisible();
+    await expect( page.locator('legend').filter({ hasText: 'badrepository' }).getByText('Path not found')).toBeVisible();
+  });
+});
