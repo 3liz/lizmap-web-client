1f380336b1624a1f5738f6dd6751f7003a831e71
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-02-27 16:10:30 +0100

    Tests - Add E2E tests for warnings display

---
+++ tests/end2end/playwright/project_load_warning.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Project warnings in CFG as admin', () => {
+test.use({ storageState: 'playwright/.auth/admin.json' });
+
+  test('Visit map with a warning', async ({ page }) => {
+    const url = '/index.php/view/map?repository=testsrepository&project=project_cfg_warnings';
+    await page.goto(url, { waitUntil: 'networkidle' });
+
+    await expect(page.locator("#lizmap-warning-message")).toBeVisible();
+  });
+
+});
+
+test.describe('Project warnings in CFG as anonymous', () => {
+  
+    test('Visit map without a warning', async ({ page }) => {
+      const url = '/index.php/view/map?repository=testsrepository&project=project_cfg_warnings';
+      await page.goto(url, { waitUntil: 'networkidle' });
+  
+      await expect(page.locator("#lizmap-warning-message")).toHaveCount(0);
+    });
+  
+});
