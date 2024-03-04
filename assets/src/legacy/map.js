baccaca97e94cc5d628dcf0ea5dc237511e9cbc5
rldhont <rldhont@gmail.com>
2024-03-04 11:08:13 +0100

    [Bugfix] Language for compact table of children in Popup
    The language is missing for DataTable for compact table of children in Popup
    
    Funded by Terre de Provence Agglomération

--- assets/src/legacy/map.js
+++ assets/src/legacy/map.js
@@ -3348,7 +3348,9 @@ window.lizMap = function() {
             }
 
             // Handle compact-tables/explode-tables behaviour
-            $('.lizmapPopupChildren .popupAllFeaturesCompact table').DataTable();
+            $('.lizmapPopupChildren .popupAllFeaturesCompact table').DataTable({
+                language: { url:lizUrls["dataTableLanguage"] }
+            });
 
             $('.lizmapPopupChildren .compact-tables, .lizmapPopupChildren .explode-tables').tooltip();
 
