b70cf9cb28690f62b433134a3631d05957bb65ce
nboisteault <nboisteault@3liz.com>
2024-03-18 14:06:56 +0100

    e2e: better wait for image stability

--- tests/end2end/playwright/startup.spec.js
+++ tests/end2end/playwright/startup.spec.js
@@ -1,34 +1,35 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test.describe('Startup', () => {
 
   test('Zoom to features extent', async ({ page }) => {
+    const getMapPromise = page.waitForRequest(/GetMap/);
     const url = '/index.php/view/map/?repository=testsrepository&project=startup&layer=sousquartiers&filter="quartmno"%20=%20%27PA%27%20OR%20"quartmno"%20=%20%27HO%27';
     await page.goto(url, { waitUntil: 'networkidle' });
+
+    // Wait for image stability
+    await getMapPromise;
 
     // Hide all elements but #map and its children
     await page.$eval("*", el => el.style.visibility = 'hidden');
     await page.$eval("#newOlMap, #newOlMap *", el => el.style.visibility = 'visible');
 
-    // Wait for image stability
-    await page.waitForTimeout(300);
-
     expect(await page.locator('#newOlMap').screenshot()).toMatchSnapshot('zoom-features-extent.png', {
       maxDiffPixels: 700
     });
   });
-  
+
   test('Projects with dot or space can load', async ({page}) => {
     const url_dots = '/index.php/view/map/?repository=testsrepository&project=base_layers+with+space';
     await page.goto(url_dots, { waitUntil: 'networkidle' });
     await page.$eval("#map, #map *", el => el.style.visibility = 'visible');
     await expect( page.locator('#node-quartiers')).toHaveCount(1);
-    
+
     const url_space = '/index.php/view/map/?repository=testsrepository&project=base_layers.withdot';
     await page.goto(url_space, { waitUntil: 'networkidle' });
     await page.$eval("#map, #map *", el => el.style.visibility = 'visible');
-     
+
     await expect( page.locator('#node-quartiers')).toHaveCount(1);
   });
 });
