c9ffef3e2d6a10bb58c6f1bc077e5a19826da9e0
nboisteault <nboisteault@3liz.com>
2024-04-08 18:08:28 +0200

    e2e: test xss

---
+++ tests/end2end/playwright/xss.spec.js
@@ -0,0 +1,41 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test.describe('XSS', () => {
+    // Test that flawed data are sanitized before being displayed
+    test('No dialog from inline JS alert() appears', async ({ page }) => {
+
+        let dialogOpens = 0;
+        page.on('dialog', dialog => {
+            dialog.accept();
+            dialogOpens++;
+        });
+
+        const url = '/index.php/view/map/?repository=testsrepository&project=xss';
+        await page.goto(url, { waitUntil: 'networkidle' });
+
+        // Edition: add XSS data
+        await page.locator('#button-edition').click();
+        await page.locator('#edition-draw').click();
+
+        await page.locator('#jforms_view_edition input[name="description"]').fill('<script>alert("XSS")</script>');
+
+        await page.locator('#jforms_view_edition__submit_submit').click();
+
+        // Open popup
+        await page.locator('#newOlMap').click({
+            position: {
+                x: 415,
+                y: 290
+            }
+        });
+
+        // Open attribute table
+        await page.locator('#button-attributeLayers').click();
+        await page
+        .locator('button[value="xss_layer"].btn-open-attribute-layer')
+        .click({ force: true });
+
+        expect(dialogOpens).toEqual(0);
+    });
+});
