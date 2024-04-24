5e78601e4f8eda49605530dd4414bfb0bf9aff6a
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-04-24 15:55:49 +0200

    Tests - Add local file, use "contain" instead of "equal", move to liz.lizmap.com

--- tests/end2end/cypress/integration/external_wms_layer-ghaction.js
+++ tests/end2end/cypress/integration/external_wms_layer-ghaction.js
@@ -2,24 +2,11 @@
 
 // The QGIS project contains 3 layers:
 
-// * **bakeries**: a SHP point layer in EPSG:4326
-// * **IGN plan**
-//   * **Plan IGN v2 2154 jpeg**: a WMS layer from the French IGN organization, requested in `image/jpeg`
-//   * **Plan IGN v2 2154 png**: the same layer but requested in `image/png`.
-
-
-// ## Procedure
-
-// Load the map `external_wms_layer`,
-
-// * [ ] open your browser developer panel with `CTRL+MAJ+i`,
-// * [ ] activate the `Network` tab with the `Images` filter (to see the requested images in the log),
-// * [ ] empty the log (search for a `bin` icon in the `Network` panel),
-// * [ ] move the map to trigger a map refresh,
-// * [ ] check the image requested for the active layer `bakeries` is in **PNG** format
-// * [ ] check the image requested for the active layer `Plan IGN v2 2154 jpeg` is in **JPEG** format
-// * [ ] activate the layer `Plan IGN v2 2154 png` in the legend
-// * [ ] check the image requested for the active layer `Plan IGN v2 2154 png` is in **PNG** format
+// World layer, included in the project
+// 2 polygons layers hosted on
+// https://liz.lizmap.com/tests/index.php/view/map?repository=testse2elwc&project=base_external_layers
+// one in `image/jpeg`
+// and one in `image/png`.
 
 describe('External WMS layers', function () {
 
@@ -39,31 +26,40 @@ describe('External WMS layers', function () {
                 })
             }).as('getMap')
 
-        // WMS https://demo.lizmap.com/lizmap/index.php/lizmap/service/?repository=cypress&project=base_external_layers&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities
-        // URL demo.lizmap.com
-        // Repository cypress
+        // WMS https://liz.lizmap.com/tests/index.php/lizmap/service?repository=testse2elwc&project=base_external_layers&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetCapabilities
+        // URL liz.lizmap.com
+        // Repository testse2elwc
         // Project base_external_layers
 
-        // As PNG
+        // As PNG for a remote WMS
         cy.get('#node-png').click()
         cy.wait('@getMap').then((interception) => {
             expect(interception.response.headers['content-type'], 'expect mime type to be image/png').to.equal('image/png')
             expect(interception.request.headers['host'], 'expect not localhost').to.not.equal(undefined)
-            expect(interception.request.headers['host'], 'expect not localhost').to.not.equal('localhost')
+            expect(interception.request.headers['host'], 'expect not localhost').to.not.contain('localhost')
         })
         cy.get('#node-png').click()
 
         // Wait for all GetMap requests
         cy.wait(4000)
 
-        // As JPEG
+        // As JPEG for a remote WMS
         cy.get('#node-jpeg').click()
         cy.wait('@getMap').then((interception) => {
             expect(interception.response.headers['content-type'], 'expect mime type to be image/jpeg').to.equal('image/jpeg')
             expect(interception.request.headers['host'], 'expect not localhost').to.not.equal(undefined)
-            expect(interception.request.headers['host'], 'expect not localhost').to.not.equal('localhost')
+            expect(interception.request.headers['host'], 'expect not localhost').to.not.contain('localhost')
         })
         cy.get('#node-jpeg').click()
+
+
+        // Local layer
+        cy.get('#node-world').click()
+        cy.wait('@getMap').then((interception) => {
+            expect(interception.response.headers['content-type'], 'expect mime type to be image/webp').to.equal('image/webp')
+            expect(interception.request.headers['host'], 'expect to be localhost').to.contain('localhost')
+        })
+        cy.get('#node-world').click()
 
     })
 })
