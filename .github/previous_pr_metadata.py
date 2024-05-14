ce3a45903c73b4e6982c8160cecd72f821499493
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-05-14 17:38:26 +0200

    Try backport bot

---
+++ .github/previous_pr_metadata.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import os
+import requests
+
+from typing import Optional, Tuple
+
+
+def current_metadata(token: str, repo: str, ref: str) -> Tuple[bool, str, str]:
+    """ Get current PR metadata. """
+    r = requests.get(
+        f"https://api.github.com/repos/{repo}/pulls/{ref}",
+        headers={
+            'Authorization': f'Bearer {token}'
+        }
+    )
+    metadata = r.json()
+    is_backport = metadata.get('title').startswith('[Backport') and metadata.get('user').get('login') == '3liz-bot'
+    if not is_backport:
+        return False, "", ""
+    body = metadata.get('body')
+    parent_number = body.split('\n')[0]
+    parent_number = parent_number.split('#')[1]
+    return is_backport, parent_number, body
+
+
+def parent_metadata(token: str, repo: str, ref: str):
+    """ Get current PR metadata. """
+    r = requests.get(
+        f"https://api.github.com/repos/{repo}/pulls/{ref}",
+        headers={
+            'Authorization': f'Bearer {token}'
+        }
+    )
+    metadata = r.json()
+    labels_info = metadata.get('labels')
+    is_sponsored = False
+    sponsor = ""
+    labels = []
+    for label in labels_info:
+        if label.get('id') == '1933297134':
+            is_sponsored = True
+        labels.append(label.get("name"))
+    body = metadata.get('body')
+    body = body.split('Funded by')
+    if len(body) >= 2:
+        body = body[1].split('\n')
+        sponsor = body[0]
+    return is_sponsored, sponsor.strip(), labels
+
+
+if __name__ == "__main__":
+    try:
+        token = os.getenv("GITHUB_TOKEN")
+        repo = os.getenv("GITHUB_REPOSITORY")
+        github_ref = os.getenv("GITHUB_PR_REF")
+        is_backport, parent_ref, description = current_metadata(token=token, repo=repo, ref=github_ref)
+        if not is_backport:
+            raise Exception
+
+        is_sponsored, sponsor, labels = parent_metadata(token=token, repo=repo, ref=parent_ref)
+        labels_str = ','.join(labels)
+        if os.environ['GITHUB_OUTPUT']:
+            with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+                print(f'labels={labels_str}', file=fh)
+            if is_sponsored and sponsor:
+                with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+                    print(f'sponsor={sponsor}', file=fh)
+
+                with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+                    print(f'description={description}\n{sponsor}', file=fh)
+
+    except Exception:
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+            print(f'labels=', file=fh)
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+            print(f'sponsor=', file=fh)
+        with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+            print(f'description=', file=fh)
