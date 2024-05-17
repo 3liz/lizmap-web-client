3e009de883a39acd3d4acc2d5cd6ff3a751f4ae8
rldhont <rldhont@gmail.com>
2024-05-17 13:09:25 +0200

    Tests e2e Base layers enhancing
    Adding tests on base layers projects

--- tests/end2end/playwright/base-layers.spec.js
+++ tests/end2end/playwright/base-layers.spec.js
@@ -1,13 +1,29 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+const gotoMap = async (url, page) => {
+    // Wait for WMS GetCapabilities promise
+    let getCapabilitiesWMSPromise = page.waitForRequest(/SERVICE=WMS&REQUEST=GetCapabilities/);
+    // Wait for WMS GetLegendGraphic promise
+    const getLegendGraphicPromise = page.waitForRequest(request => request.method() === 'POST' && request.postData() != null && request.postData()?.includes('GetLegendGraphic') === true);
+    await page.goto(url);
+    // Wait for WMS GetCapabilities
+    await getCapabilitiesWMSPromise;
+    // Wait for WMS GetLegendGraphic
+    await getLegendGraphicPromise;
+    // Check no error message displayed
+    expect(page.getByText('An error occurred while loading this map. Some necessary resources may temporari')).toHaveCount(0);
+    // Wait to be sure the map is ready
+    await page.waitForTimeout(1000)
+}
+
 test.describe('Base layers', () => {
 
     const locale = 'en-US';
 
     test.beforeEach(async ({ page }) => {
         const url = '/index.php/view/map/?repository=testsrepository&project=base_layers';
-        await page.goto(url, { waitUntil: 'networkidle' });
+        await gotoMap(url, page);
     });
 
     test('Base layers list', async ({ page }) => {
@@ -32,4 +48,52 @@ test.describe('Base layers', () => {
         await getMapRequestPromise;
         await expect(page.locator('#overview-bar .ol-scale-text')).toHaveText('1 : ' + (144448).toLocaleString(locale));
     });
+})
+
+test.describe('Base layers user defined', () => {
+
+    test.beforeEach(async ({ page }) => {
+        const url = '/index.php/view/map/?repository=testsrepository&project=base_layers_user_defined';
+        await gotoMap(url, page);
+    });
+
+    test('Base layers list', async ({ page }) => {
+        await expect(page.locator('lizmap-base-layers select option')).toHaveCount(12);
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('OSM TMS internal');
+        await page.locator('lizmap-base-layers select').selectOption('group with many layers and shortname');
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('group with many layers and shortname');
+    });
+
+})
+
+test.describe('Base layers with space', () => {
+
+    test.beforeEach(async ({ page }) => {
+        const url = '/index.php/view/map/?repository=testsrepository&project=base_layers+with+space';
+        await gotoMap(url, page);
+    });
+
+    test('Base layers list', async ({ page }) => {
+        await expect(page.locator('lizmap-base-layers select option')).toHaveCount(6);
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('empty');
+        await page.locator('lizmap-base-layers select').selectOption('osm-mapnik');
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('osm-mapnik');
+    });
+
+})
+
+test.describe('Base layers withdot', () => {
+
+    test.beforeEach(async ({ page }) => {
+        const url = '/index.php/view/map/?repository=testsrepository&project=base_layers.withdot';
+        await gotoMap(url, page);
+    });
+
+    test('Base layers list', async ({ page }) => {
+        await expect(page.locator('lizmap-base-layers select option')).toHaveCount(6);
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('empty');
+        await page.locator('lizmap-base-layers select').selectOption('osm-mapnik');
+        await expect(page.locator('lizmap-base-layers select')).toHaveValue('osm-mapnik');
+    });
+
 })
