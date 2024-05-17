1e7d744548fa976ee7f1aad44469b68083c578af
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-05-17 10:27:39 +0200

    CI - Backport, fix wrong variable name

--- .github/previous_pr_metadata.py
+++ .github/previous_pr_metadata.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
         # print(f"IS backport : {is_backport}")
         # print(f"Parent ID : {parent_ref}")
         # print(f"Desc : {description}")
-        is_sponsored, sponsor, labels = parent_metadata(token=token, repo=repo, ref=parent_ref)
+        is_sponsored, sponsor, labels = parent_metadata(token=token, repo=repo, ref_pr_parent=parent_ref)
         labels_str = ','.join([f'"{l}"' for l in labels])
         labels_str = f"[{labels_str}]"
         # print("Parent PR :")
