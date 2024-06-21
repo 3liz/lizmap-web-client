878129958f81a146fe71722ff36a05470f9eac79
rldhont <rldhont@gmail.com>
2024-06-21 18:16:47 +0200

    [Bugfix] When download failed
    * Fixed when status is 400
    * Display a default message

--- assets/src/legacy/map.js
+++ assets/src/legacy/map.js
@@ -4873,17 +4873,20 @@ window.lizMap = function() {
           // Related PR for QGIS Master https://github.com/qgis/QGIS/pull/47051
           // It should be fixed for 3.24.1 and 3.22.5
           if (this.status == 400) {
-            // Check for parenthesis inside the layer name
-            // There is a bug to be fixed in QGIS Server WFS request for this context
-            var typeName = parameters['TYPENAME'];
-            const parenthesis_regex = /[\(\)]/g;
-            const has_parenthesis = typeName.match(parenthesis_regex);
-            if (has_parenthesis) {
-              var error_message = 'The selected features cannot be exported due to a known bug in QGIS Server.';
-              error_message += '<br/>Please ask the map editor to remove the parenthesis in the layer name.';
-            } else {
-              var error_message = lizDict['layer.export.unknown.export.error'];
-            };
+            var error_message = lizDict['download.error'];
+            if ('TYPENAME' in parameters) {
+              // Check for parenthesis inside the layer name
+              // There is a bug to be fixed in QGIS Server WFS request for this context
+              var typeName = parameters['TYPENAME'];
+              const parenthesis_regex = /[\(\)]/g;
+              const has_parenthesis = typeName.match(parenthesis_regex);
+              if (has_parenthesis) {
+                error_message = 'The selected features cannot be exported due to a known bug in QGIS Server.';
+                error_message += '<br/>Please ask the map editor to remove the parenthesis in the layer name.';
+              } else {
+                error_message = lizDict['layer.export.unknown.export.error'];
+              };
+            }
 
             mAddMessage(error_message, 'error', true);
             return false;
