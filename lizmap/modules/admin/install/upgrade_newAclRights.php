a664291257dd6386d8a8c502470c55b2db0ee41b
Laurent Jouanneau <dev@ljouanneau.com>
2024-07-03 13:41:51 +0200

    Fix installation: some upgraders were not launched when upgrading from 3.5

--- lizmap/modules/admin/install/upgrade_newAclRights.php
+++ lizmap/modules/admin/install/upgrade_newAclRights.php
@@ -3,11 +3,12 @@
 class adminModuleUpgrader_newAclRights extends jInstallerModule
 {
     public $targetVersions = array(
-        '3.6.1-beta.1',
-        '3.7.0-alpha.1',
+        // '3.6.1-beta.1',
+        '3.6.14-rc.1',
+        '3.7.9-rc.1',
+        '3.8.0-rc.3',
     );
-
-    public $date = '2023-01-06';
+    public $date = '2024-07-03'; // original:'2023-01-06'
 
     public function install()
     {
@@ -24,18 +25,20 @@ class adminModuleUpgrader_newAclRights extends jInstallerModule
             jAcl2DbManager::createRight('lizmap.admin.lizmap.log.delete', 'admin~jacl2.lizmap.admin.lizmap.log.delete', 'lizmap.admin.grp');
 
             // New rights for admins
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.project.list.view');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.home.page.update');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.theme.view');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.theme.update');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.server.information.view');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.lizmap.log.view');
-            jAcl2DbManager::addRight('admins', 'lizmap.admin.lizmap.log.delete');
+            if (jAcl2DbUserGroup::getGroup('admins')) {
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.project.list.view');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.home.page.update');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.theme.view');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.theme.update');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.server.information.view');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.lizmap.log.view');
+                jAcl2DbManager::addRight('admins', 'lizmap.admin.lizmap.log.delete');
+            }
 
             // Create a new publishers group
             jAcl2DbUserGroup::createGroup('Publishers', 'publishers');
 
-            // Add the rights the the publishers groupe
+            // Add the rights the publishers groupe
             jAcl2DbManager::addRight('publishers', 'lizmap.admin.project.list.view');
             jAcl2DbManager::addRight('publishers', 'lizmap.admin.server.information.view');
         }
--- lizmap/modules/lizmap/install/upgrade_commconfig.php
+++ lizmap/modules/lizmap/install/upgrade_commconfig.php
@@ -3,11 +3,14 @@
 class lizmapModuleUpgrader_commconfig extends jInstallerModule
 {
     public $targetVersions = array(
-        '3.7.0-alpha.3',
-        '3.6.6',
+        // '3.7.0-alpha.3',
+        // '3.6.6',
+        '3.6.14-rc.1',
+        '3.7.9-rc.1',
+        '3.8.0-rc.3',
     );
 
-    public $date = '2023-08-31';
+    public $date = '2024-07-03'; // original : '2023-08-31'
 
     public function install()
     {
