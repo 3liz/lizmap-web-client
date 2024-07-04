21f99fb87fab7427a70a0a746d71099a9b58900d
rldhont <rldhont@gmail.com>
2024-07-04 10:49:26 +0200

    Tests e2e playwright: Catching errors

--- tests/end2end/playwright/globals.js
+++ tests/end2end/playwright/globals.js
@@ -6,21 +6,30 @@ export async function gotoMap(url, page, check = true) {
 
     // Wait for WMS GetCapabilities promise
     let getCapabilitiesWMSPromise = page.waitForRequest(/SERVICE=WMS&REQUEST=GetCapabilities/);
-    // Wait for WMS GetLegendGraphic promise
-    const getLegendGraphicPromise = page.waitForRequest(request => request.method() === 'POST' && request.postData() != null && request.postData()?.includes('GetLegendGraphic') === true);
     await page.goto(url);
-
-    if (!check) {
-        expect(page.getByText('An error occurred while loading this map. Some necessary resources may temporari')).toHaveCount(1);
-    }
-    // Normal check about the map
 
     // Wait for WMS GetCapabilities
     await getCapabilitiesWMSPromise;
-    // Wait for WMS GetLegendGraphic
-    await getLegendGraphicPromise;
-    // Check no error message displayed
-    expect(page.getByText('An error occurred while loading this map. Some necessary resources may temporari')).toHaveCount(0);
-    // Wait to be sure the map is ready
-    await page.waitForTimeout(1000)
+    if (check) {
+        // Wait for WMS GetLegendGraphic promise
+        const getLegendGraphicPromise = page.waitForRequest(request => request.method() === 'POST' && request.postData() != null && request.postData()?.includes('GetLegendGraphic') === true);
+        // Normal check about the map
+        // Wait for WMS GetLegendGraphic
+        await getLegendGraphicPromise;
+        // No error
+        await expect(page.locator('p.error-msg')).toHaveCount(0);
+        await expect(page.locator('#switcher lizmap-treeview ul li')).not.toHaveCount(0);
+        // Check no error message displayed
+        await expect(page.getByText('An error occurred while loading this map. Some necessary resources may temporari')).toHaveCount(0);
+        // Wait to be sure the map is ready
+        await page.waitForTimeout(1000)
+    } else {
+        // Error
+        await expect(page.locator('p.error-msg')).toHaveCount(1);
+        await expect(page.locator('#switcher lizmap-treeview ul li')).toHaveCount(0);
+        // Error message displayed
+        await expect(page.getByText('An error occurred while loading this map. Some necessary resources may temporari')).toBeVisible();
+        // Go back home link
+        await expect(page.getByRole('link', { name: 'Go back to the home page.' })).toHaveCount(1);
+    }
 }
