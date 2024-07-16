2931a1c2cd5305274138961277215fa3ee086dcb
nboisteault <nboisteault@3liz.com>
2024-07-16 16:09:48 +0200

    Fix: zoom to feature when `limitDataToBbox` is true

--- assets/src/legacy/attributeTable.js
+++ assets/src/legacy/attributeTable.js
@@ -328,8 +328,14 @@ var lizAttributeTable = function() {
 
                 // Calculate bbox from map extent if needed
                 if (config.options?.limitDataToBbox == 'True') {
-                    wfsParams['BBOX'] = lizMap.mainLizmap.map.getView().calculateExtent();
-                    wfsParams['SRSNAME'] = lizMap.mainLizmap.map.getView().getProjection().getCode();
+                    const mapExtent = lizMap.mainLizmap.map.getView().calculateExtent();
+                    const mapExtent4326 = lizMap.mainLizmap.transformExtent(
+                        mapExtent,
+                        lizMap.mainLizmap.map.getView().getProjection().getCode(),
+                        'EPSG:4326'
+                    );
+                    wfsParams['BBOX'] = mapExtent4326;
+                    wfsParams['SRSNAME'] = 'EPSG:4326';
                 }
 
                 const getFeatureRequest = lizMap.mainLizmap.wfs.getFeature(wfsParams);
@@ -433,8 +439,14 @@ var lizAttributeTable = function() {
 
                 // Calculate bbox from map extent if needed
                 if (config.options?.limitDataToBbox == 'True') {
-                    wfsParams['BBOX'] = lizMap.mainLizmap.map.getView().calculateExtent();
-                    wfsParams['SRSNAME'] = lizMap.mainLizmap.map.getView().getProjection().getCode();
+                    const mapExtent = lizMap.mainLizmap.map.getView().calculateExtent();
+                    const mapExtent4326 = lizMap.mainLizmap.transformExtent(
+                        mapExtent,
+                        lizMap.mainLizmap.map.getView().getProjection().getCode(),
+                        'EPSG:4326'
+                    );
+                    wfsParams['BBOX'] = mapExtent4326;
+                    wfsParams['SRSNAME'] = 'EPSG:4326';
                 }
 
                 const getFeatureRequest = lizMap.mainLizmap.wfs.getFeature(wfsParams);
