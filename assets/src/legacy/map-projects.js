b10d2786d6c1b67d2632ecae2183e96982e5ecab
rldhont <rldhont@gmail.com>
2024-07-23 16:35:27 +0200

    Projects switcher in maps enhancement

--- assets/src/legacy/map-projects.js
+++ assets/src/legacy/map-projects.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+    function loadProjDefinition( aCRS, aRep, aProj, aCallback ) {
+        var proj = aCRS.replace(/^\s+|\s+$/g, ''); // trim();
+        if ( proj in Proj4js.defs ) {
+            aCallback( proj );
+        } else {
+            $.get( globalThis['lizUrls'].wms, {
+                'repository':aRep,
+                'project':aProj,
+                'SERVICE':'WMS',
+                'REQUEST':'GetProj4',
+                'authid': proj
+            }, function ( aText ) {
+                Proj4js.defs[proj] = aText;
+                new OpenLayers.Projection(proj);
+                aCallback( proj );
+            }
+            );
+        }
+    }
     $('#map-projects .thumbnail a.liz-project-show-desc').each(function (i, e) { $(e).attr('onclick', ''); });
     $('#map-projects .thumbnail a.liz-project-show-desc').click(function () {
         var self = $(this);
@@ -24,10 +43,11 @@ $(document).ready(function () {
     });
     $('#map-projects .thumbnail a.liz-project-view').click(function () {
         var self = $(this);
+        const searchParams = new URLSearchParams(self.attr('href').split('?')[1]);
         var desc = self.parent().parent().find('.liz-project-desc');
         var proj = desc.find('span.proj').text();
         var bbox = desc.find('span.bbox').text();
-        lizMap.loadProjDefinition(proj, function (aProj) {
+        loadProjDefinition(proj, searchParams.get('repository'), searchParams.get('project'), function (aProj) {
             var bounds = OpenLayers.Bounds.fromString(bbox);
             bounds.transform(aProj, 'EPSG:4326');
             var mapBounds = lizMap.map.getExtent().transform(lizMap.map.getProjection(), 'EPSG:4326');
