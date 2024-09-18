6524b0a227d0ab2ddb78a89146b23111e3476609
rldhont <rldhont@gmail.com>
2024-09-18 14:46:38 +0200

    Using Lizmap\Project\Qgis\ProjectInfo in QgisProject::setLayerOpacity

--- lizmap/modules/lizmap/lib/Project/QgisProject.php
+++ lizmap/modules/lizmap/lib/Project/QgisProject.php
@@ -418,6 +418,19 @@ class QgisProject
      */
     protected function setLayerOpacity(ProjectConfig $cfg)
     {
+        if ($this->path) {
+            $project = Qgis\ProjectInfo::fromQgisPath($this->path);
+            foreach ($project->projectlayers as $layer) {
+                if (!isset($layer->layerOpacity) || $layer->layerOpacity == 1) {
+                    continue;
+                }
+                $layerCfg = $cfg->getLayer($layer->layername);
+                if ($layerCfg) {
+                    $layerCfg->opacity = $layer->layerOpacity;
+                }
+            }
+            return;
+        }
         $layers = $this->layers;
         // loop through project layers to get embedded ones as well
         foreach ($layers as $layer) {
