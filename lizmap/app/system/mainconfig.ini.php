cacbd937404d8fecbfe02d5d4ef479c237277a01
Etienne Trimaille <gustrimaille@yahoo.fr>
2024-09-18 15:06:07 +0200

    Bump recommended desktop plugin version, increase server plugin version to 2.10.1

--- lizmap/app/system/mainconfig.ini.php
+++ lizmap/app/system/mainconfig.ini.php
@@ -35,14 +35,14 @@ fallbackLocale=en_US
 ; QGIS server required minimum version
 qgisServer="3.22"
 ; Lizmap server QGIS plugin required minimum version
-lizmapServerPlugin="2.10.0"
+lizmapServerPlugin="2.10.1"
 ; Lizmap QGIS desktop plugin required/recommended minimum version for newly or updated project only
 ; This version MUST match at least on https://plugins.qgis.org/plugins/lizmap/#plugin-versions
 ; with the minimum QGIS server version supported above.
 ; This value is only forwarded to the plugin thanks to the server metadata. According to the age of the project, it
 ; will be either recommended or nothing.
-lizmapDesktopPlugin=40314
-lizmapDesktopPluginDate="2024-05-22"
+lizmapDesktopPlugin=40324
+lizmapDesktopPluginDate="2024-09-09"
 
 ; Versions written in QGIS/CFG files, for the GIS administrator
 ; Lizmap CFG files with a lower target version are not displayed in the landing page, but displayed in the administration panel to warn the GIS administrator
