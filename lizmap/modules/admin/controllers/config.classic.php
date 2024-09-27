3788fefea810c4aac5b85c342d612d558de7b5c9
Raphaël Martin <rmartin@3liz.com>
2024-09-27 16:39:59 +0200

    show modules info

--- lizmap/modules/admin/controllers/config.classic.php
+++ lizmap/modules/admin/controllers/config.classic.php
@@ -1,4 +1,7 @@
 <?php
+
+use LizmapAdmin\ModulesInfo\ModulesChecker;
+
 /**
  * Lizmap administration.
  *
@@ -87,10 +90,13 @@ class configCtrl extends jController
 
         $this->prepareServicesForm($form, $services);
 
+        $modules = new ModulesChecker();
+
         $tpl = new jTpl();
         $tpl->assign('showSystem', !lizmap::getServices()->hideSensitiveProperties());
         $tpl->assign('servicesForm', $form);
         $tpl->assign('version', $version);
+        $tpl->assign('modules', $modules->getList(true));
         $rep->body->assign('MAIN', $tpl->fetch('config'));
         $rep->body->assign('selectedMenuItem', 'lizmap_configuration');
 
