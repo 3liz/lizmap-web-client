d5ed467517edfb3fb92e1a07eb171436235f8928
Raphaël Martin <rmartin@3liz.com>
2024-09-27 16:35:30 +0200

    ModuleInfo class

---
+++ lizmap/modules/admin/lib/ModulesInfo/ModulesChecker.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LizmapAdmin\ModulesInfo;
+
+use Jelix\IniFile\IniReader;
+
+class ModulesChecker
+{
+    private const coreModules = array('jelix',
+        'jacl2',
+        'jacl2db',
+        'jcommunity',
+        'admin',
+        'dataviz',
+        'filter',
+        'action',
+        'dynamicLayers',
+        'lizmap',
+        'proj4php',
+        'view',
+        'jacl2db_admin',
+        'jauthdb_admin',
+        'master_admin');
+
+    public function getList($withCore = false)
+    {
+        $moduleConfig = \jApp::config()->modules;
+
+        // file_get_contents(\jApp::varConfigPath('installer.ini.php'));
+        $installreRadr = new IniReader(\jApp::varConfigPath('installer.ini.php'));
+
+        $enabledModuleName = array();
+        foreach ($moduleConfig as $paramName => $value) {
+
+            if (preg_match('/(\w*)\.enabled/', $paramName, $matches) && $value == '1') {
+                $moduleSlug = $matches[1];
+                $coreModule = in_array($moduleSlug, self::coreModules);
+                if ($withCore || ((!$withCore) && !$coreModule)) {
+                    $version = $installreRadr->getValue($moduleSlug.'.version', 'modules');
+                    $enabledModuleName[] = new ModuleMetaData($moduleSlug, $version, $coreModule);
+                }
+            }
+        }
+
+        return $enabledModuleName;
+    }
+}
