fa66bb4ecf16568e239f5dd0ffc301da50c1ebc1
rldhont <rldhont@gmail.com>
2024-10-15 17:56:44 +0200

    new Lizmap\Project\Qgis\Layer\* classes

---
+++ tests/units/classes/Project/Qgis/VectorLayerConstraintExpressionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Lizmap\Project\Qgis;
+use Lizmap\App;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class VectorLayerConstraintExpressionTest extends TestCase
+{
+    public function testEmpty()
+    {
+        $xmlStr ='<constraint desc="" exp="" field="id"></constraint>';
+        $oXml = App\XmlTools::xmlReaderFromString($xmlStr);
+        $constraint = Qgis\Layer\VectorLayerConstraintExpression::fromXmlReader($oXml);
+
+        $this->assertInstanceOf(Qgis\Layer\VectorLayerConstraintExpression::class, $constraint);
+        $this->assertEquals('id', $constraint->field);
+        $this->assertEquals('', $constraint->exp);
+        $this->assertEquals('', $constraint->desc);
+    }
+
+    public function testExpression()
+    {
+        $xmlStr ='<constraint desc="Web site URL must start with \'http\'" exp="left( &quot;website&quot;, 4) = \'http\'" field="website"></constraint>';
+        $oXml = App\XmlTools::xmlReaderFromString($xmlStr);
+        $constraint = Qgis\Layer\VectorLayerConstraintExpression::fromXmlReader($oXml);
+
+        $this->assertInstanceOf(Qgis\Layer\VectorLayerConstraintExpression::class, $constraint);
+        $this->assertEquals('website', $constraint->field);
+        $this->assertEquals('left( "website", 4) = \'http\'', $constraint->exp);
+        $this->assertEquals('Web site URL must start with \'http\'', $constraint->desc);
+    }
+}
