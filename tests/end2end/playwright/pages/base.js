0d53fd301b3e1962b98d54d6f3f9f2ef0b5cf1d0
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-01-16 17:21:39 +0100

    Tests - Extends base class for testing admin pages and landing page

---
+++ tests/end2end/playwright/pages/base.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+import {expect, Locator, Page} from '@playwright/test';
+
+export class BasePage {
+    /** @type {Page} */
+    page;
+
+    /**
+     * Header menu
+     * @type {Locator}
+     */
+    headerMenu;
+
+    /**
+     * Top message bar
+     * @type {Locator}
+     */
+    alert;
+
+    /**
+     * Constructor for a base page
+     * @param {Page} page The playwright page
+     */
+    constructor(page) {
+        this.page = page;
+        this.headerMenu = page.locator('#headermenu');
+        this.alert = page.locator('.alert');
+    }
+
+    /**
+     * Check main alert message : level and content if necessary
+     * @param {string} level Name of the CSS class for the level
+     * @param {string} message Content of the message, if necessary
+     */
+    async checkAlert(level, message) {
+        await expect(this.alert).toHaveClass(new RegExp(level, "g"));
+        if (message) {
+            await expect(this.alert).toHaveText(message);
+        }
+    }
+}
