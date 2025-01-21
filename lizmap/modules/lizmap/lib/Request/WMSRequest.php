b911074eeeec0fc3258009c2865d26d4b3713176
nboisteault <nboisteault@3liz.com>
2025-01-21 17:22:04 +0100

    Fix: incorrect replacement in `<a href="media/"><img src="media/"></a>` Regexp must be ungreedy

--- lizmap/modules/lizmap/lib/Request/WMSRequest.php
+++ lizmap/modules/lizmap/lib/Request/WMSRequest.php
@@ -670,7 +670,7 @@ class WMSRequest extends OGCRequest
                 $templateConfigured = true;
                 // first replace all "media/bla/bla/media.ext" by full url
                 $popupTemplate = preg_replace_callback(
-                    '#(["\']){1}((\.\./)?media/.+\.\w{3,10})(["\']){1}#',
+                    '#(["\']){1}((\.\./)?media/.+\.\w{3,10})(["\']){1}#U',
                     array($this, 'replaceMediaPathByMediaUrl'),
                     $popupTemplate
                 );
@@ -749,7 +749,7 @@ class WMSRequest extends OGCRequest
                 if ($attribute['name'] == 'maptip') {
                     // first replace all "media/bla/bla/media.ext" by full url
                     $maptipValue = preg_replace_callback(
-                        '#(["\']){1}((\.\./)?media/.+\.\w{3,10})(["\']){1}#',
+                        '#(["\']){1}((\.\./)?media/.+\.\w{3,10})(["\']){1}#U',
                         array($this, 'replaceMediaPathByMediaUrl'),
                         $attribute['value']
                     );
