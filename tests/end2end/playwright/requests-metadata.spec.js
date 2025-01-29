437b308251b6a710c270765045d5f0f0e47ee793
rldhont <rldhont@gmail.com>
2025-01-29 18:33:31 +0100

    Tests e2e playwright : Lint tests

--- tests/end2end/playwright/requests-metadata.spec.js
+++ tests/end2end/playwright/requests-metadata.spec.js
@@ -1,13 +1,18 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
+/**
+ * Playwright Page
+ * @typedef {import('@playwright/test').APIResponse} APIResponse
+ */
+
 const url = 'index.php/view/app/metadata';
 
 /**
  * Check for a JSON response about the metadata
- * @param {Response} response The response object
+ * @param {APIResponse} response The response object
  *
- * @return {JSON} The JSON response
+ * @return {Promise<any>} The JSON response
  */
 export async function checkJson(response) {
     expect(response.ok()).toBeTruthy();
