89a8a3011503b44de0fa0939a8af351871e46c6f
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2025-01-29 12:16:55 +0100

    UPDATE: Eslint conf

---
+++ assets/eslint.config.mjs
@@ -0,0 +1,50 @@
+import js from "@eslint/js";
+import jsdoc from "eslint-plugin-jsdoc";
+export default [
+  js.configs.recommended,
+  jsdoc.configs['flat/recommended'],
+
+  {
+    plugins: {
+      jsdoc,
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        OpenLayers: "readonly",
+        lizMap: "readonly",
+        lizDict: "readonly",
+        lizUrls: "readonly",
+        lizProj4: "readonly",
+
+        window: "readonly",
+        document: "readonly",
+        Document: "readonly",
+        HTMLElement: "readonly",
+        Node: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        Event: "readonly",
+        MouseEvent: "readonly",
+        Response: "readonly",
+        Element: "readonly",
+
+        $: "readonly",
+        jQuery: "readonly",
+      },
+    },
+    rules: {
+      "indent": ["error", 4, {
+        "SwitchCase": 1,
+        "ignoredNodes": ["TemplateLiteral *"],
+      }],
+      "no-prototype-builtins": "off",
+      "no-undef": "off",
+      'jsdoc/require-description': 'warn',
+    },
+  },
+];
