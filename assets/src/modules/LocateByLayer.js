afcc6c1c80f63ec49fe33301482618a1838e90ab
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-02-19 11:02:53 +0100

    Run pre-commit on all files

--- assets/src/modules/LocateByLayer.js
+++ assets/src/modules/LocateByLayer.js
@@ -25,15 +25,15 @@ export default class LocateByLayer {
     constructor(locateByLayer, vectorLayerFeatureTypeList, map, lizmap3) {
         this._map = map;
         this._vectorLayerFeatureTypeList = vectorLayerFeatureTypeList;
-        
+
         this._lizmap3 = lizmap3;
         this._lizmap3LocateByLayerConfig = lizmap3.config.locateByLayer;
-        
+
         const locateBtn = document.getElementById('button-locate');
         if (locateByLayer) {
             this.addLocateByLayer();
-            document.querySelector('#mapmenu .locate').classList.add('active') 
-            document.getElementById('locate').classList.remove('hide') 
+            document.querySelector('#mapmenu .locate').classList.add('active')
+            document.getElementById('locate').classList.remove('hide')
         } else {
             locateBtn?.parentNode.classList.add('hide');
         }
