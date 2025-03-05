089b95e8be754a8b9c28fbf8db72144877de9725
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-03-05 18:39:47 +0100

    ESLint files, and upgrade it to 9.20.1 in pre-commit, add PHP-CS-Fixer

--- tests/end2end/playwright/header.spec.js
+++ tests/end2end/playwright/header.spec.js
@@ -8,50 +8,51 @@ test.describe('Header',
     },
     () => {
 
-    test('Login info as user A on the landing page', async ({ browser }) => {
-        const userA = await browser.newContext({ storageState: 'playwright/.auth/user_in_group_a.json' });
-        const page = await userA.newPage();
-        await page.goto('index.php');
+        test('Login info as user A on the landing page', async ({ browser }) => {
+            const userA = await browser.newContext({ storageState: 'playwright/.auth/user_in_group_a.json' });
+            const page = await userA.newPage();
+            await page.goto('index.php');
 
-        // Fixme, class "hide" but visible ?
-        await expect(page.locator("#info-user-login")).toHaveText("user_in_group_a");
-        await expect(page.locator("#info-user-firstname")).toHaveText("User A");
-        await expect(page.locator("#info-user-firstname")).toHaveClass("hide");
-        await expect(page.locator("#info-user-firstname")).toBeVisible();
-        await expect(page.locator("#info-user-lastname")).toHaveText("Testadiferro");
-        await expect(page.locator("#info-user-lastname")).toHaveClass("hide");
-        await expect(page.locator("#info-user-lastname")).toBeVisible();
-        await expect(page.locator("#info-user-organization")).toHaveText("Make it KISS");
-        await expect(page.locator("#info-user-organization")).toHaveClass("hide");
-        await expect(page.locator("#info-user-organization")).toBeVisible();
-    });
+            // Fixme, class "hide" but visible ?
+            await expect(page.locator("#info-user-login")).toHaveText("user_in_group_a");
+            await expect(page.locator("#info-user-firstname")).toHaveText("User A");
+            await expect(page.locator("#info-user-firstname")).toHaveClass("hide");
+            await expect(page.locator("#info-user-firstname")).toBeVisible();
+            await expect(page.locator("#info-user-lastname")).toHaveText("Testadiferro");
+            await expect(page.locator("#info-user-lastname")).toHaveClass("hide");
+            await expect(page.locator("#info-user-lastname")).toBeVisible();
+            await expect(page.locator("#info-user-organization")).toHaveText("Make it KISS");
+            await expect(page.locator("#info-user-organization")).toHaveClass("hide");
+            await expect(page.locator("#info-user-organization")).toBeVisible();
+        });
 
-    test('Login info as user A on project page', async ({ browser }) => {
-        const userA = await browser.newContext({ storageState: 'playwright/.auth/user_in_group_a.json' });
-        const userPage = await userA.newPage();
-        const projectPage = new ProjectPage(userPage, 'world-3857');
-        await projectPage.open();
+        test('Login info as user A on project page', async ({ browser }) => {
+            const userA = await browser.newContext({ storageState: 'playwright/.auth/user_in_group_a.json' });
+            const userPage = await userA.newPage();
+            const projectPage = new ProjectPage(userPage, 'world-3857');
+            await projectPage.open();
 
-        await expect(userPage.locator("#info-user-login")).toHaveText("user_in_group_a");
-        await expect(userPage.locator("#info-user-firstname")).toHaveText("User A");
-        await expect(userPage.locator("#info-user-firstname")).toHaveClass("hide");
-        await expect(userPage.locator("#info-user-firstname")).not.toBeVisible();
-        await expect(userPage.locator("#info-user-lastname")).toHaveText("Testadiferro");
-        await expect(userPage.locator("#info-user-lastname")).toHaveClass("hide");
-        await expect(userPage.locator("#info-user-lastname")).not.toBeVisible();
-        await expect(userPage.locator("#info-user-organization")).toHaveText("Make it KISS");
-        await expect(userPage.locator("#info-user-organization")).toHaveClass("hide");
-        await expect(userPage.locator("#info-user-organization")).not.toBeVisible();
-    });
+            await expect(userPage.locator("#info-user-login")).toHaveText("user_in_group_a");
+            await expect(userPage.locator("#info-user-firstname")).toHaveText("User A");
+            await expect(userPage.locator("#info-user-firstname")).toHaveClass("hide");
+            await expect(userPage.locator("#info-user-firstname")).not.toBeVisible();
+            await expect(userPage.locator("#info-user-lastname")).toHaveText("Testadiferro");
+            await expect(userPage.locator("#info-user-lastname")).toHaveClass("hide");
+            await expect(userPage.locator("#info-user-lastname")).not.toBeVisible();
+            await expect(userPage.locator("#info-user-organization")).toHaveText("Make it KISS");
+            await expect(userPage.locator("#info-user-organization")).toHaveClass("hide");
+            await expect(userPage.locator("#info-user-organization")).not.toBeVisible();
+        });
 
-    test('Login info as anonymous on the landing page', async ({ page }) => {
-        await page.goto('index.php');
-        await expect(page.locator("#headermenu .login")).toHaveText("Connect");
-    });
+        test('Login info as anonymous on the landing page', async ({ page }) => {
+            await page.goto('index.php');
+            await expect(page.locator("#headermenu .login")).toHaveText("Connect");
+        });
 
-    test('Login info as anonymous on project page', async ({ page }) => {
-        const projectPage = new ProjectPage(page, 'world-3857');
-        await projectPage.open();
-        await expect(page.locator("#headermenu .login")).toHaveText("Connect");
-    });
-});
+        test('Login info as anonymous on project page', async ({ page }) => {
+            const projectPage = new ProjectPage(page, 'world-3857');
+            await projectPage.open();
+            await expect(page.locator("#headermenu .login")).toHaveText("Connect");
+        });
+    }
+);
