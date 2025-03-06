b1f7cb05a43be96d29e683bddf1653ba4d8b1383
rldhont <rldhont@gmail.com>
2025-03-06 17:19:36 +0100

    Clear lizmap/www/assets/jelix

--- lizmap/www/assets/jelix/ckeditor5/translations/en.js
+++
@@ -1,1 +0,0 @@
-// dummy file for Jelix webassets
--- lizmap/www/assets/jelix/js/jforms/datepickers/default/ui.pt.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($){
-    var d = $.datepicker.regional['pt'];
-    d.buttonText = 'Abrir o calendário';
-    d.resetButtonText = 'Apagar a data';
-    $.datepicker.setDefaults($.datepicker.regional['pt'])
-});
