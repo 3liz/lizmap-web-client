634a8165e03f5b87e3718bef04287571bfd58b46
Etienne Trimaille <gustrimaille@yahoo.fr>
2025-04-29 11:44:13 +0200

    Landing page - Only hide the gray box for now, until we remove it soon

--- tests/end2end/playwright/project-homepage.spec.js
+++ tests/end2end/playwright/project-homepage.spec.js
@@ -10,17 +10,28 @@ test.describe('Projects homepage @readonly', function () {
 
     test('should display project metadata (cold cache)', async function ({ page }) {
 
-        let project = page.locator('.liz-project').filter({ hasText: 'Test tags: nature, flower' });
+        let project = page.locator(
+            '.liz-repository-project-item'
+        ).filter(
+            { hasText: 'Test tags: nature, flower' }
+        ).locator('.liz-project');
+
         await expect(project).toHaveAttribute(
             "data-lizmap-proj",'EPSG:4326');
         await expect(project).toHaveAttribute(
             "data-lizmap-bbox", '-1.2459627329192546, -1.0, 1.2459627329192546, 1.0');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-keywords", 'nature, flower');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-title", 'Test tags: nature, flower');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-abstract", 'This is an abstract');
 
         const allMetadata = await project.locator('.liz-project-desc');
         await expect(allMetadata).not.toBeVisible();
 
         await project.hover();
-        await expect(allMetadata).toBeVisible();
+        // await expect(allMetadata).toBeVisible();
         await expect(allMetadata.locator('.title')).toContainText('Test tags: nature, flower');
         await expect(allMetadata.locator('.abstract')).toContainText('This is an abstract');
         await expect(allMetadata.locator('.keywordList')).toContainText('nature, flower');
@@ -30,17 +41,28 @@ test.describe('Projects homepage @readonly', function () {
         await expect(allMetadata).not.toBeVisible();
 
         // another project
-        project = page.locator('.liz-project').filter({ hasText: 'Tests tags: nature, tree' });
+        project = page.locator(
+            '.liz-repository-project-item'
+        ).filter(
+            { hasText: 'Tests tags: nature, tree' }
+        ).locator('.liz-project');
+
         await expect(project).toHaveAttribute(
             "data-lizmap-proj",'EPSG:4326');
         await expect(project).toHaveAttribute(
             "data-lizmap-bbox", '-1.2459627329192546, -1.0, 1.2459627329192546, 1.0');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-keywords", 'nature, tree');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-title", 'Tests tags: nature, tree');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-abstract", 'Tags: nature, tree');
 
         const allMetadataTree = project.locator('.liz-project-desc');
         await expect(allMetadataTree).not.toBeVisible();
 
         await project.hover();
-        await expect(allMetadataTree).toBeVisible();
+        // await expect(allMetadataTree).toBeVisible();
         await expect(allMetadataTree.locator('.title')).toContainText('Tests tags: nature, tree');
         await expect(allMetadataTree.locator('.abstract')).toContainText('Tags: nature, tree');
         await expect(allMetadataTree.locator('.keywordList')).toContainText('nature, tree');
@@ -53,16 +75,27 @@ test.describe('Projects homepage @readonly', function () {
 
     test('should display project metadata (hot cache)', async function ({ page }) {
 
-        const project = page.locator('.liz-project').filter({ hasText: 'Test tags: nature, flower' });
+        const project = page.locator(
+            '.liz-repository-project-item'
+        ).filter(
+            { hasText: 'Test tags: nature, flower' }
+        ).locator('.liz-project');
+
         await expect(project).toHaveAttribute(
             "data-lizmap-proj",'EPSG:4326');
         await expect(project).toHaveAttribute(
             "data-lizmap-bbox", '-1.2459627329192546, -1.0, 1.2459627329192546, 1.0');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-keywords", 'nature, flower');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-title", 'Test tags: nature, flower');
+        await expect(project).toHaveAttribute(
+            "data-lizmap-abstract", 'This is an abstract');
         const allMetadata = project.locator('.liz-project-desc');
         await expect(allMetadata).not.toBeVisible();
 
         await project.hover();
-        await expect(allMetadata).toBeVisible();
+        // await expect(allMetadata).toBeVisible();
         await expect(allMetadata.locator('.title')).toContainText('Test tags: nature, flower');
         await expect(allMetadata.locator('.abstract')).toContainText('This is an abstract');
         await expect(allMetadata.locator('.keywordList')).toContainText('nature, flower');
