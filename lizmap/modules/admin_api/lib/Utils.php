4b56e779c3f617283c266f061aedea86ed4387ec
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2025-04-29 11:05:19 +0200

    ADD: POST request for creating and editing repo

--- lizmap/modules/admin_api/lib/Utils.php
+++ lizmap/modules/admin_api/lib/Utils.php
@@ -2,6 +2,8 @@
 
 namespace LizmapApi;
 
+use LizmapAdmin\RepositoryRightsService;
+
 class Utils
 {
     /**
@@ -23,5 +25,89 @@ class Utils
 
         return $array[$length - 1].'/';
 
+    }
+
+    /**
+     * Retrieves and returns a list of all group IDs.
+     *
+     * @param object $manager the manager object from which group information is retrieved
+     *
+     * @return array an array of group IDs
+     */
+    private static function getAllGroups(object $manager): array
+    {
+        $all = $manager->getGroupRights()['groups'];
+        $arrayGroup = array();
+
+        foreach ($all as $group) {
+            $arrayGroup[] = $group->id_aclgrp;
+        }
+
+        return $arrayGroup;
+    }
+
+    /**
+     * Verifies the validity of the provided group and right parameters against existing groups and rights.
+     *
+     * @param null|string $group the name of the group to validate
+     * @param null|string $right the name of the right to validate
+     * @param string      $key   the key used to fetch the list of rights
+     *
+     * @return array an associative array containing a boolean under the key 'bool' indicating success or failure,
+     *               and a message under the key 'message' describing the result or error
+     */
+    public static function verifyVars(?string $group, ?string $right, string $key): array
+    {
+        if (!$group) {
+            return array(
+                'bool' => false,
+                'message' => "'group' parameter is required !",
+            );
+        }
+
+        if (!$right) {
+            return array(
+                'bool' => false,
+                'message' => "'right' parameter is required !",
+            );
+        }
+
+        $manager = new \jAcl2DbAdminUIManager();
+
+        $allGroups = Utils::getAllGroups($manager);
+
+        $isGroupValid = false;
+
+        // Not using 'array_search' because the return value is problematic when it is equal to 0.
+        foreach ($allGroups as $groupElement) {
+            if ($groupElement == $group) {
+                $isGroupValid = true;
+
+                break;
+            }
+        }
+
+        if (!$isGroupValid) {
+            return array(
+                'bool' => false,
+                'message' => "'{$group}' group doesn't exist !",
+            );
+        }
+
+        $allRights = RepositoryRightsService::getRights($key);
+
+        array_key_exists($right, $allRights) ? $isRightValid = true : $isRightValid = false;
+
+        if (!$isRightValid) {
+            return array(
+                'bool' => false,
+                'message' => "'{$right}' right doesn't exist !",
+            );
+        }
+
+        return array(
+            'bool' => true,
+            'message' => '',
+        );
     }
 }
