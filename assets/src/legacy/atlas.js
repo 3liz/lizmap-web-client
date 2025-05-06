47b2a0c3ea592b53ef6bb9aaf44825e4623e997c
rldhont <rldhont@gmail.com>
2025-05-06 18:03:28 +0200

    JS Atlas fix const assignment

--- assets/src/legacy/atlas.js
+++ assets/src/legacy/atlas.js
@@ -188,7 +188,7 @@ import { getCenter } from 'ol/extent.js';
                             }
                         }
                         // build select options
-                        const options = '<option value="-1"> --- </option>';
+                        let options = '<option value="-1"> --- </option>';
                         for (let i in lizAtlasConfig['features_sorted']) {
                             const item = lizAtlasConfig['features_sorted'][i];
 
