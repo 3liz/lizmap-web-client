cec2998dc03e3f3da910e19ad93c9dbcaca6c8e9
Antonio Viscomi <Antoviscomi@users.noreply.github.com>
2025-05-19 12:10:20 +0200

    Update Lizmap.js
    to fix map.js:3556
    
    TypeError: undefined is not iterable (cannot read property Symbol(Symbol.iterator)) at _createForOfIteratorHelper (Lizmap.js:33:86) at Object.configsloaded (Lizmap.js:80:60) at initialize.triggerEvent (OpenLayers.js?_r=250519080505:497:145) at eval (map.js:3323:33) at _callee$ (map.js:3313:53) at s (map.js:2:1) at Generator.eval (map.js:2:1) at Generator.eval [as next] (map.js:2:1) at asyncGeneratorStep (map.js:2:1) at _next (map.js:2:1)
    
    eval@map.js:3556Promise.catchinit@map.js:3555eval@map.js:3645e@jquery-3.5.1.min.js?_r=250519080505:2t@jquery-3.5.1.min.js?_r=250519080505:2setTimeout(anonymous)@jquery-3.5.1.min.js?_r=250519080505:2c@jquery-3.5.1.min.js?_r=250519080505:2add@jquery-3.5.1.min.js?_r=250519080505:2(anonymous)@jquery-3.5.1.min.js?_r=250519080505:2s.Deferred@jquery-migrate-3.3.1…s?_r=250519080505:2then@jquery-3.5.1.min.js?_r=250519080505:2S.fn.ready@jquery-3.5.1.min.js?_r=250519080505:2eval@map.js:3628./assets/src/legacy/map.js@map.js?_r=250519080505:19__webpack_require__@map.js?_r=250519080505:272(anonymous)@map.js?_r=250519080505:1438(anonymous)@map.js?_r=250519080505:1441 
    for wmsLayer.BoundingBox and wmsLayer.Layer also

--- assets/src/modules/Lizmap.js
+++ assets/src/modules/Lizmap.js
@@ -78,7 +78,7 @@ export default class Lizmap {
                 // child which could be a layer or a group.
                 let wmsLayer = wmsCapabilities.Capability.Layer;
                 while (wmsLayer.BoundingBox === undefined) {
-                    for (const wmsChildLayer of wmsLayer.Layer) {
+                    for (const wmsChildLayer of wmsLayer.Layer || {}) {
                         if (Array.isArray(wmsChildLayer.BoundingBox)) {
                             wmsLayer = wmsChildLayer;
                             break;
@@ -89,7 +89,7 @@ export default class Lizmap {
                 // Update project projection if its axis orientation is not ENU
                 if (configProj.ref !== "" && Array.isArray(wmsLayer.BoundingBox)) {
                     // loop through bounding boxes of the project provided by WMS capabilities
-                    for (const bbox of wmsLayer.BoundingBox) {
+                    for (const bbox of wmsLayer.BoundingBox || {}) {
                         // If the BBOX CRS is not the same of the project projection, continue.
                         if (bbox.crs !== configProj.ref) {
                             continue;
