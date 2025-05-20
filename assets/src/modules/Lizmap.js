faff21138703a6e4d7dc2825b4072a474bdb36d1
Antonio Viscomi <Antoviscomi@users.noreply.github.com>
2025-05-20 02:08:31 +0200

    Update Lizmap.js
    Appying review

--- assets/src/modules/Lizmap.js
+++ assets/src/modules/Lizmap.js
@@ -93,7 +93,7 @@ export default class Lizmap {
                 // Update project projection if its axis orientation is not ENU
                 if (configProj.ref !== "" && Array.isArray(wmsLayer.BoundingBox)) {
                     // loop through bounding boxes of the project provided by WMS capabilities
-                    for (const bbox of wmsLayer.BoundingBox || {}) {
+                    for (const bbox of wmsLayer.BoundingBox) {
                         // If the BBOX CRS is not the same of the project projection, continue.
                         if (bbox.crs !== configProj.ref) {
                             continue;
