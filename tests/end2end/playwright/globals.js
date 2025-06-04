f8cc1ffa1cbfc7046b9789afeb75164cfc254cba
rldhont <rldhont@gmail.com>
2025-06-04 18:05:27 +0200

    Tests e2e Playwright: globals wait for GetLegendGraphic request
    The WMS GetLegendGraphic request is not only a POST request.

--- tests/end2end/playwright/globals.js
+++ tests/end2end/playwright/globals.js
@@ -134,9 +134,14 @@ export async function gotoMap(url, page, mapMustLoad = true, layersInTreeView = 
         if (waitForGetLegendGraphic) {
             // Wait for WMS GetLegendGraphic promise
             const getLegendGraphicPromise = page.waitForRequest(
-                request => request.method() === 'POST' &&
+                request => (
+                    request.method() === 'POST' &&
                     request.postData() != null &&
                     request.postData()?.includes('GetLegendGraphic') === true
+                ) || (
+                    request.method() === 'GET' &&
+                    request.url().includes('GetLegendGraphic') === true
+                )
             );
             // Normal check about the map
             // Wait for WMS GetLegendGraphic
@@ -178,10 +183,14 @@ export async function reloadMap(page, check = true) {
     if (check) {
         // Wait for WMS GetLegendGraphic promise
         const getLegendGraphicPromise = page.waitForRequest(
-            request =>
+            request => (
                 request.method() === 'POST' &&
                 request.postData() != null &&
                 request.postData()?.includes('GetLegendGraphic') === true
+            ) || (
+                request.method() === 'GET' &&
+                request.url().includes('GetLegendGraphic') === true
+            )
         );
         // Normal check about the map
         // Wait for WMS GetLegendGraphic
