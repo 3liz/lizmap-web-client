3f35b59307c9d22f2ea669a05bc2c07265e707a9
rldhont <rldhont@gmail.com>
2025-09-22 16:26:15 +0200

    Define \Lizmap\DataTables\DataTables with static method to convert DTSearchBuilder to expression.

--- lizmap/modules/lizmap/controllers/datatables.classic.php
+++ lizmap/modules/lizmap/controllers/datatables.classic.php
@@ -11,6 +11,7 @@
  * @license Mozilla Public License : http://www.mozilla.org/MPL/
  */
 
+use Lizmap\DataTables\DataTables;
 use Lizmap\Project\UnknownLizmapProjectException;
 use Lizmap\Request\Proxy;
 use Lizmap\Request\WFSRequest;
@@ -169,106 +170,7 @@ class datatablesCtrl extends jController
 
         // Handle search made by searchBuilder
         if ($DTSearchBuilder) {
-            foreach ($DTSearchBuilder['criteria'] as $criteria) {
-                $column = $criteria['data'];
-                $condition = $criteria['condition'];
-                $value = '';
-                $value1 = isset($criteria['value1']) ? addslashes($criteria['value1']) : '';
-                $value2 = isset($criteria['value2']) ? addslashes($criteria['value2']) : '';
-
-                // Map DataTables operators to QGIS Server operators
-                $qgisOperator = '';
-
-                switch ($condition) {
-                    case '=':
-                    case '!=':
-                    case '<':
-                    case '<=':
-                    case '>':
-                    case '>=':
-                        $qgisOperator = $condition;
-                        if ($criteria['type'] == 'num') {
-                            $value = $value1;
-                        } else {
-                            $value = '\''.$value1.'\'';
-                        }
-
-                        break;
-
-                    case 'starts':
-                        $qgisOperator = 'ILIKE';
-                        $value = '\''.$value1.'%\'';
-
-                        break;
-
-                    case '!starts':
-                        $qgisOperator = 'NOT ILIKE';
-                        $value = '\''.$value1.'%\'';
-
-                        break;
-
-                    case 'contains':
-                        $qgisOperator = 'ILIKE';
-                        $value = '\'%'.$value1.'%\'';
-
-                        break;
-
-                    case '!contains':
-                        $qgisOperator = 'NOT ILIKE';
-                        $value = '\'%'.$value1.'%\'';
-
-                        break;
-
-                    case 'ends':
-                        $qgisOperator = 'ILIKE';
-                        $value = '\'%'.$value1.'\'';
-
-                        break;
-
-                    case '!ends':
-                        $qgisOperator = 'NOT ILIKE';
-                        $value = '\'%'.$value1.'\'';
-
-                        break;
-
-                    case 'null':
-                        $qgisOperator = 'IS NULL';
-
-                        break;
-
-                    case '!null':
-                        $qgisOperator = 'IS NOT NULL';
-
-                        break;
-
-                    case 'between':
-                        $qgisOperator = 'BETWEEN';
-                        if ($criteria['type'] == 'num') {
-                            $value = $value1.' AND '.$value2;
-                        } else {
-                            $value = '\''.$value1.'\' AND \''.$value2.'\'';
-                        }
-
-                        break;
-
-                    case '!between':
-                        $qgisOperator = 'NOT BETWEEN';
-                        if ($criteria['type'] == 'num') {
-                            $value = $value1.' AND '.$value2;
-                        } else {
-                            $value = '\''.$value1.'\' AND \''.$value2.'\'';
-                        }
-
-                        break;
-                }
-                // Append the filter to the exp_filter string
-                if (!empty($expFilter)) {
-                    $logic = isset($DTSearchBuilder['logic']) ? $DTSearchBuilder['logic'] : 'AND';
-                    $expFilter .= " {$logic} ";
-                }
-
-                $expFilter .= "{$column} {$qgisOperator} {$value}";
-            }
+            $expFilter = DataTables::convertSearchToExpression($DTSearchBuilder);
         }
 
         if ($expFilter) {
