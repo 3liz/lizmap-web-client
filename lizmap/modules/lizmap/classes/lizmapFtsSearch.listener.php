751f9fb14093e459adaad5cd083aaf92371f3447
Raphaël Martin <rmartin@3liz.com>
2025-11-14 16:15:07 +0100

    refacto : create dedicated LizmapSearch class use it in lizmapFtslistener

--- lizmap/modules/lizmap/classes/lizmapFtsSearch.listener.php
+++ lizmap/modules/lizmap/classes/lizmapFtsSearch.listener.php
@@ -1,5 +1,7 @@
 <?php
 
+use Lizmap\App\LizmapSearch;
+
 /**
  * Lizmap FTS searcher.
  *
@@ -15,7 +17,8 @@ class lizmapFtsSearchListener extends jEventListener
     public function onsearchServiceItem($event)
     {
         // Check if needed table lizmap_fts is queryable
-        if ($this->checkLizmapFts()) {
+        $lizmapSearch = new LizmapSearch(lizmap::getAppContext());
+        if ($lizmapSearch->check()) {
             $event->add(
                 array(
                     'type' => 'Fts',
@@ -24,56 +27,5 @@ class lizmapFtsSearchListener extends jEventListener
                 )
             );
         }
-    }
-
-    protected function checkLizmapFts()
-    {
-        $ok = false;
-        $profile = 'search';
-
-        try {
-            // try to get the specific search profile
-            jProfiles::get('jdb', $profile, true);
-        } catch (Exception $e) {
-            // else use default profile
-            $profile = null;
-        }
-
-        // The Lizmap FTS search is only available for PostgreSQL
-        $cnx = jDb::getConnection($profile);
-        if ($cnx->dbms != 'pgsql') {
-            return false;
-        }
-
-        // Check if lizmap_search table / view / materialized view exists
-        // in the search_path
-        $sql = "
-            SELECT EXISTS (
-            SELECT FROM
-                pg_catalog.pg_class c
-            JOIN
-                pg_catalog.pg_namespace n ON
-                n.oid = c.relnamespace
-            WHERE
-                n.nspname = ANY(current_schemas(FALSE)) AND
-                -- current_schemas(FALSE) returns the list of schemas in the search_path
-                c.relname = 'lizmap_search' AND
-                c.relkind = ANY(ARRAY['r', 'v', 'm', 'f', 'p'])
-                -- r = ordinary table, v = view, m = materialized view, f = foreign table, p = partitioned
-            ) AS lizmap_search_exists;
-        ";
-
-        try {
-            $res = $cnx->query($sql);
-            foreach ($res as $r) {
-                return $r->lizmap_search_exists;
-            }
-        } catch (Exception $e) {
-            jLog::logEx($e, 'error');
-
-            return false;
-        }
-
-        return false;
     }
 }
