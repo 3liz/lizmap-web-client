a84cf0ebdcd2669f8baa187727c8b1d1ec3d5384
rldhont <rldhont@gmail.com>
2026-01-28 16:25:39 +0100

    Tests e2e Playwright: wrap the `Buffer<ArrayBufferLike>` into an `Uint8Array` for `writeFile`
    The method `fs.writeFile` required a `string` or an `Uint8Array`, the response buffer is a `Buffer<ArrayBufferLike>`. We need to wrap it in an `Uint8Array` to get the right type.

--- tests/end2end/playwright/print.spec.js
+++ tests/end2end/playwright/print.spec.js
@@ -1002,8 +1002,8 @@ test.describe('Print base layers', () => {
             if (UPDATE_MOCK_FILES && GetTiles.length <= 6) {
                 // Save file in mock directory for 6 tiles maximum
                 const response = await route.fetch();
-                await fs.mkdir(dirname(pathFile), { recursive: true })
-                await fs.writeFile(pathFile, await response.body())
+                await fs.mkdir(dirname(pathFile), { recursive: true });
+                await fs.writeFile(pathFile, new Uint8Array(await response.body()));
             } else if (existsSync(pathFile)) {
                 // fulfill route's request with mock file
                 await route.fulfill({
@@ -1133,8 +1133,8 @@ test.describe('Print base layers', () => {
                 // Save file in mock directory for 6 tiles maximum
                 try {
                     const response = await route.fetch();
-                    await fs.mkdir(dirname(pathFile), { recursive: true })
-                    await fs.writeFile(pathFile, await response.body())
+                    await fs.mkdir(dirname(pathFile), { recursive: true });
+                    await fs.writeFile(pathFile, new Uint8Array(await response.body()));
                 } catch {
 
                     // fulfill route's request with default transparent tile
