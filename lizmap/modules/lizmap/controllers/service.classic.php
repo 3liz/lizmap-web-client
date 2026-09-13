6558eec1286651cb5c35e40004ae2b20675b46e6
rldhont <rldhont@gmail.com>
2026-09-13 18:06:26 +0200

    Fix: Return error if filter token is unknown or expired
    The filter token was ignored if it is unknown or expired. Now Lizmap will return an error for bar request.

--- lizmap/modules/lizmap/controllers/service.classic.php
+++ lizmap/modules/lizmap/controllers/service.classic.php
@@ -481,6 +481,11 @@ class serviceCtrl extends jController
                     ) {
                         $filters[] = $data->filter;
                     }
+                } else {
+                    // Unkown or expired token
+                    jMessage::add('Unknown or expired token: '.$token, 'BadRequest');
+
+                    return false;
                 }
             }
             if (count($filters) > 0) {
